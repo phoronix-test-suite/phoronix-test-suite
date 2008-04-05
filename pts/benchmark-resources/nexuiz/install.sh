fcb358f0812562784f34bb16a3c55ce647db81b3
Michael Larabel <michael@phx-laptop.(none)>
2008-04-05 17:59:39 -0400

    Nexuiz update (Kano patch)

--- pts/benchmark-resources/nexuiz/install.sh
+++ pts/benchmark-resources/nexuiz/install.sh
@@ -2,6 +2,12 @@
 
 cd $1
 
+if [ -x /usr/games/nexuiz -a -r /usr/share/games/nexuiz/data/music20080229.pk3 ]; then
+cat > nexuiz <<'EOT'
+#!/bin/sh
+/usr/games/nexuiz +exec normal.cfg $@ | grep fps
+EOT
+else
 if [ ! -f nexuiz-24.zip ]
   then
      wget http://internap.dl.sourceforge.net/sourceforge/nexuiz/nexuiz-24.zip -O nexuiz-24.zip
@@ -12,5 +18,5 @@ unzip -o nexuiz-24.zip
 echo "#!/bin/sh
 cd Nexuiz
 ./nexuiz-linux-glx.sh +exec normal.cfg \$@ | grep fps" > nexuiz
+fi
 chmod +x nexuiz
-
