d4d5ec7a8d37fab33c5ce20584d01ee68ee9b49a
Michael Larabel <michael@phx-laptop.(none)>
2008-04-20 11:22:34 -0400

    A number of improvements to tandem_XmlReader and tandem_XmlWriter

--- pts-core/objects/tandem_XmlReader.php
+++ pts-core/objects/tandem_XmlReader.php
@@ -21,14 +21,10 @@ class tandem_XmlReader
 	}
 	function listStatements($SEARCH_DO = FALSE, $SEARCH_QUERY = "")
 	{
-		// TODO: add check to make sure it doesn't occur in XML, but before XML start after XML declaration
+		preg_match_all("'<!--(.*?) -->'si", $this->XML_DATA, $statement_maches);
+		$return_array = array();
 
-		$XML_COPY = $this->XML_DATA;
-		preg_match_all("'<!--(.*?) -->'si", $XML_COPY, $STATEMENT_MATCHES);
-
-		$RETURN_ARRAY = array();
-
-		foreach($STATEMENT_MATCHES[0] as $statement)
+		foreach($statement_maches[0] as $statement)
 		{
 			$name = substr($statement, 0, strpos($statement, ':'));
 			$name = trim(strstr($name, ' '));
@@ -40,15 +36,15 @@ class tandem_XmlReader
 					$value = strstr($statement, ':');
 					$value = trim(substr($value, 1, strpos($value, "-->") - 1));
 
-					array_push($RETURN_ARRAY, $value);
+					array_push($return_array, $value);
 				}
 			}
 			else
 			{
-				array_push($RETURN_ARRAY, $name);
+				array_push($return_array, $name);
 			}
 		}
-		return $RETURN_ARRAY;
+		return $return_array;
 	}
 	function getXMLValue($XML_TAG)
 	{
@@ -56,28 +52,21 @@ class tandem_XmlReader
 	}
 	function getValue($XML_TAG, $XML_MATCH)
 	{
-		$XML_LOCATION_OFFSET = 0;
-
-		do
+		foreach(explode('/', $XML_TAG) as $xml_step)
 		{
-			$LOCATION_NEXT_DEPTH = strpos($XML_TAG, '/', $XML_LOCATION_OFFSET);
+			preg_match("'<$xml_step>(.*?)</$xml_step>'si", $XML_MATCH, $new_match);
 
-			if($LOCATION_NEXT_DEPTH === FALSE)
-				$LOCATION_NEXT_DEPTH = strlen($XML_TAG);
-
-			$THIS_XML_TAG = substr($XML_TAG, $XML_LOCATION_OFFSET, $LOCATION_NEXT_DEPTH - $XML_LOCATION_OFFSET);
-			preg_match("'<$THIS_XML_TAG>(.*?)</$THIS_XML_TAG>'si", $XML_MATCH, $NEW_MATCH);
-
-			if(sizeof($NEW_MATCH) > 1)
-				$XML_MATCH = $NEW_MATCH[1];
+			if(count($new_match) > 1)
+				$XML_MATCH = $new_match[1];
 			else
 				$XML_MATCH = null;
-
-			$XML_LOCATION_OFFSET = $LOCATION_NEXT_DEPTH + 1;
 		}
-		while($XML_LOCATION_OFFSET < strlen($XML_TAG));
 
 		return $XML_MATCH;
+	}
+	function getXMLValues($XML_TAG)
+	{
+		return $this->getXMLArrayValues($XML_TAG);
 	}
 	function getXMLArrayValues($XML_TAG)
 	{
@@ -85,43 +74,42 @@ class tandem_XmlReader
 	}
 	function getArrayValues($XML_TAG, $XML_MATCH)
 	{
-		$XML_ARRAY_VALUES = $this->LocationHierarchyToArray($XML_TAG);
-		$THIS_XML_CONTENTS = $XML_MATCH;
+		$xml_steps = explode('/', $XML_TAG);
+		$this_xml = $XML_MATCH;
 
-		for($i = 0; $i < sizeof($XML_ARRAY_VALUES) - 2; $i++)
-			$THIS_XML_CONTENTS = $this->getValue($XML_ARRAY_VALUES[$i], $THIS_XML_CONTENTS);
+		for($i = 0; $i < count($xml_steps) - 2; $i++)
+			$this_xml = $this->getValue($xml_steps[$i], $this_xml);
 
-		$LAST_XML_VALUE = $XML_ARRAY_VALUES[sizeof($XML_ARRAY_VALUES) - 2];
+		$next_xml_step = $xml_steps[count($xml_steps) - 2];
+		preg_match_all("'<$next_xml_step>(.*?)</$next_xml_step>'si", $this_xml, $xml_matches);
 
-		preg_match_all("'<$LAST_XML_VALUE>(.*?)</$LAST_XML_VALUE>'si", $THIS_XML_CONTENTS, $XML_MATCHES);
+		$return_array = array();
+		$extraction_tags = explode(',', end($xml_steps));
+		$extraction_tags_count = count($extraction_tags);
 
-		$RETURN_ARRAY = array();
-
-		for($i = 0; $i < sizeof($XML_MATCHES[1]); $i++)
+		for($i = 0; $i < count($xml_matches[1]); $i++)
 		{
-			$THIS_ITEM = $this->getValue(end($XML_ARRAY_VALUES), $XML_MATCHES[1][$i]);
-
-		//	if(!empty($THIS_ITEM))
-				array_push($RETURN_ARRAY, $THIS_ITEM);
+			if($extraction_tags_count == 1)
+			{
+				$this_item = $this->getValue($extraction_tags[0], $xml_matches[1][$i]);
+				array_push($return_array, $this_item);
+			}
+			else
+			{
+				if($i == 0)
+				{
+					foreach($extraction_tags as $extract)
+						$return_array[$extract] = array();
+				}
+				foreach($extraction_tags as $extract)
+				{
+					$this_item = $this->getValue($extract, $xml_matches[1][$i]);
+					array_push($return_array[$extract], $this_item);
+				}
+			}
 		}
 
-		return $RETURN_ARRAY;
-	}
-	function LocationHierarchyToArray($LOCATION)
-	{
-		$RETURN_ARRAY = array();
-		$TEMP_LOCATION = $LOCATION;
-
-		while(($POS_NEXT_DELIMITER = strpos($TEMP_LOCATION, '/')) !== FALSE)
-		{
-			array_push($RETURN_ARRAY, substr($TEMP_LOCATION, 0, $POS_NEXT_DELIMITER));
-			$TEMP_LOCATION = substr($TEMP_LOCATION, $POS_NEXT_DELIMITER + 1);
-		}
-
-		if(!empty($TEMP_LOCATION))
-			array_push($RETURN_ARRAY, $TEMP_LOCATION);
-
-		return $RETURN_ARRAY;
+		return $return_array;
 	}
 }
 ?>
--- pts-core/objects/tandem_XmlWriter.php
+++ pts-core/objects/tandem_XmlWriter.php
@@ -16,11 +16,10 @@ class tandem_XmlWriter
 	var $FORMAT_XML;
 	var $XSL_BINDING = null;
 
-	function __construct($READ_FROM_XML = "", $ENCRYPTION_ALGORITHM = "", $NICE_FORMATTING = TRUE)
+	function __construct($READ_FROM_XML = "", $NOT_IMPLEMENTED = "", $NICE_FORMATTING = TRUE)
 	{
 		// TODOs
 		// $READ_FROM_XML: Convert existing XML into array, to be appended to
-		// $ENCRYPTION_ALGORITHM: The encryption algorithm to eval() on XML_OBJECTS
 
 		$this->FORMAT_XML = $NICE_FORMATTING;
 	}
@@ -30,8 +29,8 @@ class tandem_XmlWriter
 	}
 	function addXmlObject($XML_LOCATION, $UNIQUE_IDENTIFIER = 0, $XML_VALUE, $STD_STEP = null, $STEP_ID = null)
 	{
-		$XML_ARRAY = array();
-		$ALT_STEP = -1;
+		$xml_array = array();
+		$alt_step = -1;
 		$steps = 0;
 		
 		if($STD_STEP == null)
@@ -40,33 +39,29 @@ class tandem_XmlWriter
 			$STEP_ID = $UNIQUE_IDENTIFIER;
 
 		if(array_search("$UNIQUE_IDENTIFIER,$XML_LOCATION", $this->XML_STRING_PATHS) !== FALSE)
-			$ALT_STEP = 2;
+			$alt_step = 2;
 		else
 			array_push($this->XML_STRING_PATHS, "$UNIQUE_IDENTIFIER,$XML_LOCATION");
 
-		do
+		$xml_steps = explode('/', $XML_LOCATION);
+		foreach(array_reverse($xml_steps) as $current_tag)
 		{
 			$steps++;
-			$NEXT_POS = strrpos($XML_LOCATION, '/');
-			$CURRENT_TAG = substr($XML_LOCATION, ($NEXT_POS !== FALSE) ? $NEXT_POS + 1 : 0);
-			$XML_LOCATION = substr($XML_LOCATION, 0, $NEXT_POS);
 
-			if(empty($XML_ARRAY))
+			if(empty($xml_array))
 			{
-				$XML_ARRAY = $XML_VALUE;
+				$xml_array = $XML_VALUE;
 			}
-		
-			if(!empty($CURRENT_TAG))
-				$XML_ARRAY = array("$CURRENT_TAG" => $XML_ARRAY);
+			if(!empty($current_tag))
+				$xml_array = array("$current_tag" => $xml_array);
 
 			if($steps == $STD_STEP)
-				$XML_ARRAY = array("id_$UNIQUE_IDENTIFIER" => $XML_ARRAY);
-			if($steps == $ALT_STEP)
-				$XML_ARRAY = array("id_$STEP_ID" => $XML_ARRAY);
+				$xml_array = array("id_$UNIQUE_IDENTIFIER" => $xml_array);
+			if($steps == $alt_step)
+				$xml_array = array("id_$STEP_ID" => $xml_array);
 		}
-		while($NEXT_POS !== FALSE);
 
-		$this->XML_OBJECTS = array_merge_recursive($this->XML_OBJECTS, $XML_ARRAY);
+		$this->XML_OBJECTS = array_merge_recursive($this->XML_OBJECTS, $xml_array);
 	}
 	function addStatement($STATEMENT_NAME, $STATEMENT_VALUE)
 	{
@@ -74,25 +69,24 @@ class tandem_XmlWriter
 	}
 	function getXMLStatements()
 	{
-		$RETURN_STRING = "";
-		
-		$PRINT_STATEMENTS = array_reverse($this->XML_STATEMENTS);
+		$return_string = "";
+		$statements_to_print = array_reverse($this->XML_STATEMENTS);
 
-		foreach($PRINT_STATEMENTS as $statement)
+		foreach($statements_to_print as $statement)
 		{
-			$RETURN_STRING .= "<!-- $statement -->" . "\n";
+			$return_string .= "<!-- $statement -->" . "\n";
 		}
 
-		return $RETURN_STRING;
+		return $return_string;
 	}
 	function getXML()
 	{
-		$FORMATTED_XML = $this->getXMLBelow($this->XML_OBJECTS, 0);
+		$formatted_xml = $this->getXMLBelow($this->XML_OBJECTS, 0);
 
 		$this->addStatement("Generated", date("Y-m-d H:i:s"));
-		$this->addStatement("Checksum", md5($FORMATTED_XML));
+		$this->addStatement("Checksum", md5($formatted_xml));
 
-		return "<?xml version=\"1.0\"?>\n" . $this->getXSL() . $this->getXMLStatements() . $FORMATTED_XML;
+		return "<?xml version=\"1.0\"?>\n" . $this->getXSL() . $this->getXMLStatements() . $formatted_xml;
 	}
 	function getXSL()
 	{
@@ -107,39 +101,42 @@ class tandem_XmlWriter
 	}
 	function getXMLBelow($XML_ARRAY, $TIMES_DEEP)
 	{
-		$FORMATTED_XML = "";
+		$formatted_xml = "";
 
-		foreach($XML_ARRAY as $KEY => $VALUE)
-			if(!is_array($VALUE))
+		foreach($XML_ARRAY as $key => $value)
+		{
+			if(!is_array($value))
 			{
-				$FORMATTED_XML .= $this->getXMLTabs($TIMES_DEEP) . "<$KEY>$VALUE</$KEY>" . $this->getXMLBreaks();
+				$formatted_xml .= $this->getXMLTabs($TIMES_DEEP) . "<$key>$value</$key>" . $this->getXMLBreaks();
 			}
 			else
 			{
-				if(substr($KEY, 0, 3) === "id_")
-					$FORMATTED_XML .= $this->getXMLBelow($VALUE, $TIMES_DEEP);
+				if(substr($key, 0, 3) === "id_")
+				{
+					$formatted_xml .= $this->getXMLBelow($value, $TIMES_DEEP);
+				}
 				else
 				{
-					$FORMATTED_XML .= $this->getXMLTabs($TIMES_DEEP) . "<$KEY>" . $this->getXMLBreaks();
-					$FORMATTED_XML .= $this->getXMLBelow($VALUE, $TIMES_DEEP + 1);
-					$FORMATTED_XML .= $this->getXMLTabs($TIMES_DEEP) . "</$KEY>" . $this->getXMLBreaks();
+					$formatted_xml .= $this->getXMLTabs($TIMES_DEEP) . "<$key>" . $this->getXMLBreaks();
+					$formatted_xml .= $this->getXMLBelow($value, $TIMES_DEEP + 1);
+					$formatted_xml .= $this->getXMLTabs($TIMES_DEEP) . "</$key>" . $this->getXMLBreaks();
 				}
-
 			}
+		}
 
-		return $FORMATTED_XML;
+		return $formatted_xml;
 	}
 	function getXMLTabs($TIMES_DEEP)
 	{
 		if($this->FORMAT_XML !== TRUE)
 			return;
 
-		$RETURN_FORMAT = "";
+		$return_format = "";
 
 		for($i = 0; $i < $TIMES_DEEP; $i++)
-			$RETURN_FORMAT .= "\t";
+			$return_format .= "\t";
 
-		return $RETURN_FORMAT;
+		return $return_format;
 	}
 	function getXMLBreaks()
 	{
@@ -153,5 +150,4 @@ class tandem_XmlWriter
 		return $this->XML_OBJECTS;
 	}
 }
-
 ?>
