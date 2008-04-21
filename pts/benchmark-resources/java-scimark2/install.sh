c6255dee3e05da79372cfb12b768e0ef269acce0
Michael Larabel <michael@phx-laptop.(none)>
2008-04-20 20:29:13 -0400

    Add in java-scimark2 (Java version of scimark2) profile

---
+++ pts/benchmark-resources/java-scimark2/install.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+cd $1
+
+if [ ! -f scimark2lib.zip ]
+ 
+	then
+     wget http://math.nist.gov/scimark2/scimark2lib.zip -O scimark2lib.zip
+fi
+
+unzip -o scimark2lib.zip
+
+echo "#!/bin/sh
+
+rm -f *.result
+java jnt.scimark2.commandline > \$THIS_RUN_TIME.result
+
+case \"\$1\" in
+\"TEST_COMPOSITE\")
+	cat \$THIS_RUN_TIME.result | grep \"Composite Score\"
+	;;
+\"TEST_FFT\")
+	cat \$THIS_RUN_TIME.result | grep \"FFT (1024)\"
+	;;
+\"TEST_SOR\")
+	cat \$THIS_RUN_TIME.result | grep \"SOR (100x100)\"
+	;;
+\"TEST_MONTE\")
+	cat \$THIS_RUN_TIME.result | grep \"Monte Carlo\"
+	;;
+\"TEST_SPARSE\")
+	cat \$THIS_RUN_TIME.result | grep \"Sparse matmult\"
+	;;
+\"TEST_DENSE\")
+	cat \$THIS_RUN_TIME.result | grep \"LU (100x100)\"
+	;;
+esac" > java-scimark2
+chmod +x java-scimark2
