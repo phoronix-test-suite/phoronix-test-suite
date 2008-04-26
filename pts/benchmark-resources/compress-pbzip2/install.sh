aba1160f91a25a686275791b56d223d576be9c31
Michael Larabel <michael@phx-laptop.(none)>
2008-04-26 09:44:49 -0400

    Add compress-pbzip2 test profile

---
+++ pts/benchmark-resources/compress-pbzip2/install.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+cd $1
+
+tar -xvf bzip2-1.0.5.tar.gz
+tar -xvf pbzip2-1.0.2.tar.gz
+cd bzip2-1.0.5/
+make
+cp -f libbz2.a ../pbzip2-1.0.2
+cp -f bzlib.h ../pbzip2-1.0.2
+cd ..
+cd pbzip2-1.0.2/
+make pbzip2-static
+
+if [ ! -f ../pts-shared/pts-wav-sample-file.wav ]
+  then
+     wget http://www.phoronix-test-suite.com/benchmark-files/pts-wav-sample-file.tar.bz2 -O ../pts-shared/pts-wav-sample-file.tar.bz2
+     tar -jxvf ../pts-shared/pts-wav-sample-file.tar.bz2 -C ../pts-shared/
+     rm -f ../pts-shared/pts-wav-sample-file.tar.bz2
+fi
+
+cd ..
+rm -f bigfile
+for i in 1 2 3 4 5 6 7 8
+do
+	cat ../pts-shared/pts-wav-sample-file.wav >> bigfile
+done
+
+cat > compress-pbzip2 <<EOT
+#!/bin/sh
+cd pbzip2-1.0.2/
+/usr/bin/time -f "PBZIP2 Compress Time: %e Seconds" ./pbzip2 -k -f -p\$NUM_CPU_CORES -r -5 ../bigfile 2>&1
+rm -f bigfile.bz2
+EOT
+chmod +x compress-pbzip2
