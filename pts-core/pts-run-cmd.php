b70ae5283a505bcabe536a5f167c378a7f04e7f2
Michael Larabel <michael@phx-laptop.(none)>
2008-04-27 07:50:16 -0400

    Move pts-merge-results inside pts-run-cmd

--- pts-core/pts-run-cmd.php
+++ pts-core/pts-run-cmd.php
@@ -11,8 +11,47 @@ if(isset($argv[2]))
 if(isset($argv[3]))
 	$ARG_2 = $argv[3];
 
+if(isset($argv[4]))
+	$ARG_3 = $argv[4];
+
 switch($COMMAND)
 {
+	case "MERGE_RESULTS":
+		require("pts-core/functions/pts-functions-merge.php");
+
+		$BASE_FILE = $ARG_1;
+		$MERGE_FROM_FILE = $ARG_2;
+		$MERGE_TO = $ARG_3;
+
+		if(empty($BASE_FILE) || empty($MERGE_FROM_FILE))
+		{
+			echo "\nTwo saved result profile names must be supplied.\n";
+			exit;
+		}
+
+		if(empty($MERGE_TO))
+			$MERGE_TO = $OLD_RESULTS;
+
+		$BASE_FILE = pts_find_file($BASE_FILE);
+		$MERGE_FROM_FILE = pts_find_file($MERGE_FROM_FILE);
+
+		if(empty($MERGE_TO))
+		{
+			do
+			{
+				$rand_file = rand(1000, 9999);
+				$MERGE_TO = "merge-$rand_file/";
+			}while(is_dir(SAVE_RESULTS_DIR . $MERGE_TO));
+
+			$MERGE_TO .= "composite.xml";
+		}
+
+		// Merge Results
+		$MERGED_RESULTS = pts_merge_benchmarks(file_get_contents($BASE_FILE), file_get_contents($MERGE_FROM_FILE));
+		pts_save_result($MERGE_TO, $MERGED_RESULTS);
+		display_web_browser(SAVE_RESULTS_DIR . $MERGE_TO);
+
+		break;
 	case "REMOVE_ALL_RESULTS":
 		$remove_all = pts_bool_question("Are you sure you wish to remove all saved results (Y/n)?", true);
 
