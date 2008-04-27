a2695f5b3840cbfbdae2b930e046c6bc510685d6
Michael Larabel <michael@phx-laptop.(none)>
2008-04-27 07:55:28 -0400

    Clean up pts-run-cmd

--- pts-core/pts-run-cmd.php
+++ pts-core/pts-run-cmd.php
@@ -16,80 +16,6 @@ if(isset($argv[4]))
 
 switch($COMMAND)
 {
-	case "MERGE_RESULTS":
-		require("pts-core/functions/pts-functions-merge.php");
-
-		$BASE_FILE = $ARG_1;
-		$MERGE_FROM_FILE = $ARG_2;
-		$MERGE_TO = $ARG_3;
-
-		if(empty($BASE_FILE) || empty($MERGE_FROM_FILE))
-		{
-			echo "\nTwo saved result profile names must be supplied.\n";
-			exit;
-		}
-
-		if(empty($MERGE_TO))
-			$MERGE_TO = $OLD_RESULTS;
-
-		$BASE_FILE = pts_find_file($BASE_FILE);
-		$MERGE_FROM_FILE = pts_find_file($MERGE_FROM_FILE);
-
-		if(empty($MERGE_TO))
-		{
-			do
-			{
-				$rand_file = rand(1000, 9999);
-				$MERGE_TO = "merge-$rand_file/";
-			}while(is_dir(SAVE_RESULTS_DIR . $MERGE_TO));
-
-			$MERGE_TO .= "composite.xml";
-		}
-
-		// Merge Results
-		$MERGED_RESULTS = pts_merge_benchmarks(file_get_contents($BASE_FILE), file_get_contents($MERGE_FROM_FILE));
-		pts_save_result($MERGE_TO, $MERGED_RESULTS);
-		display_web_browser(SAVE_RESULTS_DIR . $MERGE_TO);
-
-		break;
-	case "REMOVE_ALL_RESULTS":
-		$remove_all = pts_bool_question("Are you sure you wish to remove all saved results (Y/n)?", true);
-
-		if($remove_all)
-		{
-			foreach(glob(SAVE_RESULTS_DIR . "*/composite.xml") as $benchmark_file)
-			{
-				$saved_identifier = array_pop(explode('/', dirname($benchmark_file)));
-				pts_remove_saved_result($saved_identifier);
-			}
-		}
-		break;
-	case "REMOVE_RESULT":
-		if(is_file(SAVE_RESULTS_DIR . $ARG_1 . "/composite.xml"))
-		{
-			echo "\n";
-			pts_remove_saved_result($ARG_1);
-		}
-		else
-			echo "\nThis result doesn't exist!\n";
-		break;
-	case "UPLOAD_RESULT":
-
-		if(is_file($ARG_1))
-			$USE_FILE = $ARG_1;
-		else if(is_file(SAVE_RESULTS_DIR . $ARG_1 . "/composite.xml"))
-			$USE_FILE = SAVE_RESULTS_DIR . $ARG_1 . "/composite.xml";
-		else
-		{
-			echo "\nThis result doesn't exist!\n";
-			exit(0);
-		}
-
-		$upload_url = pts_global_upload_result($USE_FILE);
-
-		if(!empty($upload_url))
-			echo "Results Uploaded To: " . $upload_url . "\n\n";
-		break;
 	case "LIST_SAVED_RESULTS":
 		echo pts_string_header("Phoronix Test Suite - Saved Results");
 		foreach(glob(SAVE_RESULTS_DIR . "*/composite.xml") as $benchmark_file)
@@ -114,28 +40,15 @@ switch($COMMAND)
 			}
 		}
 		break;
-	case "SHOW_RESULT":
-		if(is_file(SAVE_RESULTS_DIR . $ARG_1 . "/composite.xml"))
-			$URL = SAVE_RESULTS_DIR . $ARG_1 . "/composite.xml";
-		//else if(trim(@file_get_contents("http://www.phoronix-test-suite.com/global/profile-check.php?id=" . $ARG_1)) == "REMOTE_FILE")
-		//	$URL = "http://global.phoronix-test-suite.com/index.php?k=profile&u=" . trim($ARG_1);
-		else
-			$URL = false;
-
-		if($URL != FALSE)
-			shell_exec("./pts/launch-browser.sh $URL &");
-		else
-			echo "\n$ARG_1 was not found.\n";
-		break;
 	case "FORCE_INSTALL_BENCHMARK":
 	case "INSTALL_BENCHMARK":
+		require("pts-core/functions/pts-functions-install.php");
+
 		if(empty($ARG_1))
 		{
 			echo "\nThe benchmark or suite name to install must be supplied.\n";
 			exit;
 		}
-
-		require("pts-core/functions/pts-functions-install.php");
 
 		if($COMMAND == "FORCE_INSTALL_BENCHMARK")
 			define("PTS_FORCE_INSTALL", 1);
@@ -155,12 +68,13 @@ switch($COMMAND)
 		pts_recurse_install_benchmark($ARG_1, $install_objects);
 		break;
 	case "INSTALL_EXTERNAL_DEPENDENCIES":
+		require("pts-core/functions/pts-functions-install.php");
+
 		if(empty($ARG_1))
 		{
 			echo "\nThe benchmark or suite name to install external dependencies for must be supplied.\n";
 			exit;
 		}
-		require("pts-core/functions/pts-functions-install.php");
 
 		if($ARG_1 == "phoronix-test-suite" || $ARG_1 == "pts" || $ARG_1 == "trondheim-pts")
 		{
@@ -272,18 +186,96 @@ switch($COMMAND)
 			echo "\n$ARG_1 is not a test.\n";
 		}
 		break;
-	case "INITIAL_CONFIG":
-		if(is_file(PTS_USER_DIR . "user-config.xml"))
+	case "SHOW_RESULT":
+		if(is_file(SAVE_RESULTS_DIR . $ARG_1 . "/composite.xml"))
+			$URL = SAVE_RESULTS_DIR . $ARG_1 . "/composite.xml";
+		//else if(trim(@file_get_contents("http://www.phoronix-test-suite.com/global/profile-check.php?id=" . $ARG_1)) == "REMOTE_FILE")
+		//	$URL = "http://global.phoronix-test-suite.com/index.php?k=profile&u=" . trim($ARG_1);
+		else
+			$URL = false;
+
+		if($URL != FALSE)
+			shell_exec("./pts/launch-browser.sh $URL &");
+		else
+			echo "\n$ARG_1 was not found.\n";
+		break;
+	case "UPLOAD_RESULT":
+
+		if(is_file($ARG_1))
+			$USE_FILE = $ARG_1;
+		else if(is_file(SAVE_RESULTS_DIR . $ARG_1 . "/composite.xml"))
+			$USE_FILE = SAVE_RESULTS_DIR . $ARG_1 . "/composite.xml";
+		else
 		{
-			copy(PTS_USER_DIR . "user-config.xml", PTS_USER_DIR . "user-config.xml.old");
-			unlink(PTS_USER_DIR . "user-config.xml");
+			echo "\nThis result doesn't exist!\n";
+			exit(0);
 		}
-		pts_user_config_init();
+
+		$upload_url = pts_global_upload_result($USE_FILE);
+
+		if(!empty($upload_url))
+			echo "Results Uploaded To: " . $upload_url . "\n\n";
+		break;
+	case "REMOVE_ALL_RESULTS":
+		$remove_all = pts_bool_question("Are you sure you wish to remove all saved results (Y/n)?", true);
+
+		if($remove_all)
+		{
+			foreach(glob(SAVE_RESULTS_DIR . "*/composite.xml") as $benchmark_file)
+			{
+				$saved_identifier = array_pop(explode('/', dirname($benchmark_file)));
+				pts_remove_saved_result($saved_identifier);
+			}
+		}
+		break;
+	case "REMOVE_RESULT":
+		if(is_file(SAVE_RESULTS_DIR . $ARG_1 . "/composite.xml"))
+		{
+			echo "\n";
+			pts_remove_saved_result($ARG_1);
+		}
+		else
+			echo "\nThis result doesn't exist!\n";
 		break;
 	case "SYS_INFO":
 		echo pts_string_header("Phoronix Test Suite v" . PTS_VERSION . " (" . PTS_CODENAME . ")\n" . "System Information");
 		echo "Hardware:\n" . pts_hw_string() . "\n\n";
 		echo "Software:\n" . pts_sw_string() . "\n\n";
+		break;
+	case "MERGE_RESULTS":
+		require("pts-core/functions/pts-functions-merge.php");
+
+		$BASE_FILE = $ARG_1;
+		$MERGE_FROM_FILE = $ARG_2;
+		$MERGE_TO = $ARG_3;
+
+		if(empty($BASE_FILE) || empty($MERGE_FROM_FILE))
+		{
+			echo "\nTwo saved result profile names must be supplied.\n";
+			exit;
+		}
+
+		if(empty($MERGE_TO))
+			$MERGE_TO = $OLD_RESULTS;
+
+		$BASE_FILE = pts_find_file($BASE_FILE);
+		$MERGE_FROM_FILE = pts_find_file($MERGE_FROM_FILE);
+
+		if(empty($MERGE_TO))
+		{
+			do
+			{
+				$rand_file = rand(1000, 9999);
+				$MERGE_TO = "merge-$rand_file/";
+			}while(is_dir(SAVE_RESULTS_DIR . $MERGE_TO));
+
+			$MERGE_TO .= "composite.xml";
+		}
+
+		// Merge Results
+		$MERGED_RESULTS = pts_merge_benchmarks(file_get_contents($BASE_FILE), file_get_contents($MERGE_FROM_FILE));
+		pts_save_result($MERGE_TO, $MERGED_RESULTS);
+		display_web_browser(SAVE_RESULTS_DIR . $MERGE_TO);
 		break;
 	case "DIAGNOSTICS_DUMP":
 		echo pts_string_header("Phoronix Test Suite v" . PTS_VERSION . " (" . PTS_CODENAME . ")\n" . "Diagnostics Dump");
@@ -295,6 +287,14 @@ switch($COMMAND)
 			foreach(pts_env_variables() as $var => $var_value)
 				echo $var . " = " . $var_value . "\n";
 			echo "\n";
+		break;
+	case "INITIAL_CONFIG":
+		if(is_file(PTS_USER_DIR . "user-config.xml"))
+		{
+			copy(PTS_USER_DIR . "user-config.xml", PTS_USER_DIR . "user-config.xml.old");
+			unlink(PTS_USER_DIR . "user-config.xml");
+		}
+		pts_user_config_init();
 		break;
 	case "LOGIN":
 		echo "\nIf you haven't already registered for your free PTS Global account, you can do so at http://global.phoronix-test-suite.com/\n\nOnce you have registered your account and clicked the link within the verification email, enter your log-in information below.\n\n";
@@ -311,10 +311,6 @@ switch($COMMAND)
 		}
 		else
 			echo "\nPTS Global Account Not Found.\n";
-		break;
-	case "REMOTE_COMPARISON":
-		echo "Now Use merge-results for remote comparison with integrated Global ID support.";
-		echo "merge-results <Saved File 1 OR Global ID> <Saved File 2 OR Global ID> <Save To>: Merge two saved result sets";
 		break;
 	default:
 		echo "Phoronix Test Suite: Internal Error. Command Not Recognized ($COMMAND).\n";
