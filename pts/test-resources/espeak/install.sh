3c67efee9f3675c88931de8817bdf20209183230
Michael Larabel <Michael AT phoronix.com>
2008-05-16 12:02:57 -0400

    Update espeak profile (Thanks to uncle_fungus)

--- pts/test-resources/espeak/install.sh
+++ pts/test-resources/espeak/install.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 
 cd $1
+THIS_DIR=$(pwd)
 unzip -o espeak-1.37-source.zip
 cd espeak-1.37-source/src/
+sed -i -e "s|/usr|$THIS_DIR/espeak_|g" Makefile
+sed -i -e "s|/usr|$THIS_DIR/espeak_|g" speech.h
 make -j $NUM_CPU_JOBS
+make install
 cd ../..
+rm -rf espeak-1.37-source/
 
 echo "#!/bin/sh
-cd espeak-1.37-source/src/
-/usr/bin/time -f \"eSpeak Synthesis Time: %e Seconds\" ./espeak -f ../../20417-8.txt -w ../../output.wav 2>&1
-rm -f ../../output.wav" > espeak
+cd espeak_/bin/
+LD_LIBRARY_PATH=$THIS_DIR/espeak_/lib/:\$LD_LIBRARY_PATH /usr/bin/time -f \"eSpeak Synthesis Time: %e Seconds\" ./espeak -f ../../20417-8.txt -w /dev/null 2>&1" > espeak
 chmod +x espeak
