03bae8d30a8f930d9d854aab3cbf9ee315b8512d
Michael Larabel <michael@phx-laptop.(none)>
2008-05-21 08:11:05 -0400

    Fix ET:QW demo profile, should now work...

--- pts/test-resources/etqw-demo/install.sh
+++ pts/test-resources/etqw-demo/install.sh
@@ -2,19 +2,11 @@
 
 cd $1
 
-if [ ! -f ETQW-demo2-client-full.r1.x86.run ]
-  then
-     wget ftp://ftp.idsoftware.com/idstuff/etqw/ETQW-demo2-client-full.r1.x86.run -O ETQW-demo2-client-full.r1.x86.run
-fi
-if [ ! -f etqw-demo-demo.tar.bz2 ]
-  then
-     wget http://www.phoronix-test-suite.com/benchmark-files/etqw-demo-demo-1.tar.bz2 -O etqw-demo-demo.tar.bz2
-fi
-
 unzip -o ETQW-demo2-client-full.r1.x86.run
 
 echo "#!/bin/sh
-cd data/\n./etqw.x86 \$@ | grep fps" > etqw
+cd data/
+./etqw.x86 \$@ | grep fps" > etqw
 chmod +x etqw
 
 tar -jxvf etqw-demo-demo.tar.bz2
