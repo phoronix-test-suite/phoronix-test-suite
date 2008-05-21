09b6f6f4db4c9340d1ce2508a008f9fedc7ba6cd
Michael Larabel <michael@phx-laptop.(none)>
2008-05-21 08:07:10 -0400

    Fix Warsow profile, should be working. Thanks to uncle_fungus.

--- pts/test-resources/warsow/install.sh
+++ pts/test-resources/warsow/install.sh
@@ -7,15 +7,16 @@ cp pts-warsow-04.wd10 warsow_0.42_unified/basewsw/demos
 cd warsow_0.42_unified/
 chmod +x warsow.x86_64
 chmod +x warsow.i386
+cd ..
 
 echo "#!/bin/sh
 cd warsow_0.42_unified/
 case \`uname -m\` in
 	\"x86_64\" )
-	./warsow.x86_64 \$@ | grep fps
+	./warsow.x86_64 \$@ | grep seconds
 	;;
 	* )
-	./warsow.i386 \$@ | grep fps
+	./warsow.i386 \$@ | grep seconds
 	;;
 esac" > warsow
 chmod +x warsow
