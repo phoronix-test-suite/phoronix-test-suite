dff0ab696147dd21f5f882d40f2ca0b20bbf9c79
Michael Larabel <michael@phx-laptop.(none)>
2008-05-22 12:01:39 -0400

    Fix etqw-demo profile, should be working well now.

--- pts/test-resources/etqw-demo/install.sh
+++ pts/test-resources/etqw-demo/install.sh
@@ -9,6 +9,18 @@ cd data/
 ./etqw.x86 \$@ | grep fps" > etqw
 chmod +x etqw
 
-tar -jxvf etqw-demo-demo.tar.bz2
+tar -jxvf etqw-demo-demo-1.tar.bz2
 mkdir data/base/demos
 mv -f pts.ndm data/base/demos/pts.ndm
+
+# etqw_pts.cfg
+echo "
+set com_unlockFPS \"1\"
+timenetdemo pts
+wait 2752
+echo ======================
+echo wait '# of frames + 2'
+echo timenetdemo ended
+echo quit
+echo ======================
+quit" > data/base/etqw_pts.cfg
