6201dd9a5652e4d008530a6da19c5a40f150fc66
Michael Larabel <michael@phx-laptop.(none)>
2008-05-22 19:31:24 -0400

    Warsow profile update...

--- pts/test-resources/warsow/install.sh
+++ pts/test-resources/warsow/install.sh
@@ -2,8 +2,10 @@
 
 cd $1
 
+tar -xvf pts-warsow-1.tar.gz
 unzip -o warsow_0.42_unified.zip
-cp pts-warsow-04.wd10 warsow_0.42_unified/basewsw/demos
+cp -f pts-warsow-04.wd10 warsow_0.42_unified/basewsw/demos
+cp -f pts-warsow.cfg warsow_0.42_unified/basewsw/
 cd warsow_0.42_unified/
 chmod +x warsow.x86_64
 chmod +x warsow.i386
@@ -11,7 +13,7 @@ cd ..
 
 echo "#!/bin/sh
 cd warsow_0.42_unified/
-case \`uname -m\` in
+case \$OS_ARCH in
 	\"x86_64\" )
 	./warsow.x86_64 \$@ | grep seconds
 	;;
