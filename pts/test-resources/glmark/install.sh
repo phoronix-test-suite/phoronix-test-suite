66b23e00b4485ddd29426131793ed21651c566fb
Michael Larabel <michael@phx-laptop.(none)>
2008-05-25 10:50:18 -0400

    GLMark test profile update... Basically rewritten against latest version of GLMark and should now be working, and working quite well with different OpenGL tests and resolutions.

--- pts/test-resources/glmark/install.sh
+++ pts/test-resources/glmark/install.sh
@@ -2,6 +2,66 @@
 
 cd $1
 
-tar -xvf GLMark-0.1.tar.gz
-gcc *.c -o glmark -lSDL -lGL -lGLU
+tar -xvf GLMark-0.5.2.tar.gz
+cd GLMark-0.5.2/
+
+patch -p0 <<'EOT'
+--- main.cpp	2008-04-26 04:30:21.000000000 -0400
++++ main.cpp.n	2008-05-25 10:21:22.000000000 -0400
+@@ -17,16 +17,11 @@
+     printf("===================================================\n");
+     printf("    GLMark 08\n");
+     printf("===================================================\n");
+-    
+-    printf("Enter screen width:  ");
+-    scanf("%d", &screen.mWidth);
+-    printf("Enter screen height: ");
+-    scanf("%d", &screen.mHeight);
+-    printf("Enter screen bpp:    ");
+-    scanf("%d", &screen.mBpp);
+-    printf("Enter '1' for fullscreen '0' for windowed: ");
+-    scanf("%d", &screen.mFullScreen);
+-    
++
++    screen.mWidth = atoi(argv[1]);
++    screen.mHeight = atoi(argv[2]);
++    screen.mBpp = 24;
++    screen.mFullScreen = 1;  
+ 
+     printf("===================================================\n");
+     if(!screen.init())
+EOT
+
+make -j $NUM_CPU_JOBS
+cd ..
+
+echo "#!/bin/sh
+
+cd GLMark-0.5.2/
+
+rm -f *.result
+./glmark \$@ > \$THIS_RUN_TIME.result
+
+case \"\$3\" in
+\"VERTEX_ARRAY\")
+	cat \$THIS_RUN_TIME.result | grep \"Vertex array\"
+	;;
+\"VERTEX_BUFFER_OBJ\")
+	cat \$THIS_RUN_TIME.result | grep \"Vertex buffer object\"
+	;;
+\"TEXTURE_LINEAR\")
+	cat \$THIS_RUN_TIME.result | grep \"Linear\"
+	;;
+\"TEXTURE_MIPMAPPED\")
+	cat \$THIS_RUN_TIME.result | grep \"Mipmapped\"
+	;;
+\"GLSL_PER_VERTEX\")
+	cat \$THIS_RUN_TIME.result | grep \"GLSL per vertex lighting\"
+	;;
+\"GLSL_PER_PIXEL\")
+	cat \$THIS_RUN_TIME.result | grep \"GLSL per pixel lighting\"
+	;;
+esac
+" > glmark
+chmod +x glmark
 
--- pts/test-resources/glmark/parse-results.php
+++ pts/test-resources/glmark/parse-results.php
@@ -1,8 +1,6 @@
 <?php
 
-//$BENCHMARK_RESULTS = substr($argv[1], strrpos($argv[1], "WAV To MP3 Encode Time:") + 23);
-//echo trim(substr($BENCHMARK_RESULTS, 0, strpos($BENCHMARK_RESULTS, "Seconds")));
-
-echo $BENCHMARK_RESULTS;
+$BENCHMARK_RESULTS = substr($argv[1], strrpos($argv[1], "FPS:") + 4);
+echo trim(substr($BENCHMARK_RESULTS, 0, strpos($BENCHMARK_RESULTS, "\n")));
 
 ?>
