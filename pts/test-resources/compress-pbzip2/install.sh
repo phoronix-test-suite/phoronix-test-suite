7989866e3bd10bba5a4753a2ba0eac5ef0e801a6
Michael Larabel <michael@phx-laptop.(none)>
2008-05-26 19:14:05 -0400

    compress-pbzip2 update of patch from apaige.

--- pts/test-resources/compress-pbzip2/install.sh
+++ pts/test-resources/compress-pbzip2/install.sh
@@ -12,10 +12,16 @@ make pbzip2-static
 
 cd ..
 
-cat > compress-pbzip2 <<EOT
+cat > pbzip2_process <<EOT
 #!/bin/sh
 cd pbzip2-1.0.2/
-/usr/bin/time -f "PBZIP2 Compress Time: %e Seconds" ./pbzip2 -k -f -p\$NUM_CPU_CORES -r -5 ../compressfile 2>&1
-rm -f compressfile.bz2
+./pbzip2 -c -p\$NUM_CPU_CORES -r -5 ../compressfile > /dev/null
+EOT
+chmod +x pbzip2_process
+
+
+cat > compress-pbzip2 <<EOT
+#!/bin/sh
+/usr/bin/time -f "PBZIP2 Compress Time: %e Seconds" ./pbzip2_process 2>&1
 EOT
 chmod +x compress-pbzip2
