ed9385722b7a08f412aa1ccca0100035478729b7
Michael Larabel <michael@phx-laptop.(none)>
2008-05-28 07:55:39 -0400

    Fix for pts-graph bug

--- pts/test-resources/pts-graph/install.sh
+++ pts/test-resources/pts-graph/install.sh
@@ -4,5 +4,5 @@ tar -xvf pts-graph-benchmark-1.tar.gz
 
 echo "#!/bin/sh
 
-time -f \"pts_Graph Time: %e Seconds\" php pts-graph-benchmark/pts_graph_benchmark.php 2>&1" > pts-graph
+/usr/bin/time -f \"pts_Graph Time: %e Seconds\" php pts-graph-benchmark/pts_graph_benchmark.php 2>&1" > pts-graph
 chmod +x pts-graph
