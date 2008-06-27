b2cecea8509fdc015aee826895cdac0b888802ef
Michael Larabel <michael@phx-laptop.(none)>
2008-06-27 18:22:49 -0400

    Add urbanterror test profile for Urban Terror FPS game

---
+++ pts/test-resources/urbanterror/install.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+unzip -o UrbanTerror_41_FULL.zip
+
+cd UrbanTerror/
+chmod +x ioUrbanTerror.i386
+chmod +x ioUrbanTerror.x86_64
+cd ..
+
+tar -xvf urbanterror-q3ut4-1.tar.gz
+mv -f autoexec.cfg UrbanTerror/q3ut4/
+mv -f pts1.dm_68 UrbanTerror/q3ut4/demos/
+
+echo "#!/bin/sh
+cd UrbanTerror/
+
+case \$OS_ARCH in
+	\"x86_64\" )
+	./ioUrbanTerror.x86_64 \$@ 2>&1 | grep fps
+	;;
+	* )
+	./ioUrbanTerror.i386 \$@ 2>&1 | grep fps
+	;;
+esac" > urbanterror
+chmod +x urbanterror
