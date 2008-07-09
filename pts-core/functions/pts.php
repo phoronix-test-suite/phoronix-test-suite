d69fac2208217f29befcec0dc450628a81cd6703
Michael Larabel <michael@phx-laptop.(none)>
2008-07-09 19:29:28 -0400

    Tag Phoronix Test Suite 1.0.3

--- pts-core/functions/pts.php
+++ pts-core/functions/pts.php
@@ -21,7 +21,7 @@
 	along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
-define("PTS_VERSION", "1.0.2");
+define("PTS_VERSION", "1.0.3");
 define("PTS_CODENAME", "TRONDHEIM");
 define("PTS_TYPE", "DESKTOP");
 
