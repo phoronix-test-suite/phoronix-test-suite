762e1d5a4993127ba7312fe6905be485b409a84f
Michael Larabel <michael@phx-laptop.(none)>
2008-07-09 18:05:06 -0400

    pts: Add Unreal Tournament 2004 Demo test profile (profile named ut2004-demo)

---
+++ pts/test-resources/ut2004-demo/install.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# Installation
+
+chmod +x ut2004-lnx-demo3334.run
+
+./ut2004-lnx-demo3334.run --noexec --target .
+
+tar -xvf UT2004-ptsconfig-1.tar.gz
+tar xvfj ut2004demo.tar.bz2
+
+case $OS_ARCH in
+	"x86_64" )
+	tar xvfj linux-amd64.tar.bz2
+	;;
+	* )
+	tar xvfj linux-x86.tar.bz2
+	;;
+esac
+mv -f System/* System/
+
+echo "#!/bin/sh
+cd System/
+./ut2004-bin \$@" > ut2004-demo
+chmod +x ut2004-demo
+
+mkdir -p ~/.ut2004demo/System
+cp -f UT2004.ini ~/.ut2004demo/System
+
---
+++ pts/test-resources/ut2004-demo/parse-results.php
@@ -0,0 +1,8 @@
+<?php
+
+$file = trim(shell_exec("cat ~/.ut2004demo/Benchmark/benchmark.log"));
+$line = substr($file, strrpos($file, "\n") + 1);
+$line_r = explode("/", $line);
+
+echo trim($line_r[1]);
+?>
