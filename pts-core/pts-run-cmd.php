a8b180f8ba4bfd8c1cc2f842ff3b7a3272c8ffc5
Michael Larabel <michael@phx-laptop.(none)>
2008-07-13 21:32:00 -0400

    Fix a bug that causes list-modules to crash when it comes to parsing a module that was already loaded.

--- pts-core/pts-run-cmd.php
+++ pts-core/pts-run-cmd.php
@@ -185,8 +185,10 @@ switch($COMMAND)
 		echo pts_string_header("Phoronix Test Suite - Modules");
 		foreach(glob(MODULE_DIR . "*.php") as $module_file)
 		{
-			include($module_file);
 		 	$module = basename($module_file, ".php");
+
+			if(!in_array($module, $GLOBALS["PTS_MODULES"]))
+				include($module_file);
 
 			eval("\$module_name = " . $module . "::module_name;"); // TODO: This can be cleaned up once PHP 5.3.0+ is out there and adopted
 			eval("\$module_version = " . $module . "::module_version;");
