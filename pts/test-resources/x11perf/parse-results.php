3f04fc18ef43c71747b1f7038ff3c54cedbf174f
Michael Larabel <michael@phx-laptop.(none)>
2008-07-15 19:02:18 -0400

    pts: Add another option to x11perf test profile

--- pts/test-resources/x11perf/parse-results.php
+++ pts/test-resources/x11perf/parse-results.php
@@ -1,6 +1,10 @@
 <?php
 
 $BENCHMARK_RESULTS = substr($argv[1], 0, strrpos($argv[1], "/sec"));
-echo trim(substr($BENCHMARK_RESULTS, strrpos($BENCHMARK_RESULTS, " ")));
+$BENCHMARK_RESULTS = trim(substr($BENCHMARK_RESULTS, strrpos($BENCHMARK_RESULTS, " ")));
 
+if(substr($BENCHMARK_RESULTS, 0, 1) == "(")
+	$BENCHMARK_RESULTS = substr($BENCHMARK_RESULTS, 1);
+
+echo $BENCHMARK_RESULTS;
 ?>
