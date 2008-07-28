4b98e3d6cc9ba10582b8543c73ffd26d7552d12c
Michael Larabel <michael@phx-laptop.(none)>
2008-07-28 08:33:43 -0400

    Revise PTS External Dependencies string

--- pts-core/functions/pts-functions-install.php
+++ pts-core/functions/pts-functions-install.php
@@ -424,7 +424,7 @@ function pts_install_packages_on_distribution_process($install_objects)
 
 		if(is_file(SCRIPT_DISTRO_DIR . "install-" . $distribution . "-packages.sh") || is_link(SCRIPT_DISTRO_DIR . "install-" . $distribution . "-packages.sh"))
 		{
-			echo "This process may take several minutes...\n";
+			echo "Installing PTS External Dependencies.\nThis process may take several minutes.\n";
 			echo shell_exec("cd " . SCRIPT_DISTRO_DIR . " && sh install-" . $distribution . "-packages.sh " . $install_objects);
 		}
 		else
