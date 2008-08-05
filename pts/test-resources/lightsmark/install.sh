6fc5af3c8c093fb2bae91d607f33c155e3c93900
Michael Larabel <michael@phx-laptop.(none)>
2008-08-05 17:29:21 -0400

    pts: Update to Lightsmark 2.0

--- pts/test-resources/lightsmark/install.sh
+++ pts/test-resources/lightsmark/install.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-tar -jxvf Lightsmark2008.1.9.tar.bz2
+tar -jxvf Lightsmark2008.2.0.tar.bz2
 
 echo "#!/bin/sh
 case \$OS_ARCH in
 	\"x86_64\" )
-	cd Lightsmark2008.1.9/bin/pc-linux64/
+	cd Lightsmark2008.2.0/bin/pc-linux64/
 	;;
 	* )
-	cd Lightsmark2008.1.9/bin/pc-linux32/
+	cd Lightsmark2008.2.0/bin/pc-linux32/
 	;;
 esac
 ./backend \$@ 2>&1" > lightsmark
