c97a1cb150a470a544a234603d846bcb26958c47
Michael Larabel <michael@phx-laptop.(none)>
2008-08-06 14:15:29 -0400

    pts_LineGraph: Don't allow the pointer to hit the minimum line

--- pts-core/objects/pts_LineGraph.php
+++ pts-core/objects/pts_LineGraph.php
@@ -83,6 +83,9 @@ class pts_LineGraph extends pts_CustomGraph
 				if($px_from_left > $this->graph_left_end)
 					$px_from_left = $this->graph_left_end - 1;
 
+				if($value_plot_top <= $this->graph_top_end)
+					$value_plot_top = $this->graph_top_end + 1;
+
 				if($previous_placement != -1 && $previous_offset != -1)
 				{
 					imageline($this->graph_image, $previous_offset, $previous_placement, $px_from_left, $value_plot_top, $paint_color);
