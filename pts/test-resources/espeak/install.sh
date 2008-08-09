deb39910c970f404f1d6b02192bd905fbbe74922
Michael Larabel <michael@phx-laptop.(none)>
2008-08-09 13:30:57 -0400

    pts: Update download links for text file on espeak test

--- pts/test-resources/espeak/install.sh
+++ pts/test-resources/espeak/install.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+tar -xvf gutenberg-science.tar.gz
 THIS_DIR=$(pwd)
 unzip -o espeak-1.37-source.zip
 cd espeak-1.37-source/src/
@@ -12,5 +13,5 @@ rm -rf espeak-1.37-source/
 
 echo "#!/bin/sh
 cd espeak_/bin/
-LD_LIBRARY_PATH=$THIS_DIR/espeak_/lib/:\$LD_LIBRARY_PATH /usr/bin/time -f \"eSpeak Synthesis Time: %e Seconds\" ./espeak -f ../../20417-8.txt -w /dev/null 2>&1" > espeak
+LD_LIBRARY_PATH=$THIS_DIR/espeak_/lib/:\$LD_LIBRARY_PATH /usr/bin/time -f \"eSpeak Synthesis Time: %e Seconds\" ./espeak -f ../../gutenberg-science.txt -w /dev/null 2>&1" > espeak
 chmod +x espeak
