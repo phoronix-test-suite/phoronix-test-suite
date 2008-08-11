1f42363f515e17f3d8a9bfb7f6d96b421ed3fa34
Michael Larabel <michael@phx-laptop.(none)>
2008-08-11 12:27:28 -0400

    pts: Upgrade openarena test profile to OpenArena 0.8.0

--- pts/test-resources/openarena/install.sh
+++ pts/test-resources/openarena/install.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-unzip -o oa076.zip
-cd openarena-0.7.6/baseoa
-tar -xvf ../../openarena-benchmark-files-2.tar.gz
+unzip -o oa080.zip
+cd openarena-0.8.0/baseoa
+tar -xvf ../../openarena-benchmark-files-3.tar.gz
 cd ../..
 
 echo "#!/bin/sh
-cd openarena-0.7.6/
+cd openarena-0.8.0/
 
 case \$OS_ARCH in
 	\"x86_64\" )
