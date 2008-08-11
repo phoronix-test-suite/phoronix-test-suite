b47a96e42308cdaaeae9b40f86561a58178e40d3
Michael Larabel <michael@phx-laptop.(none)>
2008-08-11 12:24:32 -0400

    pts: In encode-mp3 test profile upgrade to LAME 3.98

--- pts/test-resources/encode-mp3/install.sh
+++ pts/test-resources/encode-mp3/install.sh
@@ -8,13 +8,13 @@ fi
 THIS_DIR=$(pwd)
 mkdir $THIS_DIR/lame_
 
-tar -xvf lame-3.97.tar.gz
-cd lame-3.97/
+tar -xvf lame-398.tar.gz
+cd lame-398/
 ./configure --prefix=$THIS_DIR/lame_
 make -j $NUM_CPU_JOBS
 make install
 cd ..
-rm -rf lame-3.97/
+rm -rf lame-398/
 
 echo "#!/bin/sh
 /usr/bin/time -f \"WAV To MP3 Encode Time: %e Seconds\" ./lame_/bin/lame --silent -h ../pts-shared/pts-trondheim-3.wav /dev/null 2>&1" > lame
