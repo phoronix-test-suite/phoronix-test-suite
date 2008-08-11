e5fee5ca7bab517256e31ec2a7db1fee80c5bf7d
Michael Larabel <michael@phx-laptop.(none)>
2008-08-11 12:22:54 -0400

    pts: Upgrade Bonnie++ to 1.03d in bonnie test profile

--- pts/test-resources/bonnie/install.sh
+++ pts/test-resources/bonnie/install.sh
@@ -3,17 +3,18 @@
 THIS_DIR=$(pwd)
 mkdir $THIS_DIR/bonnie_
 
-tar -xvf bonnie++-1.03a.tgz
-cd bonnie++-1.03a/
+tar -xvf bonnie++-1.03d.tgz
+cd bonnie++-1.03d/
 ./configure --prefix=$THIS_DIR/bonnie_
 make -j $NUM_CPU_JOBS
 make install
 cd ..
-rm -rf bonnie++-1.03a/
+rm -rf bonnie++-1.03d/
 
 echo "#!/bin/sh
 echo \$1 > TEST_TYPE
+BONNIERAM=\$((\$SYS_MEMORY * 2))
 rm -rf scratch_dir/
 mkdir scratch_dir/
-./bonnie_/sbin/bonnie++ -d scratch_dir/ -s 2048 2>&1" > bonnie
+./bonnie_/sbin/bonnie++ -d scratch_dir/ -s \$BONNIERAM 2>&1" > bonnie
 chmod +x bonnie
