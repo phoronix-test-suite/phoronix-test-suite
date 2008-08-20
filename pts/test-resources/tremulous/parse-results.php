fc9f9da26bc56418d363fa6069337653492e4595
Michael Larabel <michael@phx-laptop.(none)>
2008-08-20 17:56:39 -0400

    pts: Fix tremulous permissions

--- pts/test-resources/tremulous/parse-results.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$BENCHMARK_RESULTS = trim(substr($argv[1], 0, strrpos($argv[1], " fps")));
-$BENCHMARK_RESULTS = trim(substr($BENCHMARK_RESULTS, strrpos($BENCHMARK_RESULTS, ' ')));
-echo $BENCHMARK_RESULTS;
-?>
