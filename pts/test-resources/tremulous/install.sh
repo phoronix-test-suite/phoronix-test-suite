85885e9921ab53b37acecb7d70b7963f789ef151
Michael Larabel <michael@phx-laptop.(none)>
2008-08-20 17:57:50 -0400

    pts: Fix tremulous test-resource file permissions

---
+++ pts/test-resources/tremulous/install.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+unzip -o tremulous-1.1.0.zip -d tremulous_/
+tar -xvf tremulous-benchmark-1.tar.gz
+
+mv tremulous-benchmark.cfg tremulous_/tremulous/base/
+mv demos/ tremulous_/tremulous/base/
+
+echo "#!/bin/sh
+cd tremulous_/tremulous/
+./tremulous.x86 \$@ 2>&1 | grep fps
+" > tremulous
+chmod +x tremulous
