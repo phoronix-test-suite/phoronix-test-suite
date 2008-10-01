5df663e6e8af0c9ca8c103958fd9323a6cf1a79b
Michael Larabel <michael@phx-laptop.(none)>
2008-10-01 19:20:23 -0400

    pts-core: Remove LOG_FILE after each test run

--- pts-core/functions/pts-functions-run.php
+++ pts-core/functions/pts-functions-run.php
@@ -484,7 +484,7 @@ function pts_run_test($test_identifier, $extra_arguments = "", $arguments_descri
 
 	for($i = 0; $i < $times_to_run; $i++)
 	{
-		$benchmark_log_file = TEST_ENV_DIR . $test_identifier . "/" . $test_identifier . "-" . THIS_RUN_TIME . "-" . $i . ".log";
+		$benchmark_log_file = TEST_ENV_DIR . $test_identifier . "/" . $test_identifier . "-" . THIS_RUN_TIME . "-" . ($i + 1) . ".log";
 		$test_extra_runtime_variables = array_merge($extra_runtime_variables, array("LOG_FILE" => $benchmark_log_file));
 		echo pts_string_header($test_title . " (Run " . ($i + 1) . " of " . $times_to_run . ")");
 		$result_output = array();
@@ -507,6 +507,11 @@ function pts_run_test($test_identifier, $extra_arguments = "", $arguments_descri
 		{
 			pts_module_process("__interim_test_run");
 			sleep(1); // Rest for a moment between tests
+		}
+
+		if(is_file($benchmark_log_file))
+		{
+			@unlink($benchmark_log_file);
 		}
 	}
 
