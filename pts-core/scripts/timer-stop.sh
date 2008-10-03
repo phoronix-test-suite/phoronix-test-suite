7981227364799d0ebb620ff3769d1e85ea90ada2
Michael Larabel <michael@phx-laptop.(none)>
2008-10-03 15:36:59 -0400

    timer-stop: Print out the time

--- pts-core/scripts/timer-stop.sh
+++ pts-core/scripts/timer-stop.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-php -r "\$start_time = @file_get_contents(getenv(\"HOME\") . \"/pts-timer\"); \$end_time = microtime(true); file_put_contents(getenv(\"HOME\") . \"/pts-timer\", (\$end_time - \$start_time));"
+php -r "\$start_time = @file_get_contents(getenv(\"HOME\") . \"/pts-timer\"); \$end_time = microtime(true); \$time_diff = \$end_time - \$start_time; file_put_contents(getenv(\"HOME\") . \"/pts-timer\", \$time_diff); echo \"\n\nTest-Time: \$time_diff Seconds\n\";"
 
