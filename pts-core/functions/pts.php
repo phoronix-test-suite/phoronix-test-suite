41386d61971fb85963dc70a97d00c3bf9031ba89
Michael Larabel <michael@phx-laptop.(none)>
2008-10-05 13:37:41 -0400

    Tag Phoronix Test Suite 1.4.0 Alpha 2
    The second alpha release of Orkdal!

--- pts-core/functions/pts.php
+++ pts-core/functions/pts.php
@@ -21,7 +21,7 @@
 	along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
-define("PTS_VERSION", "1.4.0a1");
+define("PTS_VERSION", "1.4.0a2");
 define("PTS_CODENAME", "ORKDAL");
 define("PTS_TYPE", "DESKTOP");
 
