d4733d697898ff1f8502a442fedcf3e7f343ead5
Michael Larabel <michael@phx-laptop.(none)>
2008-10-06 18:17:45 -0400

    pts_Graph: Set viewbox in SVG renderer

--- pts-core/objects/pts_Graph.php
+++ pts-core/objects/pts_Graph.php
@@ -460,7 +460,7 @@ class pts_Graph
 		}
 		else if($this->graph_renderer == "SVG")
 		{
-			$img = "<?xml version=\"1.0\"?>\n<!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 1.1//EN\" \"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\">\n<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" width=\"" . $width . "\" height=\"" . $height . "\">\n\n";
+			$img = "<?xml version=\"1.0\"?>\n<!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 1.1//EN\" \"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\">\n<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" viewbox=\"0 0 " . $width . " " . $height . "\" width=\"" . $width . "\" height=\"" . $height . "\">\n\n";
 		}
 
 		return $img;
