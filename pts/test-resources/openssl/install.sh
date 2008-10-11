78251b03db59b35f6dffd50772c6c1b47a8d05bd
Michael Larabel <michael@phx-laptop.(none)>
2008-10-11 19:07:30 -0400

    pts: Update openssl test profile to OpenSSL version 0.9.8i

--- pts/test-resources/openssl/install.sh
+++ pts/test-resources/openssl/install.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-tar -xvf openssl-0.9.8h.tar.gz
-cd openssl-0.9.8h/
+tar -xvf openssl-0.9.8i.tar.gz
+cd openssl-0.9.8i/
 ./config no-zlib
 make
 cd ..
 
 echo "#!/bin/sh
-cd openssl-0.9.8h/
+cd openssl-0.9.8i/
 ./apps/openssl speed rsa4096 -multi \$NUM_CPU_CORES > \$LOG_FILE 2>&1" > openssl
 chmod +x openssl
 
