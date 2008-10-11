1246dc3476bf2f194dfbf569bab991dcb73375b2
Michael Larabel <michael@phx-laptop.(none)>
2008-10-11 19:33:49 -0400

    pts: Update encode-mp3 test profile to LAME version 3.98.2

--- pts/test-resources/encode-mp3/install.sh
+++ pts/test-resources/encode-mp3/install.sh
@@ -3,16 +3,16 @@
 THIS_DIR=$(pwd)
 mkdir $THIS_DIR/lame_
 
-tar -xvf lame-398.tar.gz
-cd lame-398/
+tar -xvf lame-398-2.tar.gz
+cd lame-398-2/
 ./configure --prefix=$THIS_DIR/lame_
 make -j $NUM_CPU_JOBS
 make install
 cd ..
-rm -rf lame-398/
+rm -rf lame-398-2/
 
 echo "#!/bin/sh
 \$TIMER_START
-./lame_/bin/lame --silent -h \$TEST_EXTENDS/pts-trondheim.wav /dev/null 2>&1
+./lame_/bin/lame -h \$TEST_EXTENDS/pts-trondheim.wav /dev/null 2>&1
 \$TIMER_STOP" > lame
 chmod +x lame
