9a5284619f8366799aa49f130d9d513a48291aeb
Michael Larabel <michael@phx-laptop.(none)>
2008-10-11 20:13:58 -0400

    pts: Update sqlite test profile to SQLite version 3.6.3

--- pts/test-resources/sqlite/install.sh
+++ pts/test-resources/sqlite/install.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 
 tar -xvf pts-sqlite-tests-1.tar.gz
-tar -xvf sqlite-3.6.1.tar.gz
+tar -xvf sqlite-3.6.3.tar.gz
 
-cd sqlite-3.6.1/
+cd sqlite-3.6.3/
 ./configure
 make -j $NUM_CPU_JOBS
 cd ..
 
 echo "#!/bin/sh
-cat sqlite-2500-insertions.txt | ./sqlite-3.6.1/sqlite3 benchmark.db" > sqlite-inserts
+cat sqlite-2500-insertions.txt | ./sqlite-3.6.3/sqlite3 benchmark.db" > sqlite-inserts
 chmod +x sqlite-inserts
 
 echo "#!/bin/sh
 rm -f benchmark.db
-./sqlite-3.6.1/sqlite3 benchmark.db  \"CREATE TABLE pts1 ('I' SMALLINT NOT NULL, 'DT' TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, 'F1' VARCHAR(4) NOT NULL, 'F2' VARCHAR(16) NOT NULL, PRIMARY KEY ('I'), UNIQUE ('I'));\"
+./sqlite-3.6.3/sqlite3 benchmark.db  \"CREATE TABLE pts1 ('I' SMALLINT NOT NULL, 'DT' TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, 'F1' VARCHAR(4) NOT NULL, 'F2' VARCHAR(16) NOT NULL, PRIMARY KEY ('I'), UNIQUE ('I'));\"
 \$TIMER_START
 ./sqlite-inserts 2>&1
 \$TIMER_STOP
