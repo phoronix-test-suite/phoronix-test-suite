d2483288cd975dad68a8c480939785b46892313f
Michael Larabel <michael@phx-laptop.(none)>
2008-10-15 18:35:49 -0400

    pts: Fix compress-* test profiles with the proper 2GB file size

--- pts/test-resources/compress-lzma/pre.sh
+++ pts/test-resources/compress-lzma/pre.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 cd $1
-dd if=/dev/urandom of=compressfile bs=2048 count=131072
+dd if=/dev/urandom of=compressfile bs=2048 count=1048576
 
--- pts/test-resources/compress-pbzip2/pre.sh
+++ pts/test-resources/compress-pbzip2/pre.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 cd $1
-dd if=/dev/urandom of=compressfile bs=2048 count=524288
+dd if=/dev/urandom of=compressfile bs=2048 count=1048576
 
