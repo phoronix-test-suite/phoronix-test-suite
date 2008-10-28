f3029b21e7354eabaead58b114170c090c1fb2a0
Michael Larabel <michael@phx-laptop.(none)>
2008-10-28 09:49:16 -0400

    pts: Switch tandem-xml test profile to using $PHP_BIN instead of calling php (thus allows it to run where PHP isn't part of their path, such as Solaris)

--- pts/test-resources/tandem-xml/install.sh
+++ pts/test-resources/tandem-xml/install.sh
@@ -8,13 +8,13 @@ rm -f tmp/*.xml
 case \"\$1\" in
 \"WRITE\")
 	\$TIMER_START
-	php tandem-benchmark/tandem_benchmark.php WRITE 2>&1
+	\$PHP_BIN tandem-benchmark/tandem_benchmark.php WRITE 2>&1
 	\$TIMER_STOP
 	;;
 \"READ\")
 	php tandem-benchmark/tandem_benchmark.php WRITE
 	\$TIMER_START
-	php tandem-benchmark/tandem_benchmark.php READ 2>&1
+	\$PHP_BIN tandem-benchmark/tandem_benchmark.php READ 2>&1
 	\$TIMER_STOP
 	;;
 esac
