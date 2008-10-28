708aa15f3222f3f0b85ba0542a409fd86ea20be2
Michael Larabel <michael@phx-laptop.(none)>
2008-10-28 09:52:03 -0400

    pts: Switch pts-graph test profile to using $PHP_BIN instead of calling php (thus allows it to run where PHP isn't part of their path, such as Solaris)

--- pts/test-resources/pts-graph/install.sh
+++ pts/test-resources/pts-graph/install.sh
@@ -5,6 +5,6 @@ tar -xvf pts-graph-benchmark-1.tar.gz
 echo "#!/bin/sh
 
 \$TIMER_START
-php pts-graph-benchmark/pts_graph_benchmark.php 2>&1
+\$PHP_BIN pts-graph-benchmark/pts_graph_benchmark.php 2>&1
 \$TIMER_STOP" > pts-graph
 chmod +x pts-graph
