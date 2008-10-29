0c2d99b817d0c8095ae333f00667d5669528dfb1
Michael Larabel <michael@phx-laptop.(none)>
2008-10-29 15:12:11 -0400

    pts: Update insertion count to 12,500 for sqlite test profile

--- pts/test-resources/sqlite/install.sh
+++ pts/test-resources/sqlite/install.sh
@@ -9,12 +9,16 @@ make -j $NUM_CPU_JOBS
 cd ..
 
 echo "#!/bin/sh
+cat sqlite-2500-insertions.txt | ./sqlite-3.6.3/sqlite3 benchmark.db
+cat sqlite-2500-insertions.txt | ./sqlite-3.6.3/sqlite3 benchmark.db
+cat sqlite-2500-insertions.txt | ./sqlite-3.6.3/sqlite3 benchmark.db
+cat sqlite-2500-insertions.txt | ./sqlite-3.6.3/sqlite3 benchmark.db
 cat sqlite-2500-insertions.txt | ./sqlite-3.6.3/sqlite3 benchmark.db" > sqlite-inserts
 chmod +x sqlite-inserts
 
 echo "#!/bin/sh
 rm -f benchmark.db
-./sqlite-3.6.3/sqlite3 benchmark.db  \"CREATE TABLE pts1 ('I' SMALLINT NOT NULL, 'DT' TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, 'F1' VARCHAR(4) NOT NULL, 'F2' VARCHAR(16) NOT NULL, PRIMARY KEY ('I'), UNIQUE ('I'));\"
+./sqlite-3.6.3/sqlite3 benchmark.db  \"CREATE TABLE pts1 ('I' SMALLINT NOT NULL, 'DT' TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, 'F1' VARCHAR(4) NOT NULL, 'F2' VARCHAR(16) NOT NULL);\"
 \$TIMER_START
 ./sqlite-inserts 2>&1
 \$TIMER_STOP
