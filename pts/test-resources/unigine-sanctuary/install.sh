756a75ba235d81cadd534007973e5e2b1faf0c4b
Michael Larabel <michael@phx-laptop.(none)>
2008-10-30 13:15:29 -0400

    pts: Update unigine-tropics test profile against Tropics 1.1 from Unigine Corp pts: Update unigine-sanctuary test profile against Sanctuary 2.1 from Unigine Corp

--- pts/test-resources/unigine-sanctuary/install.sh
+++ pts/test-resources/unigine-sanctuary/install.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-tar -jxvf Unigine_Sanctuary2_Linux.tar.bz2
+tar -jxvf Unigine_Sanctuary-2.1.tar.bz2
 
 echo "#!/bin/sh
-cd Unigine_Sanctuary2_Linux/
+cd Unigine_Sanctuary-2.1/
 export LD_LIBRARY_PATH=bin/:\$LD_LIBRARY_PATH
 ./bin/main_x86 \$@ > \$LOG_FILE 2>&1
 cat \$LOG_FILE | grep FPS" > unigine-sanctuary
