110991d95ee53487bcfe9f36473a39bc5e69af4d
Michael Larabel <michael@phx-laptop.(none)>
2008-10-31 14:48:38 -0400

    pts: Add wine-humus-tests base test profile that extends wine-unstable and provides functionality for Humus.name tests

---
+++ pts/test-resources/base/wine-humus-tests/install.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+echo "#!/bin/sh
+sleep 30
+killall -9 \$1
+" > humus-kill-process
+chmod +x humus-kill-process
+
+echo "#!/bin/sh
+humus-kill-process \$1 &
+export WINEDEBUG=fps
+wine \$1 > \$LOG_FILE 2>&1" > wine-humus-run
+chmod +x wine-humus-run
