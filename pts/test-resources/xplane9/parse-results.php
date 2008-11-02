0db69cc24d2ee40d3b049b5fac7eb3b14dcafa52
Michael Larabel <michael@phx-laptop.(none)>
2008-11-02 08:45:34 -0500

    pts: Fix for xplane9 FPS detection

--- pts/test-resources/xplane9/parse-results.php
+++ pts/test-resources/xplane9/parse-results.php
@@ -3,5 +3,9 @@ $log_file = file_get_contents(getenv("LOG_FILE"));
 $BENCHMARK_RESULTS = substr($log_file, strpos($log_file, "phase 1:") + 8);
 $BENCHMARK_RESULTS = substr($BENCHMARK_RESULTS, strpos($BENCHMARK_RESULTS, "fps=") + 4);
 $BENCHMARK_RESULTS = trim(substr($BENCHMARK_RESULTS, 0, strpos($BENCHMARK_RESULTS, "\n")));
+
+if(intval($BENCHMARK_RESULTS) == 78)
+	$BENCHMARK_RESULTS = "0.00";
+
 echo $BENCHMARK_RESULTS;
 ?>
