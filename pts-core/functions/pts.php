5c64b00bf0b0ed8f1cffb80e99223d1f43337225
Michael Larabel <michael@phx-laptop.(none)>
2008-11-13 10:26:33 -0500

    pts-core: Tag Phoronix Test Suite "Orkdal" 1.4.2

--- pts-core/functions/pts.php
+++ pts-core/functions/pts.php
@@ -21,7 +21,7 @@
 	along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
-define("PTS_VERSION", "1.4.1");
+define("PTS_VERSION", "1.4.2");
 define("PTS_CODENAME", "ORKDAL");
 define("PTS_TYPE", "DESKTOP");
 
