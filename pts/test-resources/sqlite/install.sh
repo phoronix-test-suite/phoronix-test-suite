fa9755453df651d1e9e1a2853de35bfec96847f8
Michael Larabel <michael@phx-laptop.(none)>
2008-11-30 12:52:16 -0500

    pts: Update sqlite test profile to version 3.6.6.2

--- pts/test-resources/sqlite/install.sh
+++ pts/test-resources/sqlite/install.sh
@@ -1,26 +1,28 @@
 #!/bin/sh
 
 tar -xvf pts-sqlite-tests-1.tar.gz
-tar -xvf sqlite-3.6.3.tar.gz
 
-cd sqlite-3.6.3/
+tar -xvf sqlite-3.6.6.2.tar.gz
+mv sqlite-3.6.6.2/ sqlite_env/
+
+cd sqlite_env/
 ./configure
 make -j $NUM_CPU_JOBS
 cd ..
 
 echo "#!/bin/sh
-cat sqlite-2500-insertions.txt | ./sqlite-3.6.3/sqlite3 benchmark.db
-cat sqlite-2500-insertions.txt | ./sqlite-3.6.3/sqlite3 benchmark.db
-cat sqlite-2500-insertions.txt | ./sqlite-3.6.3/sqlite3 benchmark.db
-cat sqlite-2500-insertions.txt | ./sqlite-3.6.3/sqlite3 benchmark.db
-cat sqlite-2500-insertions.txt | ./sqlite-3.6.3/sqlite3 benchmark.db" > sqlite-inserts
+cat sqlite-2500-insertions.txt | ./sqlite_env/sqlite3 benchmark.db
+cat sqlite-2500-insertions.txt | ./sqlite_env/sqlite3 benchmark.db
+cat sqlite-2500-insertions.txt | ./sqlite_env/sqlite3 benchmark.db
+cat sqlite-2500-insertions.txt | ./sqlite_env/sqlite3 benchmark.db
+cat sqlite-2500-insertions.txt | ./sqlite_env/sqlite3 benchmark.db" > sqlite-inserts
 chmod +x sqlite-inserts
 
 echo "#!/bin/sh
 rm -f benchmark.db
-./sqlite-3.6.3/sqlite3 benchmark.db  \"CREATE TABLE pts1 ('I' SMALLINT NOT NULL, 'DT' TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, 'F1' VARCHAR(4) NOT NULL, 'F2' VARCHAR(16) NOT NULL);\"
+./sqlite_env/sqlite3 benchmark.db  \"CREATE TABLE pts1 ('I' SMALLINT NOT NULL, 'DT' TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, 'F1' VARCHAR(4) NOT NULL, 'F2' VARCHAR(16) NOT NULL);\"
 \$TIMER_START
 ./sqlite-inserts 2>&1
 \$TIMER_STOP
-rm -f benchmark.db" > sqlite
-chmod +x sqlite
+rm -f benchmark.db" > sqlite-benchmark
+chmod +x sqlite-benchmark
