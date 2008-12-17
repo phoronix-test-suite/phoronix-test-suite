08b1a8695b5107f21f4efbf3e8a19d56687dd99e
Michael Larabel <michael@phx-laptop.(none)>
2008-12-17 17:43:51 -0500

    Tag Phoronix Test Suite 1.6.0 "Tydal" Beta 1

--- pts-core/functions/pts.php
+++ pts-core/functions/pts.php
@@ -21,7 +21,7 @@
 	along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
-define("PTS_VERSION", "1.6.0a3");
+define("PTS_VERSION", "1.6.0b1");
 define("PTS_CODENAME", "TYDAL");
 define("PTS_TYPE", "DESKTOP");
 
