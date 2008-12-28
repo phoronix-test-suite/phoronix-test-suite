fe9921b7d2685c497702c516eed752bb6b1a5745
Michael Larabel <michael@phx-laptop.(none)>
2008-12-28 18:18:00 -0500

    pts: More graphics-magick tweaks

--- pts/test-resources/graphics-magick/parse-results.php
+++ pts/test-resources/graphics-magick/parse-results.php
@@ -1,5 +1,11 @@
 <?php
 $log_file = file_get_contents(getenv("LOG_FILE"));
 $BENCHMARK_RESULTS = substr($log_file, strrpos($log_file, "Results: ") + 9);
-echo trim(substr($BENCHMARK_RESULTS, 0, strpos($BENCHMARK_RESULTS, "iter") - 1));
+$BENCHMARK_RESULTS = trim(substr($BENCHMARK_RESULTS, 0, strpos($BENCHMARK_RESULTS, "iter") - 1));
+
+if($BENCHMARK_RESULTS < 2)
+	$BENCHMARK_RESULTS = 0;
+
+echo $BENCHMARK_RESULTS;
+
 ?>
