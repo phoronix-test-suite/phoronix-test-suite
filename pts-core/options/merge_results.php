3cb9515db66e186ab65ff575c28c92a791b4e09f
Michael Larabel <michael@phx-laptop.(none)>
2009-01-22 21:54:39 -0500

    pts-core: Allow more than two test result files to be merged at once using the merge-results option

--- pts-core/options/merge_results.php
+++ pts-core/options/merge_results.php
@@ -3,8 +3,8 @@
 /*
 	Phoronix Test Suite
 	URLs: http://www.phoronix.com, http://www.phoronix-test-suite.com/
-	Copyright (C) 2008, Phoronix Media
-	Copyright (C) 2008, Michael Larabel
+	Copyright (C) 2008 - 2009, Phoronix Media
+	Copyright (C) 2008 - 2009, Michael Larabel
 
 	This program is free software; you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
@@ -26,53 +26,36 @@ class merge_results implements pts_option_interface
 	{
 		pts_load_function_set("merge");
 
-		$BASE_FILE = $r[0];
-		$MERGE_FROM_FILE = $r[1];
-		$MERGE_TO = $r[2];
+		$r = array_map("pts_find_result_file", $r);
+		$result_files_to_merge = array();
 
-		if(empty($BASE_FILE) || empty($MERGE_FROM_FILE))
+		foreach($r as $result_file)
 		{
-			echo "\nTwo saved result profile names must be supplied.\n";
-		}
-		else
-		{
-			$BASE_FILE = pts_find_result_file($BASE_FILE);
-			$MERGE_FROM_FILE = pts_find_result_file($MERGE_FROM_FILE);
-
-			if($BASE_FILE == false || $MERGE_FROM_FILE == false)
+			if($result_file != false)
 			{
-				echo "\n" . $r[0] . " or " . $r[1] . " couldn't be found.\n";
-			}
-			else
-			{
-				if(!empty($MERGE_TO) && !is_dir(SAVE_RESULTS_DIR . $MERGE_TO))
-				{
-					$MERGE_TO .= "/composite.xml";
-				}
-				else
-				{
-					$MERGE_TO = null;
-				}
-
-				if(empty($MERGE_TO))
-				{
-					do
-					{
-						$rand_file = rand(1000, 9999);
-						$MERGE_TO = "merge-" . $rand_file . '/';
-					}
-					while(is_dir(SAVE_RESULTS_DIR . $MERGE_TO));
-
-					$MERGE_TO .= "composite.xml";
-				}
-
-				// Merge Results
-				$MERGED_RESULTS = pts_merge_test_results($BASE_FILE, $MERGE_FROM_FILE);
-				pts_save_result($MERGE_TO, $MERGED_RESULTS);
-				echo "Merged Results Saved To: " . SAVE_RESULTS_DIR . $MERGE_TO . "\n\n";
-				pts_display_web_browser(SAVE_RESULTS_DIR . $MERGE_TO);
+				array_push($result_files_to_merge, $result_file);
 			}
 		}
+
+		if(count($result_files_to_merge) < 2)
+		{
+			echo "\nAt least two saved result names must be supplied.\n";
+			return;
+		}
+
+		do
+		{
+			$rand_file = rand(1000, 9999);
+			$merge_to_file = "merge-" . $rand_file . '/';
+		}
+		while(is_dir(SAVE_RESULTS_DIR . $merge_to_file));
+		$merge_to_file .= "composite.xml";
+
+		// Merge Results
+		$merged_results = call_user_func_array("pts_merge_test_results", $result_files_to_merge);
+		pts_save_result($merge_to_file, $merged_results);
+		echo "Merged Results Saved To: " . SAVE_RESULTS_DIR . $merge_to_file . "\n\n";
+		pts_display_web_browser(SAVE_RESULTS_DIR . $merge_to_file);
 	}
 }
 
