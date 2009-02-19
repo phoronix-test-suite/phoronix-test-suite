13d254126979246913af18ce2f00739914c08842
Michael Larabel <michael@phx-laptop.(none)>
2009-02-19 15:56:44 -0500

    pts: Nexuiz test profile FPS calculation change

--- pts/test-resources/nexuiz/parse-results.php
+++ pts/test-resources/nexuiz/parse-results.php
@@ -1,7 +1,9 @@
 <?php
+
 $log_file = file_get_contents(getenv("LOG_FILE"));
-$BENCHMARK_RESULTS = trim(substr($log_file, strrpos($log_file, "min/avg/max:") + 12));
-$BENCHMARK_RESULTS = trim(substr($BENCHMARK_RESULTS, strpos($BENCHMARK_RESULTS, ' ')));
-$BENCHMARK_RESULTS = trim(substr($BENCHMARK_RESULTS, 0, strpos($BENCHMARK_RESULTS, ' ')));
+$BENCHMARK_RESULTS = substr($log_file, strrpos($log_file, "frames") + 7);
+$BENCHMARK_RESULTS = substr($BENCHMARK_RESULTS, strpos($BENCHMARK_RESULTS, "seconds") + 8);
+$BENCHMARK_RESULTS = substr($BENCHMARK_RESULTS, 0, strpos($BENCHMARK_RESULTS, ' fps'));
+
 echo $BENCHMARK_RESULTS;
 ?>
