5f0c4761cac87b4d4f11ab5b6ffe8752c57222c6
Michael Larabel <michael@phx-laptop.(none)>
2009-02-26 22:30:20 -0500

    pts: Update vdrift test profile to VDrift 2009-02-15 and make the test profile work to verified state

--- pts/test-resources/vdrift/install.sh
+++ pts/test-resources/vdrift/install.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+tar -xjf vdrift-benchmark-1.tar.bz2
 tar -xjf vdrift-2009-02-15-src.tar.bz2
 cd vdrift-2009-02-15/
 tar -xvf bullet-2.73-sp1.tgz
@@ -8,15 +9,53 @@ cd bullet-2.73/
 ./configure
 make
 cd ..
+patch -p0 < ../VDRIFT-ADD-BENCHMARK-MODE.patch
 scons
 
 # TODO: Drop in benchmark.vdr to ~/.vdrift/replays/
 # Config file at ~/.vdrift/VDrift.config
 
 cd ..
+
+mkdir -p ~/.vdrift/replays/
+mv benchmark.vdr ~/.vdrift/replays/
+
 echo "#!/bin/sh
 
-cd vdrift-2009-02-15/
-./build/vdrift -benchmark > \$LOG_FILE 2>&1" > vdrift
-chmod +x vdrift
+echo \"[ control ]
+autoclutch = true
+autotrans = true
+button_ramp = 0.000000
+mousegrab = true
+speed_sens_steering = 0.000000
 
+[ display ]
+width = \$1
+height = \$2
+FOV = 45.000000
+anisotropic = 0
+antialiasing = 0
+bloom = true
+camerabounce = 0.000000
+depth = 16
+fullscreen = true
+input_graph = false
+lighting = 0
+mph = true
+racingline = false
+reflections = 0
+shaders = true
+shadow_distance = 1
+shadow_quality = 1
+shadows = true
+show_fps = true
+show_hud = true
+skin = simple
+texture_size = large
+trackmap = true
+view_distance = 1000.000000
+zdepth = 24\" > ~/.vdrift/VDrift.config
+
+cd vdrift-2009-02-15/
+./build/vdrift \$@ > \$LOG_FILE 2>&1" > vdrift
+chmod +x vdrift
--- pts/test-resources/vdrift/parse-results.php
+++ pts/test-resources/vdrift/parse-results.php
@@ -1,7 +1,7 @@
 <?php
 
 $log_file = file_get_contents(getenv("LOG_FILE"));
-$BENCHMARK_RESULTS = substr($log_file, strrpos($log_file, "FPS:") + 4);
-echo trim(substr($BENCHMARK_RESULTS, 0, strpos($BENCHMARK_RESULTS, "\n")));
+$BENCHMARK_RESULTS = substr($log_file, strrpos($log_file, "Average frame-rate:") + 20);
+echo substr($BENCHMARK_RESULTS, 0, strpos($BENCHMARK_RESULTS, " f"));
 
 ?>
