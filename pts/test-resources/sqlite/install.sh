6a0f5396c46a7173b57951b9dee89b2de8b024d7
Michael Larabel <michael@phx-laptop.(none)>
2009-03-12 15:33:08 -0400

    pts: Allow alternate disks to be tested with sqlite test profile through auto-disk-mount-points option

--- pts/test-resources/sqlite/install.sh
+++ pts/test-resources/sqlite/install.sh
@@ -12,18 +12,26 @@ make -j $NUM_CPU_JOBS
 cd ..
 
 echo "#!/bin/sh
-cat sqlite-2500-insertions.txt | ./sqlite_env/sqlite3 benchmark.db
-cat sqlite-2500-insertions.txt | ./sqlite_env/sqlite3 benchmark.db
-cat sqlite-2500-insertions.txt | ./sqlite_env/sqlite3 benchmark.db
-cat sqlite-2500-insertions.txt | ./sqlite_env/sqlite3 benchmark.db
-cat sqlite-2500-insertions.txt | ./sqlite_env/sqlite3 benchmark.db" > sqlite-inserts
-chmod +x sqlite-inserts
 
-echo "#!/bin/sh
-rm -f benchmark.db
-./sqlite_env/sqlite3 benchmark.db  \"CREATE TABLE pts1 ('I' SMALLINT NOT NULL, 'DT' TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, 'F1' VARCHAR(4) NOT NULL, 'F2' VARCHAR(16) NOT NULL);\"
+if [ \"X\$@\" = \"X\" ]
+then
+	TEST_PATH=\`pwd\`
+else
+	TEST_PATH=\$@
+fi
+
+rm -f \$TEST_PATH/benchmark.db
+
+cat sqlite-2500-insertions.txt > \$TEST_PATH/sqlite-insertions.txt
+./sqlite_env/sqlite3 \$TEST_PATH/benchmark.db  \"CREATE TABLE pts1 ('I' SMALLINT NOT NULL, 'DT' TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, 'F1' VARCHAR(4) NOT NULL, 'F2' VARCHAR(16) NOT NULL);\"
+
 \$TIMER_START
-./sqlite-inserts 2>&1
+cat \$TEST_PATH/sqlite-insertions.txt | ./sqlite_env/sqlite3 \$TEST_PATH/benchmark.db
+cat \$TEST_PATH/sqlite-insertions.txt | ./sqlite_env/sqlite3 \$TEST_PATH/benchmark.db
+cat \$TEST_PATH/sqlite-insertions.txt | ./sqlite_env/sqlite3 \$TEST_PATH/benchmark.db
+cat \$TEST_PATH/sqlite-insertions.txt | ./sqlite_env/sqlite3 \$TEST_PATH/benchmark.db
+cat \$TEST_PATH/sqlite-insertions.txt | ./sqlite_env/sqlite3 \$TEST_PATH/benchmark.db
 \$TIMER_STOP
-rm -f benchmark.db" > sqlite-benchmark
+
+rm -f \$@/benchmark.db" > sqlite-benchmark
 chmod +x sqlite-benchmark
