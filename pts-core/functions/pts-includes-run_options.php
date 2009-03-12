7880e81ab78ce027138b0a3a647e1aab9c0d5d5e
Michael Larabel <michael@phx-laptop.(none)>
2009-03-12 11:10:39 -0400

    pts-core: Split out some pts-includes-run code into pts-includes-run_setup and pts-includes-run_options

---
+++ pts-core/functions/pts-includes-run_options.php
@@ -0,0 +1,309 @@
+<?php
+
+/*
+	Phoronix Test Suite
+	URLs: http://www.phoronix.com, http://www.phoronix-test-suite.com/
+	Copyright (C) 2008 - 2009, Phoronix Media
+	Copyright (C) 2008 - 2009, Michael Larabel
+	pts-includes-run_setup.php: Test options functions needed for running tests/suites.
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+function pts_defaults_test_options($identifier)
+{
+	// Defaults mode for single test
+	$all_args_real = array();
+	$all_args_description = array();
+	$description_separate = " - ";
+	$test_options = pts_test_options($identifier);
+
+	for($this_option_pos = 0; $this_option_pos < count($test_options); $this_option_pos++)
+	{
+		$o = $test_options[$this_option_pos];
+		$option_count = $o->option_count();
+
+		$option_args = array();
+		$option_args_description = array();
+
+		$default_entry = $o->get_option_default();
+
+		if($option_count == 2)
+		{
+			for($i = 0; $i < $option_count; $i++)
+			{
+				$this_arg = $o->get_option_prefix() . $o->get_option_value($i) . $o->get_option_postfix();
+				$this_arg_description = $o->get_name() . ": " . $o->get_option_name($i);
+
+				if(($cut_point = strpos($this_arg_description, "(")) > 1 && strpos($this_arg_description, ")") > $cut_point)
+				{
+					$this_arg_description = substr($this_arg_description, 0, $cut_point);
+				}
+
+				array_push($option_args, $this_arg);
+				array_push($option_args_description, $this_arg_description);
+			}
+		}
+		else
+		{
+			$this_arg = $o->get_option_prefix() . $o->get_option_value($default_entry) . $o->get_option_postfix();
+			$this_arg_description = $o->get_name() . ": " . $o->get_option_name($default_entry);
+
+			if(($cut_point = strpos($this_arg_description, "(")) > 1 && strpos($this_arg_description, ")") > $cut_point)
+			{
+				$this_arg_description = substr($this_arg_description, 0, $cut_point);
+			}
+			array_push($option_args, $this_arg);
+			array_push($option_args_description, $this_arg_description);
+		}
+
+		array_push($all_args_real, $option_args);
+		array_push($all_args_description, $option_args_description);
+	}
+
+	$test_args = array();
+	pts_all_combos($test_args, "", $all_args_real, 0);
+
+	$test_args_description = array();
+	pts_all_combos($test_args_description, "", $all_args_description, 0, $description_separate);
+
+	return array($test_args, $test_args_description);
+}
+function pts_generate_batch_run_options($identifier)
+{
+	// Batch mode for single test
+	$batch_all_args_real = array();
+	$batch_all_args_description = array();
+	$description_separate = " ";
+	$test_options = pts_test_options($identifier);
+
+	for($this_option_pos = 0; $this_option_pos < count($test_options); $this_option_pos++)
+	{
+		$o = $test_options[$this_option_pos];
+		$option_count = $o->option_count();
+
+		$option_args = array();
+		$option_args_description = array();
+
+		for($i = 0; $i < $option_count; $i++)
+		{
+			// A bit redundant processing, but will ensure against malformed XML problems and extra stuff added
+			$this_arg = $o->get_option_prefix() . $o->get_option_value($i) . $o->get_option_postfix();
+			$this_arg_description = $o->get_name() . ": " . $o->get_option_name($i);
+
+			if(($cut_point = strpos($this_arg_description, "(")) > 1 && strpos($this_arg_description, ")") > $cut_point)
+			{
+				$this_arg_description = substr($this_arg_description, 0, $cut_point);
+			}
+			array_push($option_args, $this_arg);
+			array_push($option_args_description, $this_arg_description);
+		}
+
+		if($i > 1)
+		{
+			$description_separate = " - ";
+		}
+
+		array_push($batch_all_args_real, $option_args);
+		array_push($batch_all_args_description, $option_args_description);
+	}
+
+	$test_args = array();
+	pts_all_combos($test_args, "", $batch_all_args_real, 0);
+
+	$test_args_description = array();
+	pts_all_combos($test_args_description, "", $batch_all_args_description, 0, $description_separate);
+
+	return array($test_args, $test_args_description);
+}
+function pts_all_combos(&$return_arr, $current_string, $options, $counter, $delimiter = " ")
+{
+	// In batch mode, find all possible combinations for test options
+	if(count($options) <= $counter)
+	{
+		array_push($return_arr, trim($current_string));
+	}
+	else
+        {
+		foreach($options[$counter] as $single_option)
+		{
+			$new_current_string = $current_string;
+
+			if(strlen($new_current_string) > 0)
+			{
+				$new_current_string .= $delimiter;
+			}
+
+			$new_current_string .= $single_option;
+
+			pts_all_combos($return_arr, $new_current_string, $options, $counter + 1, $delimiter);
+		}
+	}
+}
+function pts_auto_process_test_option($identifier, &$option_names, &$option_values)
+{
+	// Some test items have options that are dynamically built
+	switch($identifier)
+	{
+		case "auto-resolution":
+			// Base options off available screen resolutions
+			if(count($option_names) == 1 && count($option_values) == 1)
+			{
+				$available_video_modes = hw_gpu_xrandr_available_modes();
+				$format_name = $option_names[0];
+				$format_value = $option_values[0];
+				$option_names = array();
+				$option_values = array();
+
+				foreach($available_video_modes as $video_mode)
+				{
+					$this_name = str_replace("\$VIDEO_WIDTH", $video_mode[0], $format_name);
+					$this_name = str_replace("\$VIDEO_HEIGHT", $video_mode[1], $this_name);
+
+					$this_value = str_replace("\$VIDEO_WIDTH", $video_mode[0], $format_value);
+					$this_value = str_replace("\$VIDEO_HEIGHT", $video_mode[1], $this_value);
+
+					array_push($option_names, $this_name);
+					array_push($option_values, $this_value);
+				}
+			}
+			break;
+		case "auto-disk-partitions":
+			// Base options off available disk partitions
+			$all_devices = array_merge(glob("/dev/hd*"), glob("/dev/sd*"));
+			$all_devices_count = count($all_devices);
+
+			for($i = 0; $i < $all_devices_count; $i++)
+			{
+				$last_char = substr($all_devices[$i], -1);
+
+				if(!is_numeric($last_char))
+				{
+					unset($all_devices[$i]);
+				}
+			}
+
+			$option_values = array();
+			foreach($all_devices as $partition)
+			{
+				array_push($option_values, $partition);
+			}
+			$option_names = $option_values;
+			break;
+		case "auto-disks":
+			// Base options off attached disks
+			$all_devices = array_merge(glob("/dev/hd*"), glob("/dev/sd*"));
+			$all_devices_count = count($all_devices);
+
+			for($i = 0; $i < $all_devices_count; $i++)
+			{
+				$last_char = substr($all_devices[$i], -1);
+
+				if(is_numeric($last_char))
+				{
+					unset($all_devices[$i]);
+				}
+			}
+
+			$option_values = array();
+			foreach($all_devices as $disk)
+			{
+				array_push($option_values, $disk);
+			}
+			$option_names = $option_values;
+			break;
+		case "auto-removable-media":
+			$removable_media = glob("/media/*/");
+
+			for($i = 0; $i < count($removable_media); $i++)
+			{
+				if(is_dir($removable_media[$i])) // add more checks later on
+				{
+					array_push($option_names, $removable_media[$i]);
+					array_push($option_values, $removable_media[$i]);
+				}
+			}
+			break;
+		case "auto-file-select":
+			$names = $option_names;
+			$values = $option_values;
+			$option_names = array();
+			$option_values = array();
+
+			for($i = 0; $i < count($names) && $i < count($values); $i++)
+			{
+				if(is_file($values[$i]))
+				{
+					array_push($option_names, $names[$i]);
+					array_push($option_values, $values[$i]);
+				}
+			}
+			break;
+		case "auto-directory-select":
+			$names = $option_names;
+			$values = $option_values;
+			$option_names = array();
+			$option_values = array();
+
+			for($i = 0; $i < count($names) && $i < count($values); $i++)
+			{
+				if(is_dir($values[$i]))
+				{
+					array_push($option_names, $names[$i]);
+					array_push($option_values, $values[$i]);
+				}
+			}
+			break;
+	}
+}
+function pts_test_options($identifier)
+{
+	$xml_parser = new pts_test_tandem_XmlReader($identifier);
+	$settings_name = $xml_parser->getXMLArrayValues(P_TEST_OPTIONS_DISPLAYNAME);
+	$settings_argument_prefix = $xml_parser->getXMLArrayValues(P_TEST_OPTIONS_ARGPREFIX);
+	$settings_argument_postfix = $xml_parser->getXMLArrayValues(P_TEST_OPTIONS_ARGPOSTFIX);
+	$settings_identifier = $xml_parser->getXMLArrayValues(P_TEST_OPTIONS_IDENTIFIER);
+	$settings_default = $xml_parser->getXMLArrayValues(P_TEST_OPTIONS_DEFAULTENTRY);
+	$settings_menu = $xml_parser->getXMLArrayValues(P_TEST_OPTIONS_MENU_GROUP);
+
+	$test_options = array();
+
+	for($option_count = 0; $option_count < count($settings_name); $option_count++)
+	{
+		$xml_parser = new tandem_XmlReader($settings_menu[$option_count]);
+		$option_names = $xml_parser->getXMLArrayValues(S_TEST_OPTIONS_MENU_GROUP_NAME);
+		$option_values = $xml_parser->getXMLArrayValues(S_TEST_OPTIONS_MENU_GROUP_VALUE);
+		pts_auto_process_test_option($settings_identifier[$option_count], $option_names, $option_values);
+
+		$user_option = new pts_test_option($settings_identifier[$option_count], $settings_name[$option_count]);
+		$prefix = $settings_argument_prefix[$option_count];
+
+		$user_option->set_option_prefix($prefix);
+		$user_option->set_option_postfix($settings_argument_postfix[$option_count]);
+
+		for($i = 0; $i < count($option_names) && $i < count($option_values); $i++)
+		{
+			$user_option->add_option($option_names[$i], $option_values[$i]);
+		}
+
+		$user_option->set_option_default($settings_default[$option_count]);
+
+		array_push($test_options, $user_option);
+	}
+
+	return $test_options;
+}
+
+?>
---
+++ pts-core/functions/pts-includes-run_setup.php
@@ -0,0 +1,397 @@
+<?php
+
+/*
+	Phoronix Test Suite
+	URLs: http://www.phoronix.com, http://www.phoronix-test-suite.com/
+	Copyright (C) 2008 - 2009, Phoronix Media
+	Copyright (C) 2008 - 2009, Michael Larabel
+	pts-includes-run_setup.php: Setup functions needed for running tests/suites.
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+function pts_prompt_results_identifier($current_identifiers = null)
+{
+	// Prompt for a results identifier
+	$results_identifier = null;
+	$show_identifiers = array();
+
+	if(pts_read_assignment("IS_BATCH_MODE") == false || pts_batch_prompt_test_identifier())
+	{
+		if(is_array($current_identifiers) && count($current_identifiers) > 0)
+		{
+			foreach($current_identifiers as $identifier)
+			{
+				if(is_array($identifier))
+				{
+					foreach($identifier as $identifier_2)
+					{
+						array_push($show_identifiers, $identifier_2);
+					}
+				}
+				else
+				{
+					array_push($show_identifiers, $identifier);
+				}
+			}
+
+			$show_identifiers = array_unique($show_identifiers);
+
+			echo "\nCurrent Test Identifiers:\n";
+			foreach($show_identifiers as $identifier)
+			{
+				echo "- " . $identifier . "\n";
+			}
+			echo "\n";
+		}
+
+		$times_tried = 0;
+		do
+		{
+			if($times_tried == 0 && (($env_identifier = getenv("TEST_RESULTS_IDENTIFIER")) != false || 
+			($env_identifier = pts_read_assignment("AUTO_TEST_RESULTS_IDENTIFIER")) != false))
+			{
+				$results_identifier = $env_identifier;
+				echo "Test Identifier: " . $results_identifier . "\n";
+			}
+			else
+			{
+				echo "Enter a unique name for this test run: ";
+				$results_identifier = trim(str_replace(array("/"), "", fgets(STDIN)));
+			}
+			$times_tried++;
+		}
+		while(in_array($results_identifier, $show_identifiers));
+	}
+
+	if(empty($results_identifier))
+	{
+		$results_identifier = date("Y-m-d H:i");
+	}
+	else
+	{
+		$results_identifier = pts_swap_variables($results_identifier, "pts_user_runtime_variables");
+	}
+
+	pts_set_assignment_once("TEST_RESULTS_IDENTIFIER", $results_identifier);
+
+	return $results_identifier;
+}
+function pts_prompt_save_file_name($check_env = true, $to_run)
+{
+	// Prompt to save a file when running a test
+	$proposed_name = null;
+	$custom_title = null;
+
+	if($check_env != false)
+	{
+		if(!empty($check_env) || ($check_env = getenv("TEST_RESULTS_NAME")) != false)
+		{
+			$custom_title = $check_env;
+			$proposed_name = pts_input_string_to_identifier($check_env);
+			//echo "Saving Results To: " . $proposed_name . "\n";
+		}
+	}
+
+	if(pts_read_assignment("IS_BATCH_MODE") == false || pts_batch_prompt_save_name())
+	{
+		$is_reserved_word = pts_is_test($proposed_name) || pts_is_suite($proposed_name);
+
+		while(empty($proposed_name) || $is_reserved_word || !pts_validate_save_file_name($proposed_name, $to_run))
+		{
+			if($is_reserved_word)
+			{
+				echo "\n\nThe name of the saved file cannot be the same as a test/suite: " . $proposed_name . "\n";
+				$is_reserved_word = false;
+			}
+			else if(!pts_validate_save_file_name($proposed_name, $to_run))
+			{
+				echo "\n\n" . $proposed_name . " is associated with a different test/suite.\n";
+			}
+
+			echo "Enter a name to save these results: ";
+			$proposed_name = trim(fgets(STDIN));
+			$custom_title = $proposed_name;
+			$proposed_name = pts_input_string_to_identifier($proposed_name);
+
+			$is_reserved_word = pts_is_run_object($proposed_name);
+		}
+	}
+
+	if(empty($proposed_name))
+	{
+		$proposed_name = date("Y-m-d-Hi");
+	}
+	if(empty($custom_title))
+	{
+		$custom_title = $proposed_name;
+	}
+	pts_set_assignment_next("PREV_SAVE_NAME_TITLE", $custom_title);
+
+	return array($proposed_name, $custom_title);
+}
+function pts_validate_save_file_name($proposed_save_name, $to_run)
+{
+	$is_validated = true;
+
+	if(is_file(SAVE_RESULTS_DIR . $proposed_save_name . "/composite.xml") && !pts_is_assignment("AUTO_SAVE_NAME"))
+	{
+		$xml_parser = new pts_results_tandem_XmlReader($proposed_save_name);
+		$test_suite = $xml_parser->getXMLValue(P_RESULTS_SUITE_NAME);
+
+		if(!pts_is_assignment("GLOBAL_COMPARISON"))
+		{
+			if($test_suite != $to_run && !pts_is_assignment("MULTI_TYPE_RUN"))
+			{
+				$is_validated = false;
+			}
+		}
+	}
+
+	return $is_validated;
+}
+function pts_prompt_svg_result_options($svg_file)
+{
+	// Image graph result driven test selection
+	$run_options = pts_parse_svg_options($svg_file);
+	$test_to_run = null;
+
+	if(($run_option_count = count($run_options)) > 0)
+	{
+		if($run_option_count > 0)
+		{
+			if($run_option_count == 1)
+			{
+				$test_to_run = $run_options[0][0];
+			}
+			else
+			{
+				do
+				{
+					echo "\n";
+					for($i = 0; $i < $run_option_count; $i++)
+					{
+						echo ($i + 1) . ": " . $run_options[$i][1] . "\n";
+					}
+					echo "\nEnter Your Choice: ";
+
+					$run_choice = trim(fgets(STDIN));
+				}
+				while($run_choice < 1 || $run_choice > $run_option_count);
+				$test_to_run = $run_options[($run_choice - 1)][0];
+			}
+		}
+	}
+
+	return $test_to_run;
+}
+function pts_prompt_test_options($identifier)
+{
+	$xml_parser = new pts_test_tandem_XmlReader($identifier);
+	$test_title = $xml_parser->getXMLValue(P_TEST_TITLE);
+
+	$user_args = "";
+	$text_args = "";
+	$test_options = pts_test_options($identifier);
+
+	if(count($test_options) > 0)
+	{
+		echo pts_string_header("Test Configuration: " . $test_title);
+	}
+
+	if(pts_is_assignment("AUTOMATED_MODE"))
+	{
+		$preset_selections = pts_read_assignment("AUTO_TEST_OPTION_SELECTIONS");
+	}
+
+	for($this_option_pos = 0; $this_option_pos < count($test_options); $this_option_pos++)
+	{
+		$o = $test_options[$this_option_pos];
+		$option_count = $o->option_count();
+		$option_identifier = $o->get_identifier();
+
+		if($option_count == 0)
+		{
+			// User inputs their option
+			if(pts_is_assignment("AUTOMATED_MODE") && isset($preset_selections[$identifier][$option_identifier]))
+			{
+				$value = $preset_selections[$identifier][$option_identifier];
+			}
+			else
+			{
+				do
+				{
+					echo "\n" . $o->get_name() . "\n" . "Enter Value: ";
+					$value = trim(fgets(STDIN));
+				}
+				while(empty($value));
+			}
+
+			$user_args .= $o->format_option_value_from_input($value);
+		}
+		else
+		{
+			if($option_count == 1)
+			{
+				// Only one option in menu, so auto-select it
+				$bench_choice = 0;
+			}
+			else
+			{
+				// Have the user select the desired option
+				if(pts_is_assignment("AUTOMATED_MODE") && isset($preset_selections[$identifier][$option_identifier]))
+				{
+					$bench_choice = $preset_selections[$identifier][$option_identifier];
+				}
+				else
+				{
+					do
+					{
+						echo "\n" . $o->get_name() . ":\n";
+
+						$i = 1;
+						foreach($o->get_all_option_names() as $option_name)
+						{
+							echo $i . ": " . $option_name . "\n";
+							$i++;
+						}
+						echo "\nEnter Your Choice: ";
+						$bench_choice = trim(fgets(STDIN));
+					}
+					while(($bench_choice = $o->is_valid_select_choice($bench_choice)) === false);
+				}
+			}
+
+			// Format the selected option
+			$text_args .= $o->format_option_display_from_select($bench_choice);
+
+			if($this_option_pos < (count($test_options) - 1))
+			{
+				$text_args .= " - ";
+			}
+
+			$user_args .= $o->format_option_value_from_select($bench_choice) . " ";
+		}
+	}
+
+	return array($user_args, $text_args);
+}
+function pts_promt_user_tags($default_tags = "")
+{
+	$tags_input = "";
+
+	if(pts_read_assignment("IS_BATCH_MODE") == false && pts_read_assignment("AUTOMATED_MODE") == false)
+	{
+		echo "\nTags are optional and used on Phoronix Global for making it easy to share, search, and organize test results. Example tags could be the type of test performed (i.e. WINE tests) or the hardware used (i.e. Dual Core SMP).\n\nEnter the tags you wish to provide (separated by commas): ";
+		$tags_input .= fgets(STDIN);
+
+		if(function_exists("preg_replace"))
+		{
+			$tags_input = preg_replace("/[^a-zA-Z0-9s, -]/", "", $tags_input);
+		}
+		else
+		{
+			$tags_input = pts_remove_chars($tags_input, true, true, true);
+		}
+
+		$tags_input = trim($tags_input);
+	}
+
+	if(empty($tags_input))
+	{
+		if(!is_array($default_tags) && !empty($default_tags))
+		{
+			$default_tags = array($default_tags);
+		}
+
+		$tags_input = pts_global_auto_tags($default_tags);
+	}
+
+	return $tags_input;
+}
+function pts_input_string_to_identifier($input)
+{
+	$input = pts_swap_variables($input, "pts_user_runtime_variables");
+	$input = trim(str_replace(array(' ', '/', '&', '\''), "", strtolower($input)));
+
+	return $input;
+}
+function pts_global_auto_tags($extra_attr = null)
+{
+	// Generate automatic tags for the system, used for Phoronix Global
+	$tags_array = array();
+
+	if(!empty($extra_attr) && is_array($extra_attr))
+	{
+		foreach($extra_attr as $attribute)
+		{
+			array_push($tags_array, $attribute);
+		}
+	}
+
+	switch(hw_cpu_core_count())
+	{
+		case 1:
+			array_push($tags_array, "Single Core");
+			break;
+		case 2:
+			array_push($tags_array, "Dual Core");
+			break;
+		case 4:
+			array_push($tags_array, "Quad Core");
+			break;
+		case 8:
+			array_push($tags_array, "Octal Core");
+			break;
+	}
+
+	$cpu_type = hw_cpu_string();
+	if(strpos($cpu_type, "Intel") !== false)
+	{
+		array_push($tags_array, "Intel");
+	}
+	else if(strpos($cpu_type, "AMD") !== false)
+	{
+		array_push($tags_array, "AMD");
+	}
+	else if(strpos($cpu_type, "VIA") !== false)
+	{
+		array_push($tags_array, "VIA");
+	}
+
+	$gpu_type = hw_gpu_string();
+	if(strpos($cpu_type, "ATI") !== false)
+	{
+		array_push($tags_array, "ATI");
+	}
+	else if(strpos($cpu_type, "NVIDIA") !== false)
+	{
+		array_push($tags_array, "NVIDIA");
+	}
+
+	if(sw_os_architecture() == "x86_64" && IS_LINUX)
+	{
+		array_push($tags_array, "64-bit Linux");
+	}
+
+	$os = sw_os_release();
+	if($os != "Unknown")
+	{
+		array_push($tags_array, $os);
+	}
+
+	return implode(", ", $tags_array);
+}
+
+?>
