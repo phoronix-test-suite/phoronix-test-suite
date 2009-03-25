dd34022594e26aac32b7c85670fed57b8ed88e50
Michael Larabel <michael@phx-laptop.(none)>
2009-03-24 20:41:21 -0400

    pts: Upgrade compress-7zip test profile to p7zip 4.65

--- pts/test-resources/compress-7zip/install.sh
+++ pts/test-resources/compress-7zip/install.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-tar -xjf p7zip_4.58_src_all.tar.bz2
-cd p7zip_4.58/
+tar -xjf p7zip_4.65_src_all.tar.bz2
+cd p7zip_4.65/
 make -j $NUM_CPU_JOBS
 cd ..
 
 echo "#!/bin/sh
-./p7zip_4.58/bin/7za b > \$LOG_FILE 2>&1" > compress-7zip
+./p7zip_4.65/bin/7za b > \$LOG_FILE 2>&1" > compress-7zip
 chmod +x compress-7zip
