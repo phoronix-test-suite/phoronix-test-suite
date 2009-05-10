8d4ead11d4dd0c3ad8d195c214aeb259aa218536
Michael Larabel <michael@mubuntu.(none)>
2009-05-10 15:40:22 -0400

    Tag Phoronix Test Suite 2.0.0 "Sandtorg" Alpha 1

--- pts-core/library/pts.php
+++ pts-core/library/pts.php
@@ -21,7 +21,7 @@
 	along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
-define("PTS_VERSION", "2.0.0a0");
+define("PTS_VERSION", "2.0.0a1");
 define("PTS_CODENAME", "SANDTORG");
 
 function pts_codename($full_string = false)
