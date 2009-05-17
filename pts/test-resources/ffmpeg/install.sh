77744e0de93858028ffd0d2263acfb2163282f8e
Michael Larabel <michael@mubuntu.(none)>
2009-05-17 14:21:32 -0400

    pts: Support using multiple threads for FFmpeg test profile

--- pts/test-resources/ffmpeg/install.sh
+++ pts/test-resources/ffmpeg/install.sh
@@ -10,6 +10,6 @@ cd ..
 echo "#!/bin/sh
 
 \$TIMER_START
-./ffmpeg-0.5/ffmpeg -i \$TEST_EXTENDS/pts-trondheim.avi -y -target ntsc-vcd /dev/null 2>&1
+./ffmpeg-0.5/ffmpeg -i \$TEST_EXTENDS/pts-trondheim.avi -threads \$NUM_CPU_CORES -y -target ntsc-vcd /dev/null 2>&1
 \$TIMER_STOP" > ffmpeg
 chmod +x ffmpeg
