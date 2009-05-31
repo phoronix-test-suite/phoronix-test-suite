4c79daaddfb9e382c7bbb1268312898df12148d4
Michael Larabel <michael@mubuntu.(none)>
2009-05-31 17:55:56 -0400

    pts: Update mafft test profile to version 6.706

--- pts/test-resources/mafft/install.sh
+++ pts/test-resources/mafft/install.sh
@@ -1,26 +1,25 @@
 #!/bin/sh
 
-mkdir -p $HOME/mafft_/install
-tar -xvf mafft-6.240-src.tgz
-cd mafft-6.240/src/
+rm -rf $HOME/mafft_
+mkdir $HOME/mafft_
+tar -xvf mafft-6.706-without-extensions-src.tgz
+cd mafft-6.706-without-extensions/core/
 
 make clean
 
-sed -i -e "s|PREFIX = /usr/local/lib/mafft|PREFIX = $HOME/mafft_/install|g" Makefile
+sed -i -e "s|PREFIX = /usr/local|PREFIX = $HOME/mafft_|g" Makefile
 
 make -j $NUM_CPU_JOBS
 echo $? > ~/install-exit-status
 make install
 cd ~/
-cp -f mafft-6.240/scripts/mafft mafft_/install/
-rm -rf mafft-6.240/
-rm -rf bin/
+cp -f mafft-6.706-without-extensions/scripts/mafft mafft_/
 
-bunzip2 pyruvate_decarboxylase.fasta.bz2 -c > mafft_/install/pyruvate_decarboxylase.fasta
+bunzip2 pyruvate_decarboxylase.fasta.bz2 -c > mafft_/pyruvate_decarboxylase.fasta
 
 cat>align<<EOT
 #!/bin/sh
-cd mafft_/install/
+cd mafft_/
 ./mafft --localpair --maxiterate 10000 pyruvate_decarboxylase.fasta 1>/dev/null 2>&1
 cd ..
 EOT
