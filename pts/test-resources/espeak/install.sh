588f265fffd06e2e1b68ef10bb87fafff9315010
Michael Larabel <michael@mubuntu.(none)>
2009-05-30 20:39:21 -0400

    pts: Update espeak test profile to eSpeak version 1.40.02

--- pts/test-resources/espeak/install.sh
+++ pts/test-resources/espeak/install.sh
@@ -1,19 +1,20 @@
 #!/bin/sh
 
 tar -xvf gutenberg-science.tar.gz
-THIS_DIR=$(pwd)
-unzip -o espeak-1.39-source.zip
-cd espeak-1.39-source/src/
-sed -i -e "s|/usr|$THIS_DIR/espeak_|g" Makefile
-sed -i -e "s|/usr|$THIS_DIR/espeak_|g" speech.h
+unzip -o espeak-1.40.02-source.zip
+cd espeak-1.40.02-source/src/
+sed -i -e "s|/usr|$HOME/espeak_|g" Makefile
+sed -i -e "s|/usr|$HOME/espeak_|g" speech.h
 make -j $NUM_CPU_JOBS
+echo $? > ~/install-exit-status
 make install
 cd ../..
-rm -rf espeak-1.39-source/
+rm -rf espeak-1.40.02-source/
 
 echo "#!/bin/sh
 cd espeak_/bin/
 \$TIMER_START
 LD_LIBRARY_PATH=$THIS_DIR/espeak_/lib/:\$LD_LIBRARY_PATH ./espeak -f ~/gutenberg-science.txt -w /dev/null 2>&1
+echo \$? > ~/test-exit-status
 \$TIMER_STOP" > espeak
 chmod +x espeak
