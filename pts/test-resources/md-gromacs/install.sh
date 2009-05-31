636c337936c6d041909207c1c5527a9d75edf356
Michael Larabel <michael@mubuntu.(none)>
2009-05-31 17:11:52 -0400

    pts: Update md-gromacs test profile to version 4.0.5

--- pts/test-resources/md-gromacs/install.sh
+++ pts/test-resources/md-gromacs/install.sh
@@ -2,50 +2,43 @@
 
 rm -rf $HOME/fftw_
 rm -rf $HOME/mpich2_
-rm -rf $HOME/gromacs333_
+rm -rf $HOME/gromacs40_
 mkdir $HOME/fftw_
 mkdir $HOME/mpich2_
-mkdir $HOME/gromacs333_
+mkdir $HOME/gromacs40_
 
-tar -xvf fftw-3.1.2.tar.gz
-cd fftw-3.1.2/
+tar -xvf fftw-3.2.1.tar.gz
+cd fftw-3.2.1/
 ./configure --prefix=$HOME/fftw_ --enable-float --enable-sse --enable-threads
 make -j $NUM_CPU_JOBS
 make install
 make distclean
-./configure --prefix=$HOME/fftw_ --enable-sse2 --enable-threads --enable-type-prefix
+./configure --prefix=$HOME/fftw_ --enable-sse2 --enable-threads
 make -j $NUM_CPU_JOBS
 make install
 cd ..
-rm -rf fftw-3.1.2/
+rm -rf fftw-3.2.1/
 
-tar -xvf mpich2-1.0.7.tar.gz
-cd mpich2-1.0.7/
-./configure --prefix=$HOME/mpich2_ --enable-fast=all --with-pm=gforker
+tar -xvf mpich2-1.0.8p1.tar.gz
+cd mpich2-1.0.8p1/
+./configure --prefix=$HOME/mpich2_ --enable-fast=all --with-pm=gforker --disable-option-checking
 make
 make install
 cd ..
-rm -rf mpich2-1.0.7/
+rm -rf mpich2-1.0.8p1/
 
-tar -xvf gromacs-3.3.3.tar.gz
-cd gromacs-3.3.3/
-./configure --prefix=$HOME/gromacs333_ --enable-mpi --program-suffix="_SSE_MPI" CPPFLAGS=-I$HOME/fftw_/include LDFLAGS=-L$HOME/fftw_/lib PATH=$HOME/mpich2_/bin/:$PATH
+tar -xvf gromacs-4.0.5.tar.gz
+cd gromacs-4.0.5/
+./configure --prefix=$HOME/gromacs40_ --enable-mpi --program-suffix="_SSE_MPI" CPPFLAGS=-I$HOME/fftw_/include LDFLAGS=-L$HOME/fftw_/lib PATH=$HOME/mpich2_/bin/:$PATH
 make -j $NUM_CPU_JOBS PATH=$HOME/mpich2_/bin/:$PATH
 make install
 make clean
-./configure --prefix=$HOME/gromacs333_ CPPFLAGS=-I$HOME/fftw_/include LDFLAGS=-L$HOME/fftw_/lib --program-suffix="_SSE"
-make -j $NUM_CPU_JOBS
-make install
-make clean
-./configure --prefix=$HOME/gromacs333_ --enable-mpi --disable-float --program-suffix="_SSE2_MPI" CPPFLAGS=-I$HOME/fftw_/include LDFLAGS=-L$HOME/fftw_/lib PATH=$HOME/mpich2_/bin/:$PATH
+./configure --prefix=$HOME/gromacs40_ --enable-mpi --disable-float --program-suffix="_SSE2_MPI" CPPFLAGS=-I$HOME/fftw_/include LDFLAGS=-L$HOME/fftw_/lib PATH=$HOME/mpich2_/bin/:$PATH
 make -j $NUM_CPU_JOBS PATH=$HOME/mpich2_/bin/:$PATH
 make install
 make clean
-./configure --prefix=$HOME/gromacs333_ CPPFLAGS=-I$HOME/fftw_/include LDFLAGS=-L$HOME/fftw_/lib --disable-float --program-suffix="_SSE2"
-make -j $NUM_CPU_JOBS
-make install
 cd ..
-rm -rf gromacs-3.3.3/
+rm -rf gromacs-4.0.5/
 
 echo "#!/bin/sh
 if [ -d \$HOME/gmxbench ]
@@ -92,19 +85,15 @@ esac
 
 case \"\$2\" in
 \"mpi\")
-        \$HOME/gromacs333_/bin/grompp_\$PRECISION\_MPI -np \$NUM_CPU_CORES -nov 1>/dev/null 2>&1
-        \$HOME/mpich2_/bin/mpiexec -np \$NUM_CPU_CORES \$HOME/gromacs333_/bin/mdrun_\$PRECISION\_MPI 1>\$HOME/flopcount 2>&1
+	\$HOME/mpich2_/bin/mpiexec -np \$NUM_CPU_CORES \$HOME/gromacs40_/bin/mdrun_\$PRECISION\_MPI 1>\$HOME/flopcount 2>&1
 	;;
 \"single-node\")
-        \$HOME/gromacs333_/bin/grompp_\$PRECISION -nov 1>/dev/null 2>&1
-        \$HOME/gromacs333_/bin/mdrun_\$PRECISION 1>\$HOME/flopcount 2>&1
+	\$HOME/gromacs40_/bin/mdrun_\$PRECISION\_MPI -npme 1 1>\$HOME/flopcount 2>&1
 	;;
 *)
 	exit
 	;;
 esac
 
-grep -C 1 'Performance:' \$HOME/flopcount
-
-cd \$HOME/" > gromacs
+grep -C 1 'Performance:' \$HOME/flopcount" > gromacs
 chmod +x gromacs
