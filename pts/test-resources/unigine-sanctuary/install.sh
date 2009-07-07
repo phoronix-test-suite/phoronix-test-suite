049c9e21a95a9810409482213f1da12b744d18fb
Michael Larabel <michael@mubuntu.(none)>
2009-07-07 11:15:35 -0400

    pts: Update unigine-sanctuary test profile to Unigine Sanctuary 2.2

--- pts/test-resources/unigine-sanctuary/install.sh
+++ pts/test-resources/unigine-sanctuary/install.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
-tar -jxvf Unigine_Sanctuary-2.1.tar.bz2
+chmod +x Unigine_Sanctuary-2.2.run
+./Unigine_Sanctuary-2.2.run
 
 echo "#!/bin/sh
-cd Unigine_Sanctuary-2.1/
+cd sanctuary/
 export LD_LIBRARY_PATH=bin/:\$LD_LIBRARY_PATH
-./bin/main_x86 \$@ > \$LOG_FILE 2>&1
+./bin/Sanctuary \$@ > \$LOG_FILE 2>&1
 cat \$LOG_FILE | grep FPS" > unigine-sanctuary
 chmod +x unigine-sanctuary
 
