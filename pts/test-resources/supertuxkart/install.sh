463da0a1f61dc9241686ea3a20d74af39c5cee63
Michael Larabel <michael@mubuntu.(none)>
2009-07-20 21:06:23 -0400

    pts: Add supertuxkart test profile for SuperTuxKart racing game

---
+++ pts/test-resources/supertuxkart/install.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+tar -jxf supertuxkart-0.6-linuxi486.tar.bz2
+
+mkdir .supertuxkart/
+tar -xvf supertuxkart-1.tar.gz
+mv config .supertuxkart/
+
+echo "#!/bin/sh
+cd supertuxkart-0.6-linuxi486/
+LD_LIBRARY_PATH=./bin/: bin/supertuxkart \$@ > \$LOG_FILE 2>&1
+cat \$LOG_FILE | grep fps" > supertuxkart
+chmod +x supertuxkart
+
---
+++ pts/test-resources/supertuxkart/parse-results.php
@@ -0,0 +1,6 @@
+<?php
+$log_file = file_get_contents(getenv("LOG_FILE"));
+$BENCHMARK_RESULTS = substr($log_file, strrpos($log_file, "FPS:") + 5);
+$BENCHMARK_RESULTS = trim(substr($BENCHMARK_RESULTS, 0, strpos($BENCHMARK_RESULTS, '\n')));
+echo $BENCHMARK_RESULTS;
+?>
