c79e42ddfe6c2608acebe88e2f6ba75b1092e1e3
Michael Larabel <michael@mubuntu.(none)>
2009-08-29 15:33:05 -0500

    pts: Update warsow test profile against Warsow 0.5

--- pts/test-resources/warsow/install.sh
+++ pts/test-resources/warsow/install.sh
@@ -1,21 +1,19 @@
 #!/bin/sh
 
-tar -xvf pts-warsow-1.tar.gz
+tar -xvf pts-warsow-2.tar.gz
 
 case $OS_TYPE in
 	"MacOSX" )
-		unzip -o warsow_0.42_mac.zip
-		cp -f pts-warsow-04.wd10 Warsow.app/Contents/Resources/Warsow/basewsw/demos
+		unzip -o warsow_0.5_mac_intel.zip
+		# TODO: fixup Mac support for Warsow 0.5
+		cp -f pts-bardu.wd11 Warsow.app/Contents/Resources/Warsow/basewsw/demos
 		cp -f pts-warsow.cfg Warsow.app/Contents/Resources/Warsow/basewsw
 	;;
 	"Linux" )
-		unzip -o warsow_0.42_unified.zip
-		cp -f pts-warsow-04.wd10 warsow_0.42_unified/basewsw/demos
-		cp -f pts-warsow.cfg warsow_0.42_unified/basewsw/
-		cd warsow_0.42_unified/
-		chmod +x warsow.x86_64
-		chmod +x warsow.i386
-		cd ..
+		unzip -o warsow_0.5_unified.zip
+		mkdir -p basewsw/demos
+		cp -f pts-bardu.wd11 basewsw/demos
+		cp -f pts-warsow.cfg basewsw/
 	;;
 esac
 
@@ -27,7 +25,6 @@ case \$OS_TYPE in
 		./Warsow.app/Contents/MacOS/Warsow \$@ > \$LOG_FILE 2>&1 # TODO: the Mac OS X binary doesn't seem to listen to command line arguments that are passed
 	;;
 	\"Linux\" )
-		cd warsow_0.42_unified/
 		if [ \$OS_ARCH = \"x86_64\" ]
 		then
 			./warsow.x86_64 \$@ > \$LOG_FILE 2>&1
