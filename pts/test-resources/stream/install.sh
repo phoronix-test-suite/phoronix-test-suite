c634e902b324d86f49c383910832aaad82ce4e8b
Michael Larabel <michael@mubuntu.(none)>
2009-08-30 13:29:36 -0500

    pts: Use OpenMP support in stream test profile

--- pts/test-resources/stream/install.sh
+++ pts/test-resources/stream/install.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 tar -xvf stream-2009-04-11.tar.gz
-cc stream.c -o stream-bin
+cc stream.c -O2 -fopenmp -o stream-bin
 echo \$? > ~/test-exit-status
 
 echo "#!/bin/sh
+export OMP_NUM_THREADS=\$NUM_CPU_CORES
 ./stream-bin > \$LOG_FILE 2>&1
 echo \$? > ~/test-exit-status" > stream
 chmod +x stream
