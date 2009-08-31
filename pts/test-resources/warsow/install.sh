a185cd9b9d8bbbc3fb4fd37c08f5be19b757c5c7
Michael Larabel <michael@mubuntu.(none)>
2009-08-30 21:27:08 -0500

    pts: warsow test profile should be all set

--- pts/test-resources/warsow/install.sh
+++ pts/test-resources/warsow/install.sh
@@ -5,9 +5,9 @@ tar -xvf pts-warsow-2.tar.gz
 case $OS_TYPE in
 	"MacOSX" )
 		unzip -o warsow_0.5_mac_intel.zip
-		mkdir -p $DEBUG_REAL_HOME/Library/Application\ Support/Warsow-0.5/basewsw/demos
-		cp -f pts-bardu.wd11 $DEBUG_REAL_HOME/Library/Application\ Support/Warsow-0.5/basewsw/demos/
-		cp -f pts-warsow.cfg $DEBUG_REAL_HOME/Library/Application\ Support/Warsow-0.5/basewsw/
+		mkdir -p Library/Application\ Support/Warsow-0.5/basewsw/demos
+		cp -f pts-bardu.wd11 Library/Application\ Support/Warsow-0.5/basewsw/demos/
+		cp -f pts-warsow.cfg Library/Application\ Support/Warsow-0.5/basewsw/
 	;;
 	"Linux" )
 		unzip -o warsow_0.5_unified.zip
@@ -22,7 +22,8 @@ rm -f .warsow/basewsw/1.log
 
 case \$OS_TYPE in
 	\"MacOSX\" )
-		/Volumes/Warsow\ 0.5/Warsow\ SDL.app/Contents/MacOS/Warsow\ SDL \$@ > \$LOG_FILE 2>&1
+		/Volumes/Warsow\ 0.5/Warsow\ SDL.app/Contents/MacOS/Warsow\ SDL \$@
+		cat Library/Application\ Support/Warsow-0.5/basewsw/pts-log.log > \$LOG_FILE
 	;;
 	\"Linux\" )
 		if [ \$OS_ARCH = \"x86_64\" ]
@@ -31,6 +32,7 @@ case \$OS_TYPE in
 		else
 			./warsow.i386 \$@ > \$LOG_FILE 2>&1
 		fi
+		cat ~/.warsow/basewsw/pts-log.log > \$LOG_FILE
 	;;
 esac" > warsow
 chmod +x warsow
