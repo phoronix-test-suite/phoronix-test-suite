954dff47bed9a4fee89ea46c2b70b61afb71e645
Michael Larabel <michael@mubuntu.(none)>
2009-09-25 20:41:26 -0500

    pts: Update minion test profile against Minion 0.9

--- pts/test-resources/minion/install.sh
+++ pts/test-resources/minion/install.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
-rm -rf minion-0.8.1/
-tar -xvf minion-0.8.1-src.tar.gz
-cd minion-0.8.1/
+rm -rf minion-0.9/
+tar -xvf minion-0.9-src.tar.gz
+cd minion-0.9/
 mkdir build
 cd build/
 cmake ..
 make minion -j $NUM_CPU_JOBS
 echo $? > ~/install-exit-status
 cd ../..
-rm -rf minion-0.8.1/build/CMakeFiles/
+rm -rf minion-0.9/build/CMakeFiles/
 
 echo "#!/bin/sh
-cd minion-0.8.1/
+cd minion-0.9/
 ./build/minion \$@ > \$LOG_FILE 2>&1
 echo \$? > ~/test-exit-status" > minion
 chmod +x minion
