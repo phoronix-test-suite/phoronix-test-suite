9dd23c96aefd2071ca11ed6c8b47adcd095a0144
Michael Larabel <michael@mubuntu.(none)>
2009-09-27 21:29:36 -0500

    pts-core: Add in two new files

---
+++ pts-core/library/pts-includes-comparisons.php
@@ -0,0 +1,106 @@
+<?php
+
+/*
+	Phoronix Test Suite
+	URLs: http://www.phoronix.com, http://www.phoronix-test-suite.com/
+	Copyright (C) 2009, Phoronix Media
+	Copyright (C) 2009, Michael Larabel
+	pts-includes-comparisons.php: Functions needed for performing reference comparisons
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+function pts_result_file_reference_tests($result)
+{
+	$result_file = new pts_result_file($result);
+	$result_test = $result_file->get_suite_name();
+	$result_identifiers = $result_file->get_system_identifiers();
+	$test_result_hashes = array();
+	$reference_tests = array();
+
+	foreach($result_file->get_result_objects() as $result_object)
+	{
+		array_push($test_result_hashes, $result_object->get_comparison_hash());
+	}
+
+	if(pts_is_suite($result_test))
+	{
+		$reference_systems_xml = pts_suite_read_xml($result_test, P_SUITE_REFERENCE_SYSTEMS);
+	}
+	else if(pts_is_test($result_test))
+	{
+		$reference_systems_xml = pts_test_read_xml($result_test, P_TEST_REFERENCE_SYSTEMS);
+	}
+	else
+	{
+		$reference_systems_xml = null;
+	}
+
+	$reference_ids = pts_trim_explode(",", $reference_systems_xml);
+	$reference_file_ids = pts_generic_reference_system_comparison_ids();
+
+	foreach(array_merge($reference_ids, $reference_file_ids) as $global_id)
+	{
+		if(pts_is_global_id($global_id))
+		{
+			if(!pts_is_test_result($global_id))
+			{
+				pts_clone_from_global($global_id, false);
+			}
+
+			$global_result_file = new pts_result_file($global_id);
+			$global_result_hashes = array();
+
+			$global_result_identifiers = $global_result_file->get_system_identifiers();
+
+			foreach($global_result_identifiers as $index => $identifier_check)
+			{
+				if(in_array($identifier_check, $result_identifiers))
+				{
+					unset($global_result_identifiers[$index]);
+				}
+			}
+
+			if(count($global_result_identifiers) > 0)
+			{
+				$hash_failed = false;
+
+				foreach($global_result_file->get_result_objects() as $result_object)
+				{
+					array_push($global_result_hashes, $result_object->get_comparison_hash());
+				}
+				foreach($test_result_hashes as $hash)
+				{
+					if(!in_array($hash, $global_result_hashes))
+					{
+						$hash_failed = true;
+						break;
+					}
+				}
+
+				if(!$hash_failed)
+				{
+					foreach($global_result_identifiers as $system_identifier)
+					{
+						array_push($reference_tests, new pts_result_merge_select($global_id, $system_identifier));
+					}
+				}
+			}
+		}
+	}
+
+	return $reference_tests;
+}
+
+?>
---
+++ pts-core/library/pts-includes-execution.php
@@ -0,0 +1,65 @@
+<?php
+
+/*
+	Phoronix Test Suite
+	URLs: http://www.phoronix.com, http://www.phoronix-test-suite.com/
+	Copyright (C) 2009, Phoronix Media
+	Copyright (C) 2009, Michael Larabel
+	pts-includes-execution.php: Functions needed for execution during only the test installation and run processes
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+function pts_call_test_script($test_identifier, $script_name, $print_string = "", $pass_argument = "", $extra_vars = null, $use_ctp = true)
+{
+	$result = null;
+	$test_directory = TEST_ENV_DIR . $test_identifier . "/";
+
+	$tests_r = ($use_ctp ? pts_contained_tests($test_identifier, true) : array($test_identifier));
+
+	foreach($tests_r as $this_test)
+	{
+		if(is_file(($run_file = pts_location_test_resources($this_test) . $script_name . ".php")) || is_file(($run_file = pts_location_test_resources($this_test) . $script_name . ".sh")))
+		{
+			$file_extension = substr($run_file, (strrpos($run_file, ".") + 1));
+
+			if(!empty($print_string))
+			{
+				echo $print_string;
+			}
+
+			if($file_extension == "php")
+			{
+				$this_result = pts_exec("cd " .  $test_directory . " && " . PHP_BIN . " " . $run_file . " \"" . $pass_argument . "\" 2>&1", $extra_vars);
+			}
+			else if($file_extension == "sh")
+			{
+				$this_result = pts_exec("cd " .  $test_directory . " && sh " . $run_file . " \"" . $pass_argument . "\" 2>&1", $extra_vars);
+			}
+			else
+			{
+				$this_result = null;
+			}
+
+			if(trim($this_result) != "")
+			{
+				$result = $this_result;
+			}
+		}
+	}
+
+	return $result;
+}
+
+?>
