8824ee07ef1953b497183b6cae79ad8b7ebc5208
Michael Larabel <michael@mubuntu.(none)>
2009-10-12 20:37:58 -0500

    pts-core: Support exporting some functions from pts-core to the parse-results process during test runs

--- pts-core/library/pts-includes-execution.php
+++ pts-core/library/pts-includes-execution.php
@@ -41,6 +41,12 @@ function pts_call_test_script($test_identifier, $script_name, $print_string = ""
 
 			if($file_extension == "php")
 			{
+				if($script_name == "parse-results")
+				{
+					$extra_vars["PARSE_RESULTS_SCRIPT"] = $run_file;
+					$run_file = TEST_LIBRARIES_DIR . "results-parsing-functions.php";
+				}
+
 				$this_result = pts_exec("cd " .  $test_directory . " && " . PHP_BIN . " " . $run_file . " \"" . $pass_argument . "\" 2>&1", $extra_vars);
 			}
 			else if($file_extension == "sh")
---
+++ pts-core/test-libraries/results-parsing-functions.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+	Phoronix Test Suite
+	URLs: http://www.phoronix.com, http://www.phoronix-test-suite.com/
+	Copyright (C) 2008 - 2009, Phoronix Media
+	Copyright (C) 2008 - 2009, Michael Larabel
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+function pts_report_numeric_result($result)
+{
+	// For now it's just a matter of printing the result
+	echo is_numeric($result) ? $result : null;
+}
+
+include(getenv("PARSE_RESULTS_SCRIPT"));
+
+?>
