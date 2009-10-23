82c368692ab52484477bcfca181dd74b0678c54a
Michael Larabel <michael@mubuntu.(none)>
2009-10-23 16:45:21 -0500

    pts: Update graphics-magick test profile against GraphicsMagick 1.3.7

--- pts/test-resources/graphics-magick/install.sh
+++ pts/test-resources/graphics-magick/install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 tar -xvf libpng-1.2.39.tar.gz
-tar -xjf GraphicsMagick-1.3.6.tar.bz2
+tar -xjf GraphicsMagick-1.3.7.tar.bz2
 
 mkdir $HOME/gm_
 
@@ -11,13 +11,13 @@ make -j $NUM_CPU_JOBS
 make install
 cd ..
 
-cd GraphicsMagick-1.3.6/
+cd GraphicsMagick-1.3.7/
 LDFLAGS="-L$HOME/gm_/lib" CPPFLAGS="-I$HOME/gm_/include" ./configure --without-perl --prefix=$HOME/gm_ --with-png=yes > /dev/null
 make -j $NUM_CPU_JOBS
 echo $? > ~/install-exit-status
 make install
 cd ..
-rm -rf GraphicsMagick-1.3.6/
+rm -rf GraphicsMagick-1.3.7/
 rm -rf libpng-1.2.39/
 rm -rf gm_/share/doc/GraphicsMagick/
 rm -rf gm_/share/man/
