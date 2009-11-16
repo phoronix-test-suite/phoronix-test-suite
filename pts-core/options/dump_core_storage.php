171cef1dc8fa681a8402e4e012a167a0ec02dd46
Michael Larabel <michael@phxmichael.hsd1.il.comcast.net>
2009-11-16 09:38:45 -0600

    pts-core: Add dump-core-storage option for dumping the contents of the core.pt2so object

---
+++ pts-core/options/dump_core_storage.php
@@ -0,0 +1,58 @@
+<?php
+
+/*
+	Phoronix Test Suite
+	URLs: http://www.phoronix.com, http://www.phoronix-test-suite.com/
+	Copyright (C) 2009, Phoronix Media
+	Copyright (C) 2009, Michael Larabel
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+class dump_core_storage implements pts_option_interface
+{
+	public static function run($r)
+	{
+		echo pts_string_header("Phoronix Test Suite Core Storage");
+		$pso = pts_storage_object::recover_from_file(PTS_CORE_STORAGE);
+		$phodevi_sc = array();
+
+		foreach($pso->get_objects() as $pso_index => $pso_object)
+		{
+			if($pso_index != "phodevi_smart_cache")
+			{
+				echo $pso_index . ": ";
+
+				if(is_array($pso_object))
+				{
+					foreach($pso_object as $key => $element)
+					{
+						echo "\n\t" . $key . ": " . $element;
+					}
+					echo "\n";
+				}
+				else
+				{
+					echo $pso_object;
+				}
+
+				echo "\n";
+			}
+		}
+
+		echo "\n";
+	}
+}
+
+?>
