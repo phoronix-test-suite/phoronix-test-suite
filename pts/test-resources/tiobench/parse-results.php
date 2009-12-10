bc429dab3befbb903d15c5be022089daf8be5fa2
Michael Larabel <michael@phxmichael.hsd1.il.comcast.net>
2009-12-10 09:27:25 -0600

    pts: Overhaul the tiobench test profile

--- pts/test-resources/tiobench/parse-results.php
+++ pts/test-resources/tiobench/parse-results.php
@@ -1,13 +1,9 @@
 <?php
 
 $log_file = pts_read_log_file();
-$log_file = substr($log_file, 0, strrpos($log_file, "Total") - 3);
-$log_file = substr($log_file, 0, strrpos($log_file, "\n") - 2);
-$log_file = substr($log_file, strrpos($log_file, "\n"));
-$log_parts = explode("|", $log_file);
+$log_file = substr($log_file, 0, strrpos($log_file, "MB/s"));
+$result = trim(substr($log_file, strrpos($log_file, "|") + 1));
 
-$ms = trim(substr($log_parts[2], 0, strpos($log_parts[2], " ms")));
-$result = $ms * 1000; // convert milliseconds to microseconds
 pts_report_numeric_result($result);
 
 ?>
