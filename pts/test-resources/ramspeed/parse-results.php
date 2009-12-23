225fdefccc1dd2fedec453d510947971b2ff65d8
Michael Larabel <michael@phxmichael.hsd1.il.comcast.net>
2009-12-22 18:24:17 -0600

    pts: Fix for ramspeed 2.6.0 update

--- pts/test-resources/ramspeed/parse-results.php
+++ pts/test-resources/ramspeed/parse-results.php
@@ -2,7 +2,7 @@
 
 $log_file = pts_read_log_file();
 $arg = trim($log_file);
-$arg = substr($arg, 0, strrpos($arg, "Mb/s") - 1);
+$arg = substr($arg, 0, strrpos($arg, "MB/s") - 1);
 $arg = substr($arg, strrpos($arg, ' '));
 pts_report_numeric_result($arg);
 
