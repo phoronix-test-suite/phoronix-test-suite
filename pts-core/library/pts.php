6f1eeacf69078c878832255f7dbfc8667d323bd5
Michael Larabel <michael@phxmichael.hsd1.il.comcast.net>
2010-01-14 16:46:23 -0600

    Tag Phoronix Test Suite 2.4 "Lenvik" Beta 1

--- pts-core/library/pts.php
+++ pts-core/library/pts.php
@@ -23,8 +23,8 @@
 
 error_reporting(E_ERROR | E_STRICT);
 
-define("PTS_VERSION", "2.4.0a3");
-define("PTS_BUILD_INT", 2331);
+define("PTS_VERSION", "2.4.0b1");
+define("PTS_BUILD_INT", 2340);
 define("PTS_CODENAME", "LENVIK");
 
 function pts_codename($full_string = false)
