d3caa6a0af679a982154087b7280cb5f0551f56b
Michael Larabel <michael@phxmichael.hsd1.il.comcast.net>
2010-01-26 10:16:56 -0600

    pts: drop norsetto-shadow

--- pts/test-resources/norsetto-shadow/parse-results.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$BENCHMARK_RESULTS = substr($argv[1], strrpos($argv[1], "Average FPS:") + 12);
-$BENCHMARK_RESULTS = trim(substr($BENCHMARK_RESULTS, 0, strpos($BENCHMARK_RESULTS, "\n")));
-pts_report_numeric_result($BENCHMARK_RESULTS);
-
-?>
