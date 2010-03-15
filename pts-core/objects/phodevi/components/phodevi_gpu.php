76cc029b3814ab38ce5e8676b3d24518e73733cb
Michael Larabel <michael@phxmichael.hsd1.il.comcast.net>
2010-03-14 21:36:19 -0500

    pts-core: Fixes from previous ATI Power mangement clock reading commit

--- pts-core/objects/phodevi/components/phodevi_gpu.php
+++ pts-core/objects/phodevi/components/phodevi_gpu.php
@@ -661,7 +661,7 @@ class phodevi_gpu extends phodevi_device_interface
 					if(is_file("/sys/kernel/debug/dri/0/radeon_pm_info"))
 					{
 						// radeon_pm_info should be present with Linux 2.6.34+
-						foreach(pts_trim_explode(file_get_contents("/sys/kernel/debug/dri/0/radeon_pm_info")) as $pm_line)
+						foreach(pts_trim_explode("\n", pts_file_get_contents("/sys/kernel/debug/dri/0/radeon_pm_info")) as $pm_line)
 						{
 							list($descriptor, $value) = pts_trim_explode(':', $pm_line);
 
@@ -947,7 +947,7 @@ class phodevi_gpu extends phodevi_device_interface
 			if(is_file("/sys/kernel/debug/dri/0/radeon_pm_info"))
 			{
 				// radeon_pm_info should be present with Linux 2.6.34+
-				foreach(pts_trim_explode(file_get_contents("/sys/kernel/debug/dri/0/radeon_pm_info")) as $pm_line)
+				foreach(pts_trim_explode("\n", pts_file_get_contents("/sys/kernel/debug/dri/0/radeon_pm_info")) as $pm_line)
 				{
 					list($descriptor, $value) = pts_trim_explode(':', $pm_line);
 
