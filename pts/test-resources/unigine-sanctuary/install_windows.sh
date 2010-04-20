a298fefbe0b060af171b698c7e98aad7128424f5
Michael Larabel <michael@phxmichael.hsd1.il.comcast.net>
2010-04-20 08:57:43 -0500

    pts: unigine-sanctuary for Windows

---
+++ pts/test-resources/unigine-sanctuary/install_windows.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+echo "#!/bin/sh
+cd \"C:\Program Files (x86)\Unigine\Sanctuary\"
+Sanctuary.exe -video_app opengl -data_path ./ -sound_app null -engine_config data/unigine.cfg -system_script sanctuary/unigine.cpp -video_mode -1 -video_fullscreen 1 -extern_define PHORONIX \$@ > \$LOG_FILE" > unigine-sanctuary
+
+# This assumes you will install to the default location
+# C:\Program Files (x86)\Unigine\Sanctuary
+Unigine_Sanctuary-2.2.msi
+
