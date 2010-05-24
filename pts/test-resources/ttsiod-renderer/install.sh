b760af05d02f4e98ee65d1acc223b5c16f8402dd
Michael Larabel <Michael@phoronix.com>
2010-05-24 08:03:02 -0500

    Phoronix Test Suite 2.6 Lyngen

--- pts/test-resources/ttsiod-renderer/install.sh
+++ pts/test-resources/ttsiod-renderer/install.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-tar -jxvf renderer-2.1u.tar.bz2
+tar -jxvf renderer-2.1v.tar.bz2
 
-cd renderer-2.1u/
+cd renderer-2.1v/
 ./configure
 make
 echo $? > ~/install-exit-status
 cd ..
 
 echo "#!/bin/sh
-cd renderer-2.1u/3D-Objects
+cd renderer-2.1v/3D-Objects
 OMP_NUM_THREADS=\$NUM_CPU_CORES ../src/renderer \$@ > \$LOG_FILE 2>&1
 echo \$? > ~/test-exit-status" > ttsiod-renderer
 chmod +x ttsiod-renderer
