75f238f860f4b9943cc7458e14222708d07e29e7
Michael Larabel <Michael@phoronix.com>
2010-05-24 19:02:41 -0500

    pts: Update unigine-heaven test profile to Unigine Heaven 2.1

--- pts/test-resources/unigine-heaven/install.sh
+++ pts/test-resources/unigine-heaven/install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-chmod +x Unigine_Heaven-2.0.run
-./Unigine_Heaven-2.0.run
+chmod +x Unigine_Heaven-2.1.run
+./Unigine_Heaven-2.1.run
 
 echo "#!/bin/sh
 cd Unigine_Heaven/
@@ -9,9 +9,9 @@ export LD_LIBRARY_PATH=bin/:\$LD_LIBRARY_PATH
 
 if [ \$OS_ARCH = \"x86_64\" ]
 then
-	./bin/Heaven_x64 -video_app opengl -data_path ../ -sound_app null -engine_config ../data/heaven_2.0.cfg -system_script heaven/unigine.cpp -video_mode -1 -video_fullscreen 1 -extern_define PHORONIX \$@ > \$LOG_FILE 2>&1
+	./bin/Heaven_x64 -video_app opengl -data_path ../ -sound_app null -engine_config ../data/heaven_2.1.cfg -system_script heaven/unigine.cpp -video_mode -1 -video_fullscreen 1 -extern_define PHORONIX \$@ > \$LOG_FILE 2>&1
 else
-	./bin/Heaven_x86 -video_app opengl -data_path ../ -sound_app null -engine_config ../data/heaven_2.0.cfg -system_script heaven/unigine.cpp -video_mode -1 -video_fullscreen 1 -extern_define PHORONIX \$@ > \$LOG_FILE 2>&1
+	./bin/Heaven_x86 -video_app opengl -data_path ../ -sound_app null -engine_config ../data/heaven_2.1.cfg -system_script heaven/unigine.cpp -video_mode -1 -video_fullscreen 1 -extern_define PHORONIX \$@ > \$LOG_FILE 2>&1
 fi" > unigine-heaven
 chmod +x unigine-heaven
 
--- pts/test-resources/unigine-heaven/install_windows.sh
+++ pts/test-resources/unigine-heaven/install_windows.sh
@@ -2,9 +2,9 @@
 
 echo "#!/bin/sh
 cd \"C:\Program Files (x86)\Unigine\Heaven\"
-Heaven.exe -video_app opengl -data_path ./ -sound_app null -engine_config data/heaven_2.0.cfg -system_script heaven/unigine.cpp -video_mode -1 -video_fullscreen 1 -extern_define PHORONIX \$@ > \$LOG_FILE" > unigine-heaven
+Heaven.exe -video_app opengl -data_path ./ -sound_app null -engine_config data/heaven_2.1.cfg -system_script heaven/unigine.cpp -video_mode -1 -video_fullscreen 1 -extern_define PHORONIX \$@ > \$LOG_FILE" > unigine-heaven
 
 # This assumes you will install to the default location
 # C:\Program Files (x86)\Unigine\Heaven
-Unigine_Heaven-2.0.msi
+Unigine_Heaven-2.1.msi
 
