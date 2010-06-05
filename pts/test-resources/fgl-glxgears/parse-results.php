1c0704d3012d71716d30f6cf20000fd77bc8c972
Michael Larabel <Michael@phoronix.com>
2010-06-04 22:36:14 -0500

    pts: Drop fgl-glxgears, fract test profiles

--- pts/test-resources/fgl-glxgears/parse-results.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$BENCHMARK_RESULTS = trim(substr($argv[1], strpos($argv[1], '=') + 1));
-echo substr($BENCHMARK_RESULTS, 0, strpos($BENCHMARK_RESULTS, ' '));
-
-?>
--- pts/test-resources/fract/parse-results.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$BENCHMARK_RESULTS = substr($argv[1], strpos($argv[1], "this yields") + 11);
-$BENCHMARK_RESULTS = trim(substr($BENCHMARK_RESULTS, 0, strpos($BENCHMARK_RESULTS, "FPS")));
-pts_report_numeric_result($BENCHMARK_RESULTS);
-
-?>
