fadc029c2e94c572353edda8f7cee9ca2f5a193e
Michael Larabel <Michael@phoronix.com>
2010-06-18 13:47:21 -0500

    pts: Update video-cpu-usage test profile to use Big Buck Bunny 1080p H.264 sample

--- pts/test-resources/video-cpu-usage/install.sh
+++ pts/test-resources/video-cpu-usage/install.sh
@@ -3,11 +3,8 @@
 echo "#!/bin/sh
 
 \$SYSTEM_MONITOR_START cpu.usage all-comma 1
-sleep 5
-for i in 1 2 3
-do
-   \$TEST_MPLAYER_BASE/mplayer \$@ \$TEST_VIDEO_SAMPLE/Grey.ts
-done
-sleep 5
+sleep 10
+\$TEST_MPLAYER_BASE/mplayer \$@ big_buck_bunny_1080p_h264.mov
+sleep 10
 \$SYSTEM_MONITOR_STOP \$LOG_FILE" > video-cpu-usage
 chmod +x video-cpu-usage
