c594c71d4ace0be7d16afa4f50f20c9fd4022de4
Michael Larabel <Michael@phoronix.com>
2010-11-18 20:53:29 -0600

    Iveland Alpha 2

--- pts-core/pts-core.php
+++ pts-core/pts-core.php
@@ -21,8 +21,8 @@
 	along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
-define("PTS_VERSION", "3.0.0a1");
-define("PTS_CORE_VERSION", 2940);
+define("PTS_VERSION", "3.0.0a2");
+define("PTS_CORE_VERSION", 2942);
 define("PTS_CODENAME", "IVELAND");
 define("PTS_IS_CLIENT", PTS_MODE == "CLIENT");
 
