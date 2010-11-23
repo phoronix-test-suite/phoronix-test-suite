ae59e010162188c90f7a50815f4719e582b18905
Michael Larabel <Michael@phoronix.com>
2010-11-23 13:54:43 -0600

    pts: Update bullet test profile against upstream Bullet 2.77

--- pts/test-profiles/bullet/install.sh
+++ pts/test-profiles/bullet/install.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-tar -xvf bullet-2.75.tgz
-cd bullet-2.75/
+tar -xvf bullet-2.77.tgz
+cd bullet-2.77/
 cmake .
 make
 echo \$? > ~/test-exit-status
 cd ..
 
 echo "#!/bin/sh
-cd bullet-2.75/Demos/Benchmarks
+cd bullet-2.77/Demos/Benchmarks
 ./AppBenchmarks > \$LOG_FILE 2>&1
 echo \"\n\" >> \$LOG_FILE
 echo \$? > ~/test-exit-status" > bullet
