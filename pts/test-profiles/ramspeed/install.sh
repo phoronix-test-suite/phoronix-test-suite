ebd994bb3b9282b13c185e8c9b25bb80374231a1
Michael Larabel <Michael@phoronix.com>
2010-11-30 08:00:01 -0600

    pts: Update ramspeed test profile against RAMspeed SMP (RAMSMP) 3.5.0

--- pts/test-profiles/ramspeed/install.sh
+++ pts/test-profiles/ramspeed/install.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-tar -zxvf ramspeed-2.6.0.tar.gz
+tar -zxvf ramsmp-3.5.0.tar.gz
 
-cd ramspeed-2.6.0/
+cd ramsmp-3.5.0/
 cat build.sh | grep -v "read ANS" > build_pts.sh
 chmod +x build_pts.sh
 ./build_pts.sh
 cd ..
 
 echo "#!/bin/sh
-cd ramspeed-2.6.0/
-./ramspeed \$@ > \$LOG_FILE 2>&1" > ramspeed
+cd ramsmp-3.5.0/
+./ramsmp \$@ > \$LOG_FILE 2>&1" > ramspeed
 chmod +x ramspeed
 
