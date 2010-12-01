3331646d0fb5e0cae8a37c69cbada59cd1b380d9
Michael Larabel <Michael@phoronix.com>
2010-12-01 07:24:57 -0600

    pts: Go through and verify MacOSX test definitions

--- pts/test-profiles/geekbench/post.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-case $OS_TYPE in
-	"MacOSX" )
-		diskutil eject /Volumes/Geekbench\ 2.1/
-	;;
-esac
--- pts/test-profiles/geekbench/pre.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-case $OS_TYPE in
-	"MacOSX" )
-		hdid Geekbench21-MacOSX.dmg
-	;;
-esac
