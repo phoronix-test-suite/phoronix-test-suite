e5658db1128372e507d896e3e92a82974f678495
Michael Larabel <Michael@phoronix.com>
2010-12-09 21:25:15 -0600

    pts-core: Rename list-tests to list-available-tests to better reflect new state

--- pts-core/commands/list_available_tests.php
+++ pts-core/commands/list_available_tests.php
@@ -20,7 +20,7 @@
 	along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
-class list_tests implements pts_option_interface
+class list_available_tests implements pts_option_interface
 {
 	public static function run($r)
 	{
