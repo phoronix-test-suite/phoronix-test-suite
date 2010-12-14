88b699451ac1cd24fd837a9711e2738fa4bf27f8
Michael Larabel <Michael@phoronix.com>
2010-12-13 18:06:17 -0600

    pts-core: Whoops

--- pts-core/objects/pts_test_run_manager.php
+++ pts-core/objects/pts_test_run_manager.php
@@ -369,7 +369,7 @@ class pts_test_run_manager
 				{
 					pts_client::$display->generic_prompt("Enter a unique name for this test run: ");
 					$input = trim(pts_user_io::read_user_input());
-					$results_identifier = pts_strings::keep_in_string($input, pts_strings::CHAR_LETTER | pts_strings::CHAR_NUMERIC | pts_strings::CHAR_DASH | pts_strings::CHAR_UNDERSCORE | pts_strings::CHAR_COLON | pts_strings::CHAR_COMMA | pts_strings::CHAR_SLASH | pts_strings::CHAR_SPACE);
+					$results_identifier = pts_strings::keep_in_string($input, pts_strings::CHAR_LETTER | pts_strings::CHAR_NUMERIC | pts_strings::CHAR_DASH | pts_strings::CHAR_UNDERSCORE | pts_strings::CHAR_COLON | pts_strings::CHAR_COMMA | pts_strings::CHAR_SLASH | pts_strings::CHAR_SPACE | pts_strings::CHAR_DECIMAL);
 				}
 				$times_tried++;
 
