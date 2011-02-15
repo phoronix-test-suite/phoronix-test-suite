3aa57ddea3c0b5845a2abfd758a627eeab0a047d
Michael Larabel <Michael@phoronix.com>
2011-02-14 21:32:56 -0600

    result_notifier: More work

--- pts-core/modules/result_notifier.php
+++ pts-core/modules/result_notifier.php
@@ -20,7 +20,7 @@
 
 	SETUP STEPS:
 
-	1.) Run "phoronix-test-suite module-setup result_notifier"
+	1.) Run 'phoronix-test-suite module-setup result_notifier'
 	2.) This will prompt you through inputting the commands / absolute file paths to executables to run for each step. Leave empty for irrelevant ones.
 	3.) To have this module always load automatically by the phoronix-test-suite command, add result_notifier to the LoadModules tag in ~/.phoronix-test-suite/user-config.xml
 		i.e. my config portion looked like: <LoadModules>toggle_screensaver, update_checker, result_notifier</LoadModules>
@@ -29,10 +29,10 @@
 
 class result_notifier extends pts_module_interface
 {
-	const module_name = "Result Notifier";
-	const module_version = "1.0.0";
-	const module_description = "A notification module.";
-	const module_author = "Michael Larabel";
+	const module_name = 'Result Notifier';
+	const module_version = '1.1.0';
+	const module_description = 'A notification module.';
+	const module_author = 'Michael Larabel';
 
 	public static function module_info()
 	{
@@ -41,86 +41,86 @@ class result_notifier extends pts_module_interface
 	public static function module_setup()
 	{
 		return array(
-			new pts_module_option("pre_test_process", "Pre-test process hook", null),
-			new pts_module_option("pre_test_run_process", "Pre-test run execution hook", null),
-			new pts_module_option("interim_test_run_process", "Interim-test run execution hook", null),
-			new pts_module_option("post_test_run_process", "Post-test run execution hook", null),
-			new pts_module_option("post_test_process", "Post-test process script", null)
+			new pts_module_option('pre_test_process', 'Pre-test process hook', null),
+			new pts_module_option('pre_test_run_process', 'Pre-test run execution hook', null),
+			new pts_module_option('interim_test_run_process', 'Interim-test run execution hook', null),
+			new pts_module_option('post_test_run_process', 'Post-test run execution hook', null),
+			new pts_module_option('post_test_process', 'Post-test process script', null)
 			);
 	}
 
 	public static function __startup()
 	{
 		// NOTE: This will just print to the terminal when PTS has loaded this module, so you know in fact it's being loaded/should be working
-		echo "\nJust started the result_notifier module.\n";
+		echo PHP_EOL . 'The result_notifier module for providing external hooks has been loaded.' . PHP_EOL;
 	}
 	public static function __pre_run_process(&$test_run_manager)
 	{
-		$executable = pts_module::read_option("pre_test_process");
-		self::process_user_config_external_hook_process($executable, "Running the pre-test process external hook", $test_run_manager);
+		$executable = pts_module::read_option('pre_test_process');
+		self::process_user_config_external_hook_process('pre_test_process', $executable, 'Running the pre-test process external hook', $test_run_manager);
 	}
 	public static function __pre_test_run(&$test_run_request)
 	{
-		$executable = pts_module::read_option("pre_test_run_process");
-		self::process_user_config_external_hook_process($executable, "Running the pre-test external hook", $test_run_manager);
+		$executable = pts_module::read_option('pre_test_run_process');
+		self::process_user_config_external_hook_process('pre_test_run_process', $executable, 'Running the pre-test external hook', $test_run_manager);
 	}
 	public static function __interim_test_run(&$test_run_request)
 	{
-		$executable = pts_module::read_option("interim_test_run_process");
-		self::process_user_config_external_hook_process($executable, "Running the interim-test external hook", $test_run_request);
+		$executable = pts_module::read_option('interim_test_run_process');
+		self::process_user_config_external_hook_process('interim_test_run_process', $executable, 'Running the interim-test external hook', $test_run_request);
 	}
 	public static function __post_test_run(&$test_run_request)
 	{
-		$executable = pts_module::read_option("post_test_run_process");
-		self::process_user_config_external_hook_process($executable, "Running the post-test external hook", $test_run_manager);
+		$executable = pts_module::read_option('post_test_run_process');
+		self::process_user_config_external_hook_process('post_test_run_process', $executable, 'Running the post-test external hook', $test_run_manager);
 	}
 	public static function __post_run_process(&$test_run_manager)
 	{
-		$executable = pts_module::read_option("post_test_process");
-		self::process_user_config_external_hook_process($executable, "Running the post-test process external hook", $test_run_manager);
+		$executable = pts_module::read_option('post_test_process');
+		self::process_user_config_external_hook_process('post_test_process', $executable, 'Running the post-test process external hook', $test_run_manager);
 	}
 
 	// This is called after the XML save, but not sure Intel needs this since __post_run_process is there too...
 /*
 	public static function __post_test_run_process(&$test_run_manager)
 	{
-		$executable = pts_module::read_option("post_test_process");
-		self::process_user_config_external_hook_process($executable, "Doing external post test process", $test_run_manager);
+		$executable = pts_module::read_option('post_test_process');
+		self::process_user_config_external_hook_process($executable, 'Doing external post test process', $test_run_manager);
 	}
 */
-	protected static function process_user_config_external_hook_process($cmd_value, $description_string = null, &$passed_obj = null)
+	protected static function process_user_config_external_hook_process($process, $cmd_value, $description_string = null, &$passed_obj = null)
 	{
 		if(!empty($cmd_value) && (is_executable($cmd_value) || ($cmd_value = pts_client::executable_in_path($cmd_value))))
 		{
 			$descriptor_spec = array(
-				0 => array("pipe", 'r'),
-				1 => array("pipe", 'w'),
-				2 => array("pipe", 'w')
+				0 => array('pipe', 'r'),
+				1 => array('pipe', 'w'),
+				2 => array('pipe', 'w')
 				);
 
-			$env_vars = array();
+			$env_vars = array('PTS_EXTERNAL_TEST_HOOK' => $process);
 
 			if($passed_obj instanceof pts_test_result)
 			{
-				$env_vars["PTS_EXTERNAL_TEST_IDENTIFIER"] = $passed_obj->test_profile->get_identifier() . '-' . $passed_obj->test_result_buffer->get_count();
-				$env_vars["PTS_EXTERNAL_TEST_ARGS"] = $passed_obj->get_arguments();
-				$env_vars["PTS_EXTERNAL_TEST_DESCRIPTION"] = $passed_obj->get_arguments_description();
-				$env_vars["PTS_EXTERNAL_TEST_RESULT_SET"] = $passed_obj->test_result_buffer->get_values_as_string();
-				$env_vars["PTS_EXTERNAL_TEST_RESULT"] = $passed_obj->get_result();
-				$env_vars["PTS_EXTERNAL_TEST_HASH"] = $passed_obj->get_comparison_hash();
-				$env_vars["PTS_EXTERNAL_TEST_STD_DEV_PERCENT"] = pts_math::percent_standard_deviation($passed_obj->test_result_buffer->get_values());
+				$env_vars['PTS_EXTERNAL_TEST_IDENTIFIER'] = $passed_obj->test_profile->get_identifier() . '-' . $passed_obj->test_result_buffer->get_count();
+				$env_vars['PTS_EXTERNAL_TEST_ARGS'] = $passed_obj->get_arguments();
+				$env_vars['PTS_EXTERNAL_TEST_DESCRIPTION'] = $passed_obj->get_arguments_description();
+				$env_vars['PTS_EXTERNAL_TEST_RESULT_SET'] = $passed_obj->test_result_buffer->get_values_as_string();
+				$env_vars['PTS_EXTERNAL_TEST_RESULT'] = $passed_obj->get_result();
+				$env_vars['PTS_EXTERNAL_TEST_HASH'] = $passed_obj->get_comparison_hash();
+				$env_vars['PTS_EXTERNAL_TEST_STD_DEV_PERCENT'] = pts_math::percent_standard_deviation($passed_obj->test_result_buffer->get_values());
 
 				if(is_file($passed_obj->test_profile->get_install_dir() . 'cache-share-' . PTS_INIT_TIME . '.pt2so'))
 				{
 					// There's a cache share present
-					$env_vars["PTS_EXTERNAL_TEST_CACHE_SHARE"] = 1;
+					$env_vars['PTS_EXTERNAL_TEST_CACHE_SHARE'] = 1;
 				}
 			}
 			else if($passed_obj instanceof pts_test_run_manager)
 			{
-				$env_vars["PTS_EXTERNAL_TESTS_IN_QUEUE"] = implode(':', $passed_obj->get_tests_to_run_identifiers());
-				$env_vars["PTS_EXTERNAL_TEST_FILE_NAME"] = $passed_obj->get_file_name();
-				$env_vars["PTS_EXTERNAL_TEST_IDENTIFIER"] = $passed_obj->get_results_identifier();
+				$env_vars['PTS_EXTERNAL_TESTS_IN_QUEUE'] = implode(':', $passed_obj->get_tests_to_run_identifiers());
+				$env_vars['PTS_EXTERNAL_TEST_FILE_NAME'] = $passed_obj->get_file_name();
+				$env_vars['PTS_EXTERNAL_TEST_IDENTIFIER'] = $passed_obj->get_results_identifier();
 			}
 
 			$description_string != null && pts_client::$display->generic_heading($description_string);
@@ -128,7 +128,7 @@ class result_notifier extends pts_module_interface
 			$std_output = stream_get_contents($pipes[1]);
 			$return_value = proc_close($proc);
 
-			// If you want PTS to exit or something when your script returns !0, you could add an "exit;" or whatever you want below
+			// If you want PTS to exit or something when your script returns !0, you could add an 'exit;' or whatever you want below
 			// The contents of $std_output is anything that may have been written by your script, if you want it to be interpreted by anything in this module
 			if($return_value != 0)
 			{
