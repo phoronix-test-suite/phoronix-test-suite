f92acf30f3c6cd79dc1a16cb89187aff2445f2f9
Michael Larabel <Michael@phoronix.com>
2011-02-25 09:45:19 -0600

    documentation: Final update

--- pts-core/pts-core.php
+++ pts-core/pts-core.php
@@ -21,8 +21,8 @@
 	along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
-define('PTS_VERSION', '3.0.0r2');
-define('PTS_CORE_VERSION', 2992);
+define('PTS_VERSION', '3.0.0');
+define('PTS_CORE_VERSION', 2999);
 define('PTS_CODENAME', 'IVELAND');
 define('PTS_IS_CLIENT', PTS_MODE == 'CLIENT');
 
