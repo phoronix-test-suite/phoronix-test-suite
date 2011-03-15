c968cf7d0b3f5b76c96103561588c8a30ec23b38
Michael Larabel <Michael@phoronix.com>
2011-03-15 09:39:50 -0500

    Phoronix Test Suite 3.0.1-Iveland point release

--- pts-core/pts-core.php
+++ pts-core/pts-core.php
@@ -21,8 +21,8 @@
 	along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
-define('PTS_VERSION', '3.0.0');
-define('PTS_CORE_VERSION', 3002);
+define('PTS_VERSION', '3.0.1');
+define('PTS_CORE_VERSION', 3010);
 define('PTS_CODENAME', 'IVELAND');
 define('PTS_IS_CLIENT', PTS_MODE == 'CLIENT');
 
