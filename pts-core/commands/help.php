56b0a0a1dda0042e81cbdcfc65bd42b739a79eee
Michael Larabel <Michael@phoronix.com>
2011-05-18 10:37:53 -0500

    pts-core: HTML documentation improvements

--- pts-core/commands/help.php
+++ pts-core/commands/help.php
@@ -3,8 +3,8 @@
 /*
 	Phoronix Test Suite
 	URLs: http://www.phoronix.com, http://www.phoronix-test-suite.com/
-	Copyright (C) 2010, Phoronix Media
-	Copyright (C) 2010, Michael Larabel
+	Copyright (C) 2010 - 2011, Phoronix Media
+	Copyright (C) 2010 - 2011, Michael Larabel
 
 	This program is free software; you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
@@ -28,7 +28,7 @@ class help implements pts_option_interface
 	public static function run($r)
 	{
 		echo "\n" . pts_title(true) . "\n\n";
-		echo pts_documentation::basic_description() . "\n\nView the included PDF documentation or visit http://www.phoronix-test-suite.com/ for full details.\n";
+		echo pts_documentation::basic_description() . "\n\nView the included PDF / HTML documentation or visit http://www.phoronix-test-suite.com/ for full details.\n";
 		$options = pts_documentation::client_commands_array();
 
 		foreach($options as $section => &$contents)
--- pts-core/objects/pts_html_template.php
+++ pts-core/objects/pts_html_template.php
@@ -24,6 +24,7 @@ class pts_html_template
 {
 	private $dom;
 	private $dom_body;
+	private $section_list;
 
 	public function __construct($Title = '', $SubTitle = '')
 	{
@@ -39,6 +40,13 @@ class pts_html_template
 
 		$p = $this->dom->createElement('h1', 'Phoronix Test Suite');
 		$this->dom_body->appendChild($p);
+
+		$p = $this->dom->createElement('hr');
+		$p->setAttribute('style', 'height: 50px; border: 0;');
+		$this->dom_body->appendChild($p);
+
+		$this->section_list = $this->dom->createElement('ol');
+		$this->dom_body->appendChild($this->section_list);
 	}
 	public function html_to_html($html)
 	{
@@ -53,6 +61,22 @@ class pts_html_template
 			$dom->loadHTML($html);
 		}
 
+		$section_title = $dom->getElementsByTagName('html')->item(0)->getElementsByTagName('head')->item(0)->nodeValue;
+
+		$section_li_a = $this->dom->createElement('a', $section_title);
+		$section_li_a->setAttribute('href', '#' . str_replace(' ', null, $section_title));
+		$section_li = $this->dom->createElement('li');
+		$section_li->appendChild($section_li_a);
+		$this->section_list->appendChild($section_li);
+
+		$p = $this->dom->createElement('hr');
+		$p->setAttribute('style', 'height: 50px; border: 0;');
+		$this->dom_body->appendChild($p);
+		$p = $this->dom->createElement('a');
+		$p->setAttribute('name', str_replace(' ', null, $section_title));
+		$this->dom_body->appendChild($p);
+		$p = $this->dom->createElement('h1', $section_title);
+		$this->dom_body->appendChild($p);
 		// TODO: the below code is known to emit a fatal error right now since the nodes are different, need to copy/merge nodes between docs
 		foreach($dom->getElementsByTagName('html')->item(0)->getElementsByTagName('body')->item(0)->childNodes as $node)
 		{
@@ -63,6 +87,9 @@ class pts_html_template
 	}
 	public function Output($File)
 	{
+		$p = $this->dom->createElement('p', 'Copyright &copy; 2008 - ' . date('Y') . ' by Phoronix Media.');
+		$p->setAttribute('style', 'padding-top: 30px; text-align: center;');
+		$this->dom_body->appendChild($p);
 		return $this->dom->saveHTMLFile($File);
 	}
 }
