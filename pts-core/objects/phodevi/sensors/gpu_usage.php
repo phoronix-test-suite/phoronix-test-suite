51752f6e3bdca55dcd325ed45a8d2d79efc85de7
Michael Larabel <Michael@phoronix.com>
2011-05-19 11:30:06 -0500

    phodevi: Support determing Intel GPU load via number of commands processed per second

--- pts-core/objects/phodevi/sensors/gpu_usage.php
+++ pts-core/objects/phodevi/sensors/gpu_usage.php
@@ -24,6 +24,7 @@ class gpu_usage implements phodevi_sensor
 {
 	static $probe_ati_overdrive = false;
 	static $probe_radeon_fences = false;
+	static $probe_intel_commands = false;
 	static $probe_nvidia_smi = false;
 
 	public static function get_type()
@@ -44,6 +45,10 @@ class gpu_usage implements phodevi_sensor
 		{
 			$unit = 'Fences/s';
 		}
+		else if(self::$probe_intel_commands)
+		{
+			$unit = 'Commands/s';
+		}
 
 		return $unit;
 	}
@@ -59,14 +64,26 @@ class gpu_usage implements phodevi_sensor
 				return true;
 			}
 		}
-		else if(phodevi::is_mesa_graphics() && is_readable('/sys/kernel/debug/dri/0/radeon_fence_info'))
+		else if(phodevi::is_mesa_graphics())
 		{
-			$fence_speed = self::radeon_fence_speed();
-
-			if(is_numeric($fence_speed) && $fence_speed >= 0)
+			if(is_readable('/sys/kernel/debug/dri/0/radeon_fence_info'))
 			{
-				self::$probe_radeon_fences = true;
-				return true;
+				$fence_speed = self::radeon_fence_speed();
+
+				if(is_numeric($fence_speed) && $fence_speed >= 0)
+				{
+					self::$probe_radeon_fences = true;
+					return true;
+				}
+			}
+			else if(is_readable('/sys/kernel/debug/dri/0/i915_gem_seqno'))
+			{
+				$commands = self::intel_command_speed();
+				if(is_numeric($commands) && $commands > 0)
+				{
+					self::$probe_intel_commands = true;
+					return true;
+				}
 			}
 		}
 		else if(phodevi::is_nvidia_graphics())
@@ -98,6 +115,10 @@ class gpu_usage implements phodevi_sensor
 		else if(self::$probe_radeon_fences)
 		{
 			return self::radeon_fence_speed();
+		}
+		else if(self::$probe_intel_commands)
+		{
+			return self::intel_command_speed();
 		}
 	}
 	public static function ati_overdrive_core_usage()
@@ -140,6 +161,34 @@ class gpu_usage implements phodevi_sensor
 		$fence_speed = hexdec($end_signaled_fence) - hexdec($start_signaled_fence);
 
 		return $fence_speed;
+	}
+	protected static function intel_current_sequence_count()
+	{
+		$count = 0;
+		$i915_gem_seqno = file_get_contents('/sys/kernel/debug/dri/0/i915_gem_seqno');
+		$current_sequence = strpos($i915_gem_seqno, 'Current sequence (render ring): ');
+
+		if($current_sequence !== false)
+		{
+			$current_sequence = substr($i915_gem_seqno, $current_sequence + 32);
+			$current_sequence = substr($current_sequence, 0, strpos($current_sequence, PHP_EOL));
+
+			if(is_numeric($current_sequence))
+			{
+				$count = $current_sequence;
+			}
+		}
+
+		return $count;
+	}
+	public static function intel_command_speed()
+	{
+		// Determine GPU usage
+		$first_read = self::intel_current_sequence_count();
+		sleep(1);
+		$second_read = self::intel_current_sequence_count();
+
+		return $second_read - $first_read;
 	}
 }
 
