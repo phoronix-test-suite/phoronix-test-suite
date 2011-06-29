8a5160dffbcfa7ee62e7d9cb1429bb281c242b9d
Michael Larabel <Michael@phoronix.com>
2011-06-29 08:26:38 -0500

    Phoronix Test Suite 3.2.1

--- pts-core/pts-core.php
+++ pts-core/pts-core.php
@@ -21,8 +21,8 @@
 	along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
-define('PTS_VERSION', '3.2.0');
-define('PTS_CORE_VERSION', 3200);
+define('PTS_VERSION', '3.2.1');
+define('PTS_CORE_VERSION', 3210);
 define('PTS_CODENAME', 'GRIMSTAD');
 define('PTS_IS_CLIENT', PTS_MODE == 'CLIENT');
 
