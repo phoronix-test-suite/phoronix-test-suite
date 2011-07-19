258fb3a28ec88686d832c4e5100fe129ea105e06
Michael Larabel <Michael@phoronix.com>
2011-07-19 07:35:30 -0500

    Lillesand Milestone 1

--- pts-core/pts-core.php
+++ pts-core/pts-core.php
@@ -21,8 +21,8 @@
 	along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
-define('PTS_VERSION', '3.4.0m0');
-define('PTS_CORE_VERSION', 3300);
+define('PTS_VERSION', '3.4.0m1');
+define('PTS_CORE_VERSION', 3310);
 define('PTS_CODENAME', 'LILLESAND');
 define('PTS_IS_CLIENT', (defined('PTS_MODE') && PTS_MODE == 'CLIENT'));
 
