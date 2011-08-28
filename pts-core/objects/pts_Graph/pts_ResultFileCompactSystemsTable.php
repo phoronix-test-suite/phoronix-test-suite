50aaccf812bf0045ce2f483b82f4629ce5d9d98f
Michael Larabel <Michael@phoronix.com>
2011-08-28 15:17:02 -0500

    pts_ResultFileCompactSystemsTable: Improvements...

--- pts-core/objects/pts_Graph/pts_ResultFileCompactSystemsTable.php
+++ pts-core/objects/pts_Graph/pts_ResultFileCompactSystemsTable.php
@@ -23,16 +23,21 @@
 
 class pts_ResultFileCompactSystemsTable extends pts_Graph
 {
-	protected $result_file;
+	protected $components;
 	protected $intent;
 
 	public function __construct(&$result_file, $intent = false)
 	{
 		parent::__construct();
-		$this->result_file = $result_file;
-		$this->intent = is_array($intent) ? $intent : array(array(), array());
 
+		$this->intent = is_array($intent) ? $intent : array(array(), array());
 		$this->graph_title = $result_file->get_title();
+
+		$hw = $result_file->get_system_hardware();
+		$sw = $result_file->get_system_software();
+		$hw = pts_result_file_analyzer::system_component_string_to_array(array_shift($hw));
+		$sw = pts_result_file_analyzer::system_component_string_to_array(array_shift($sw));
+		$this->components = array_merge($hw, $sw);
 	}
 	public function renderChart($file = null)
 	{
@@ -42,25 +47,20 @@ class pts_ResultFileCompactSystemsTable extends pts_Graph
 	}
 	public function render_graph_start()
 	{
-		$this->graph_top_heading_height = 8 + $this->graph_font_size_heading;
+		$this->graph_top_heading_height = 22 + $this->graph_font_size_heading;
 
-		$hw = $this->result_file->get_system_hardware();
-		$sw = $this->result_file->get_system_software();
-		$hw = pts_result_file_analyzer::system_component_string_to_array(array_shift($hw));
-		$sw = pts_result_file_analyzer::system_component_string_to_array(array_shift($sw));
-		$components = array_merge($hw, $sw);
-		$longest_component = $this->find_longest_string($components);
-		$component_header_height = $this->text_string_height($longest_component, $this->graph_font, ($this->graph_font_size_identifiers + 1.5));
+		$longest_component = $this->find_longest_string($this->components);
+		$component_header_height = $this->text_string_height($longest_component, $this->graph_font, ($this->graph_font_size_identifiers + 3));
 
 		$this->graph_attr_width = 10 + max(
 			$this->text_string_width($this->graph_title, $this->graph_font, $this->graph_font_size_heading),
-			$this->text_string_width($longest_component, $this->graph_font, ($this->graph_font_size_identifiers + 2))
+			$this->text_string_width($longest_component, $this->graph_font, ($this->graph_font_size_identifiers + 3))
 			);
 
 		$bottom_footer = 50; // needs to be at least 86 to make room for PTS logo
 		$this->graph_attr_height =
 			$this->graph_top_heading_height +
-			((count($components) + ($this->intent[1] ? (count($this->intent[0]) * count($this->intent[1])) - 1 : 0)) * $component_header_height) +
+			((count($this->components) + ($this->intent[1] ? (count($this->intent[0]) * count($this->intent[1])) - 1 : 0)) * $component_header_height) +
 			$bottom_footer
 			;
 
@@ -72,11 +72,13 @@ class pts_ResultFileCompactSystemsTable extends pts_Graph
 		// Header
 		$this->graph_image->draw_rectangle(2, 1, $this->graph_attr_width - 1, $this->graph_top_heading_height, $this->graph_color_main_headers);
 		$this->graph_image->write_text_center($this->graph_title, $this->graph_font, $this->graph_font_size_heading, $this->graph_color_background, 0, 2, $this->graph_attr_width, 2);
+		$this->graph_image->write_text_left(new pts_graph_ir_value($this->graph_watermark_text, array('href' => $this->graph_watermark_url)), $this->graph_font, 8, $this->graph_color_background, 4, ($this->graph_top_heading_height - 6), 4, ($this->graph_top_heading_height - 6));
+		$this->graph_image->write_text_right(new pts_graph_ir_value($this->graph_version, array('href' => 'http://www.phoronix-test-suite.com/')), $this->graph_font, 8, $this->graph_color_background, ($this->graph_attr_width - 4), ($this->graph_top_heading_height - 6), ($this->graph_attr_width - 4), ($this->graph_top_heading_height - 6));
 
 		// Body
 		$offset = $this->graph_top_heading_height;
 		$dash = false;
-		foreach($components as $type => $component)
+		foreach($this->components as $type => $component)
 		{
 			if(($key = array_search($type, $this->intent[0])) !== false)
 			{
@@ -106,10 +108,10 @@ class pts_ResultFileCompactSystemsTable extends pts_Graph
 
 			if(isset($component[1]))
 			{
-				$this->graph_image->draw_rectangle_border(0, $offset, $this->graph_attr_width, $next_offset, $this->graph_color_highlight);
+				$this->graph_image->draw_rectangle_border(0, $offset + 1, $this->graph_attr_width, $next_offset - 1, $this->graph_color_highlight);
 			}
 
-			$this->graph_image->write_text_right($type . (isset($component[1]) ? 's' : null), $this->graph_font, 7, $this->graph_color_text, 0, $offset + 6, $this->graph_attr_width - 4, $offset + 6);
+			$this->graph_image->write_text_right($type . (isset($component[1]) && substr($type, -1) != 'y' ? 's' : null), $this->graph_font, 7, $this->graph_color_text, 0, $offset + 7, $this->graph_attr_width - 4, $offset + 7);
 			$offset += 2;
 
 			foreach($component as $c)
@@ -130,8 +132,13 @@ class pts_ResultFileCompactSystemsTable extends pts_Graph
 		$this->graph_image->draw_rectangle(1, ($this->graph_attr_height - $bottom_footer), $this->graph_attr_width - 1, $this->graph_attr_height, $this->graph_color_main_headers);
 		$this->graph_image->image_copy_merge(new pts_graph_ir_value($this->graph_image->png_image_to_type('http://www.phoronix-test-suite.com/external/pts-logo-80x42-white.png'), array('href' => 'http://www.phoronix-test-suite.com/')), 10, ($this->graph_attr_height - 48), 0, 0, 80, 42);
 
-		$this->graph_image->write_text_right(new pts_graph_ir_value($this->graph_watermark_text, array('href' => $this->graph_watermark_url)), $this->graph_font, 8, $this->graph_color_background, ($this->graph_attr_width - 6), ($this->graph_attr_height - 24), ($this->graph_attr_width - 6), ($this->graph_attr_height - 24));
-		$this->graph_image->write_text_right(new pts_graph_ir_value($this->graph_version, array('href' => 'http://www.phoronix-test-suite.com/')), $this->graph_font, 8, $this->graph_color_background, ($this->graph_attr_width - 6), ($this->graph_attr_height - 10), ($this->graph_attr_width - 6), ($this->graph_attr_height - 10));
+		if(defined('OPENBENCHMARKING_IDS'))
+		{
+			$back_width = $this->graph_attr_width - 4;
+			$this->graph_image->write_text_right(new pts_graph_ir_value(OPENBENCHMARKING_TITLE, array('font-weight' => 'bold', 'href' => 'http://openbenchmarking.org/result/' . OPENBENCHMARKING_IDS)), $this->graph_font, 8, $this->graph_color_background, $back_width, ($this->graph_attr_height - 38), $back_width, ($this->graph_attr_height - 38));
+			$this->graph_image->write_text_right(new pts_graph_ir_value('System Information', array('href' => 'http://openbenchmarking.org/system/' . OPENBENCHMARKING_IDS)), $this->graph_font, 8, $this->graph_color_background, $back_width, ($this->graph_attr_height - 24), $back_width, ($this->graph_attr_height - 24));
+			$this->graph_image->write_text_right(new pts_graph_ir_value('OPC Classification', array('href' => 'http://openbenchmarking.org/opc/' . OPENBENCHMARKING_IDS)), $this->graph_font, 8, $this->graph_color_background, $back_width, ($this->graph_attr_height - 10), $back_width, ($this->graph_attr_height - 10));
+		}
 	}
 	public function render_graph_finish()
 	{
