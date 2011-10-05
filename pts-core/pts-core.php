dd1fef1d3b311a7ba87de1c686965fa490785e37
Michael Larabel <Michael@phoronix.com>
2011-10-05 07:10:24 -0500

    pts-core: Phoronix Test Suite 3.6 Arendal begins....

--- pts-core/pts-core.php
+++ pts-core/pts-core.php
@@ -21,9 +21,9 @@
 	along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
-define('PTS_VERSION', '3.4.0');
-define('PTS_CORE_VERSION', 3400);
-define('PTS_CODENAME', 'LILLESAND');
+define('PTS_VERSION', '3.6.0m0');
+define('PTS_CORE_VERSION', 3500);
+define('PTS_CODENAME', 'ARENDAL');
 define('PTS_IS_CLIENT', (defined('PTS_MODE') && PTS_MODE == 'CLIENT'));
 
 
