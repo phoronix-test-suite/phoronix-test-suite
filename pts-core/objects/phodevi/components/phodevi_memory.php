f578d448fa22fb820563a7bbac32022571fdb8d3
Michael Larabel <Michael@phoronix.com>
2011-10-09 09:04:20 -0500

    phodevi: Improve detection of low RAM capacities (1GB)

--- pts-core/objects/phodevi/components/phodevi_memory.php
+++ pts-core/objects/phodevi/components/phodevi_memory.php
@@ -165,7 +165,7 @@ class phodevi_memory extends phodevi_device_interface
 			$info = intval(trim(substr($info, 0, strpos($info, 'kB'))));
 			$info = floor($info / 1024);
 
-			if(is_numeric($info) && $info > 990)
+			if(is_numeric($info) && $info > 950)
 			{
 				if($info > 4608)
 				{
