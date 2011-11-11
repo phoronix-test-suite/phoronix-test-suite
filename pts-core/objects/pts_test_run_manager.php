4c0d290116477be8b99d4d526c577728f0d18f24
Michael Larabel <Michael@phoronix.com>
2011-11-11 11:30:55 -0600

    pts-core: Improvement

--- pts-core/objects/pts_test_run_manager.php
+++ pts-core/objects/pts_test_run_manager.php
@@ -275,7 +275,8 @@ class pts_test_run_manager
 
 			foreach($recent_results as $m_time => &$recent_result)
 			{
-				$recent_result = sprintf('%-' . $res_length . 'ls [%-ls]', $recent_result, pts_strings::days_ago_format_string(floor(($current_time - $m_time) / 86400)) . ' old');
+				$days = floor(($current_time - $m_time) / 86400);
+				$recent_result = sprintf('%-' . $res_length . 'ls [%-ls]', $recent_result, ($days == 0 ? 'Today' : pts_strings::days_ago_format_string($days . ' old')));
 			}
 			echo PHP_EOL . 'Recently Saved Test Results:' . PHP_EOL;
 			echo pts_user_io::display_text_list($recent_results) . PHP_EOL;
