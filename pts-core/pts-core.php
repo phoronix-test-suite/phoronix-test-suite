803e78908a6cf68e9e012f3aa87491d7b5390475
Michael Larabel <Michael@phoronix.com>
2011-12-01 04:02:13 -0600

    Arendal Milestone 3

--- pts-core/pts-core.php
+++ pts-core/pts-core.php
@@ -21,8 +21,8 @@
 	along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
-define('PTS_VERSION', '3.6.0m2');
-define('PTS_CORE_VERSION', 3521);
+define('PTS_VERSION', '3.6.0m3');
+define('PTS_CORE_VERSION', 3530);
 define('PTS_CODENAME', 'ARENDAL');
 define('PTS_IS_CLIENT', (defined('PTS_MODE') && PTS_MODE == 'CLIENT'));
 define('PTS_IS_DEV_BUILD', (substr(PTS_VERSION, -2, 1) == 'm'));
