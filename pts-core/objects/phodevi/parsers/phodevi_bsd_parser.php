24c7e51f74536a20246f97f0a71c61eda8862eb3
Michael Larabel <Michael@phoronix.com>
2012-02-21 18:45:14 -0600

    phodevi: BSD's kenv returns empty on missing, so fend against that

--- pts-core/objects/phodevi/parsers/phodevi_bsd_parser.php
+++ pts-core/objects/phodevi/parsers/phodevi_bsd_parser.php
@@ -3,8 +3,8 @@
 /*
 	Phoronix Test Suite
 	URLs: http://www.phoronix.com, http://www.phoronix-test-suite.com/
-	Copyright (C) 2009 - 2010, Phoronix Media
-	Copyright (C) 2009 - 2010, Michael Larabel
+	Copyright (C) 2009 - 2012, Phoronix Media
+	Copyright (C) 2009 - 2012, Michael Larabel
 	phodevi_bsd_parser.php: General parsing functions specific to BSD
 
 	This program is free software; you can redistribute it and/or modify
@@ -65,7 +65,7 @@ class phodevi_bsd_parser
 			}
 		}
 
-		return $ret;
+		return $ret != 'empty' ? $ret : null;
 	}
 	public static function read_acpiconf($desc)
 	{
