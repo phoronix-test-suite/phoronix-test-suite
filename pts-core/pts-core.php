b57bc12cb107941458e20b016b27bedfec03833e
Michael Larabel <Michael@phoronix.com>
2012-03-18 19:27:08 -0500

    Phoronix Test Suite 3.8.0-Bygland

--- pts-core/pts-core.php
+++ pts-core/pts-core.php
@@ -21,8 +21,8 @@
 	along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
-define('PTS_VERSION', '3.8.0m3');
-define('PTS_CORE_VERSION', 3732);
+define('PTS_VERSION', '3.8.0');
+define('PTS_CORE_VERSION', 3800);
 define('PTS_CODENAME', 'BYGLAND');
 define('PTS_IS_CLIENT', (defined('PTS_MODE') && PTS_MODE == 'CLIENT'));
 define('PTS_IS_DEV_BUILD', (substr(PTS_VERSION, -2, 1) == 'm'));
