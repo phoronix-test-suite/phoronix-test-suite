a4ffe631d4f236441e41a8bd62d471e886d6b622
Michael Larabel <Michael@phoronix.com>
2012-11-22 12:29:27 -0600

    Phoronix Test Suite 4.2-Randaberg Milestone 3 '

--- pts-core/pts-core.php
+++ pts-core/pts-core.php
@@ -21,8 +21,8 @@
 	along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
-define('PTS_VERSION', '4.2.0m2');
-define('PTS_CORE_VERSION', 4120);
+define('PTS_VERSION', '4.2.0m3');
+define('PTS_CORE_VERSION', 4130);
 define('PTS_CODENAME', 'RANDABERG');
 define('PTS_IS_CLIENT', (defined('PTS_MODE') && PTS_MODE == 'CLIENT'));
 define('PTS_IS_DEV_BUILD', (substr(PTS_VERSION, -2, 1) == 'm'));
