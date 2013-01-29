c187aa3d4ba11dc217c408090f47ac237a34a260
Michael Larabel <Michael@phoronix.com>
2013-01-29 16:47:10 -0600

    pts-core: Add "portsnap fetch extract" support to the FreeBSD external dependency handling

--- pts-core/external-test-dependencies/scripts/install-freebsd-packages.sh
+++ pts-core/external-test-dependencies/scripts/install-freebsd-packages.sh
@@ -2,6 +2,12 @@
 
 # FreeBSD package installation
 
+# Check that ports is setup, below code should work for making sure good state with PC-BSD at least
+if [ -f /usr/ports/distfiles ] && [ ! -d /usr/ports/devel ] && [ -x /usr/sbin/portsnap ];
+then
+   sudo portsnap extract
+fi
+
 # for now try to set BATCH several ways...
 setenv BATCH 1
 # using config-reursive would be another option...
