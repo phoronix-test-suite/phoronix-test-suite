36c781b7a144e639e6da6e763c6b3b400649090d
Michael Larabel <Michael@phoronix.com>
2013-01-29 16:47:30 -0600

    pts-core: Oops, add in fetch.

--- pts-core/external-test-dependencies/scripts/install-freebsd-packages.sh
+++ pts-core/external-test-dependencies/scripts/install-freebsd-packages.sh
@@ -5,7 +5,7 @@
 # Check that ports is setup, below code should work for making sure good state with PC-BSD at least
 if [ -f /usr/ports/distfiles ] && [ ! -d /usr/ports/devel ] && [ -x /usr/sbin/portsnap ];
 then
-   sudo portsnap extract
+   sudo portsnap fetch extract
 fi
 
 # for now try to set BATCH several ways...
