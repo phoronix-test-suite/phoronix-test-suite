0f9a1256daa8ad23c6d847671165903b3c88896d
Michael Larabel <Michael@phoronix.com>
2013-05-15 15:47:10 -0500

    pts-core: Updates from Michal Zatloukal

--- pts-core/objects/phodevi/sensors/cpu_freq.php
+++ pts-core/objects/phodevi/sensors/cpu_freq.php
@@ -3,8 +3,8 @@
 /*
 	Phoronix Test Suite
 	URLs: http://www.phoronix.com, http://www.phoronix-test-suite.com/
-	Copyright (C) 2009 - 2011, Phoronix Media
-	Copyright (C) 2009 - 2011, Michael Larabel
+	Copyright (C) 2009 - 2013, Phoronix Media
+	Copyright (C) 2009 - 2013, Michael Larabel
 
 	This program is free software; you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
@@ -81,6 +81,7 @@ class cpu_freq implements phodevi_sensor
 			if(($cut_point = strpos($info, ' ')) > 0)
 			{
 				$info = substr($info, 0, $cut_point);
+				$info = str_replace(',', '.', $info);
 			}
 		}
 
