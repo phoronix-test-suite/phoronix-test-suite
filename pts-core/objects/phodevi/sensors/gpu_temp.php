e89671b1431d506b697b84eae1f350a525498d34
Michael Larabel <Michael@phoronix.com>
2013-06-28 08:22:09 -0500

    phodevi: Add gpu.temp safety check to prevent against AMD Catalyst OverDrive sending garbage

--- pts-core/objects/phodevi/sensors/gpu_temp.php
+++ pts-core/objects/phodevi/sensors/gpu_temp.php
@@ -3,8 +3,8 @@
 /*
 	Phoronix Test Suite
 	URLs: http://www.phoronix.com, http://www.phoronix-test-suite.com/
-	Copyright (C) 2009 - 2011, Phoronix Media
-	Copyright (C) 2009 - 2011, Michael Larabel
+	Copyright (C) 2009 - 2013, Phoronix Media
+	Copyright (C) 2009 - 2013, Michael Larabel
 
 	This program is free software; you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
@@ -93,6 +93,11 @@ class gpu_temp implements phodevi_sensor
 					}
 				}
 			}
+		}
+
+		if($temp_c > 1000)
+		{
+			return -1;
 		}
 
 		return $temp_c;
