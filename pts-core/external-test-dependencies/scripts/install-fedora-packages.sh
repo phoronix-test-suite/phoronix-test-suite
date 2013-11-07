2d0b97bde5efbd28d7fc60cc727eff102d300ad3
Michael Larabel <Michael@phoronix.com>
2013-11-07 11:23:51 -0600

    pts-core: Packaging external dependency fixes

--- pts-core/external-test-dependencies/scripts/install-fedora-packages.sh
+++ pts-core/external-test-dependencies/scripts/install-fedora-packages.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
-# Fedora package installation
+# Fedora / Red Hat package installation
 
-echo "Please enter your root password below:" 1>&2
-su root -c "yum -y --skip-broken install $*"
+if [ `whoami` = "ec2-user" ] then
+	sudo yum -y --skip-broken install $*
+else
+	echo "Please enter your root password below:" 1>&2
+	su root -c "yum -y --skip-broken install $*"
+fi
+
 exit
