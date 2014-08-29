d23bca1c46b000c2bc0459420753f4489785f701
Michael Larabel <Michael@phoronix.com>
2014-08-28 20:37:41 -0500

    system_monitor: Correct perf-per-Watt calculation for LIB results

--- pts-core/modules/system_monitor.php
+++ pts-core/modules/system_monitor.php
@@ -26,7 +26,7 @@
 class system_monitor extends pts_module_interface
 {
 	const module_name = 'System Monitor';
-	const module_version = '3.1.0';
+	const module_version = '3.1.1';
 	const module_description = 'This module contains sensor monitoring support.';
 	const module_author = 'Michael Larabel';
 
@@ -201,7 +201,7 @@ class system_monitor extends pts_module_interface
 					{
 						$test_result->test_profile->set_result_proportion('HIB');
 						$test_result->test_profile->set_result_scale('Performance Per Watt');
-						$test_result->set_result(pts_math::set_precision(1 / ($test_result->get_result() / $watt_average)));
+						$test_result->set_result(pts_math::set_precision((1 / $test_result->get_result()) / $watt_average);
 						$result_file_writer->add_result_from_result_object_with_value_string($test_result, $test_result->get_result());
 					}
 				}
