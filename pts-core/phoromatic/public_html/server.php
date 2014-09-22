16ee622c5f68ef9e42d04857105bb70f19d1e260
Michael Larabel <Michael@phoronix.com>
2014-09-21 20:47:16 -0500

    phoromatic: Update the new server.php information file

--- pts-core/phoromatic/public_html/server.php
+++ pts-core/phoromatic/public_html/server.php
@@ -1,7 +1,27 @@
 <?php
 
+/*
+	Phoronix Test Suite
+	URLs: http://www.phoronix.com, http://www.phoronix-test-suite.com/
+	Copyright (C) 2014, Phoronix Media
+	Copyright (C) 2014, Michael Larabel
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
 include('../../pts-core.php');
 
-echo pts_title(true) . ' Phoromatic Server';
+echo pts_title(true) . ' Phoromatic Server [' . $_SERVER['SERVER_SOFTWARE'] . ']';
 
 ?>
