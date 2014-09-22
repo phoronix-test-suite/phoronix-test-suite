1d5c4b88f528644a0ee036263eadfec734430f49
Michael Larabel <Michael@phoronix.com>
2014-09-21 21:19:13 -0500

    pts-core: Improvements to make-download-cache

--- pts-core/commands/make_download_cache.php
+++ pts-core/commands/make_download_cache.php
@@ -3,8 +3,8 @@
 /*
 	Phoronix Test Suite
 	URLs: http://www.phoronix.com, http://www.phoronix-test-suite.com/
-	Copyright (C) 2008 - 2011, Phoronix Media
-	Copyright (C) 2008 - 2011, Michael Larabel
+	Copyright (C) 2008 - 2014, Phoronix Media
+	Copyright (C) 2008 - 2014, Michael Larabel
 
 	This program is free software; you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
@@ -28,10 +28,7 @@ class make_download_cache implements pts_option_interface
 	public static function run($r)
 	{
 		// Generates a PTS Download Cache
-		$dc_write_directory = null;
-
 		$dc_write_directory = pts_strings::add_trailing_slash(pts_client::parse_home_directory(pts_config::read_user_config('PhoronixTestSuite/Options/Installation/CacheDirectory', PTS_DOWNLOAD_CACHE_PATH)));
-
 		if($dc_write_directory == null || !is_writable($dc_write_directory))
 		{
 			echo 'No writable download cache directory was found. A download cache cannot be created.' . PHP_EOL . PHP_EOL;
@@ -41,32 +38,27 @@ class make_download_cache implements pts_option_interface
 		echo PHP_EOL . 'Download Cache Directory: ' . $dc_write_directory . PHP_EOL;
 
 		$xml_writer = new nye_XmlWriter();
-
 		foreach(pts_tests::installed_tests() as $test)
 		{
 			$test_profile = new pts_test_profile($test);
-			$cached = false;
 			echo PHP_EOL . 'Checking Downloads For: ' . $test . PHP_EOL;
 
 			foreach(pts_test_install_request::read_download_object_list($test_profile, false) as $file)
 			{
+				$cached = false;
 				if(is_file($dc_write_directory . $file->get_filename()) && $file->check_file_hash($dc_write_directory . $file->get_filename()))
 				{
 					echo '   Previously Cached: ' . $file->get_filename() . PHP_EOL;
 					$cached = true;
 				}
-				else
+				else if(is_dir($test_profile->get_install_dir()))
 				{
-					if(is_dir($test_profile->get_install_dir()))
+					if(is_file($test_profile->get_install_dir() . $file->get_filename()) && $file->check_file_hash($test_profile->get_install_dir() . $file->get_filename()))
 					{
-						if(is_file($test_profile->get_install_dir() . $file->get_filename()) && $file->check_file_hash($test_profile->get_install_dir() . $file->get_filename()))
+						echo '   Caching: ' . $file->get_filename() . PHP_EOL;
+						if(copy($test_profile->get_install_dir() . $file->get_filename(), $dc_write_directory . $file->get_filename()))
 						{
-							echo '   Caching: ' . $file->get_filename() . PHP_EOL;
-
-							if(copy($test_profile->get_install_dir() . $file->get_filename(), $dc_write_directory . $file->get_filename()))
-							{
-								$cached = true;
-							}
+							$cached = true;
 						}
 					}
 				}
@@ -74,14 +66,14 @@ class make_download_cache implements pts_option_interface
 				if($cached)
 				{
 					$xml_writer->addXmlNode('PhoronixTestSuite/DownloadCache/Package/FileName', $file->get_filename());
-					$xml_writer->addXmlNode('PhoronixTestSuite/DownloadCache/Package/MD5', $file->get_md5());
-					$xml_writer->addXmlNode('PhoronixTestSuite/DownloadCache/Package/SHA256', $file->get_sha256());
+					$xml_writer->addXmlNodeWNE('PhoronixTestSuite/DownloadCache/Package/MD5', $file->get_md5());
+					$xml_writer->addXmlNodeWNE('PhoronixTestSuite/DownloadCache/Package/SHA256', $file->get_sha256());
+					$xml_writer->addXmlNodeWNE('PhoronixTestSuite/DownloadCache/Package/AssociatedTest', $test_profile->get_identifier());
 				}
 			}
 		}
 
-		$cache_xml = $xml_writer->getXML();
-		file_put_contents($dc_write_directory . 'pts-download-cache.xml', $cache_xml);
+		file_put_contents($dc_write_directory . 'pts-download-cache.xml', $xml_writer->getXML());
 		echo PHP_EOL;
 	}
 }
