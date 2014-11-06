d88d5a756d5970e113c868fbf5e52ec738efa368
Michael Larabel <Michael@phoronix.com>
2014-11-05 21:11:28 -0600

    Phoronix Test Suite 5.4 Milestone 4

--- pts-core/pts-core.php
+++ pts-core/pts-core.php
@@ -156,8 +156,8 @@ function pts_version_codenames()
 		);
 }
 
-pts_define('PTS_VERSION', '5.4.0m3');
-pts_define('PTS_CORE_VERSION', 5330);
+pts_define('PTS_VERSION', '5.4.0m4');
+pts_define('PTS_CORE_VERSION', 5340);
 pts_define('PTS_CODENAME', 'LIPKI');
 pts_define('PTS_IS_CLIENT', (defined('PTS_MODE') && strstr(PTS_MODE, 'CLIENT') !== false));
 pts_define('PTS_IS_WEB_CLIENT', (defined('PTS_MODE') && PTS_MODE == 'WEB_CLIENT'));
