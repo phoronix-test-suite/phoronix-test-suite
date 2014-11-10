7d1365de3ab7229e0f4b56e0fd521806861f5096
Michael Larabel <Michael@phoronix.com>
2014-11-10 12:44:56 -0600

    phoromatic: Whoops

--- pts-core/commands/start_phoromatic_server.php
+++ pts-core/commands/start_phoromatic_server.php
@@ -80,7 +80,7 @@ class start_phoromatic_server implements pts_option_interface
 				$web_port = $remote_access;
 				$web_socket_port = pts_config::read_user_config('PhoronixTestSuite/Options/Server/WebSocketPort', '');
 
-				while(($web_socket_port == null || !is_numeric($web_socket_port)) || (($fp = fsockopen('127.0.0.1', $web_socket_port, $errno, $errstr, 5)) != false));
+				while($web_socket_port == null || !is_numeric($web_socket_port) || (($fp = fsockopen('127.0.0.1', $web_socket_port, $errno, $errstr, 5)) != false))
 				{
 					if($fp)
 						fclose($fp);
