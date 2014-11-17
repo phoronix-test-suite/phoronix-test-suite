c43511ef95ba8304b9df77de7a6dc33e1448a048
Michael Larabel <Michael@phoronix.com>
2014-11-17 11:55:13 -0600

    phoromatic: Fix for session_save_path() issue on EL6

--- pts-core/phoromatic/public_html/index.php
+++ pts-core/phoromatic/public_html/index.php
@@ -19,6 +19,11 @@
 	You should have received a copy of the GNU General Public License
 	along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
+if(session_save_path() == null)
+{
+	session_save_path('/tmp');
+}
+
 define('PHOROMATIC_SERVER', true);
 error_reporting(E_ALL);
 session_start();
