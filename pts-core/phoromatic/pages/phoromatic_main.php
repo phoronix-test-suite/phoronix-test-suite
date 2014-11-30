1d46fdf0a9850073feda5dd72653573ebb1366fe
Michael Larabel <Michael@phoronix.com>
2014-11-29 23:01:50 -0600

    phoromatic: Tweaks

--- pts-core/phoromatic/pages/phoromatic_main.php
+++ pts-core/phoromatic/pages/phoromatic_main.php
@@ -230,7 +230,7 @@ class phoromatic_main implements pts_webui_interface
 					<ul>
 						<li><h1>Recent System Activity</h1></li>';
 
-		$stmt = phoromatic_server::$db->prepare('SELECT Title, SystemID, LocalIP, CurrentTask, LastCommunication FROM phoromatic_systems WHERE AccountID = :account_id AND State >= 0 ORDER BY LastCommunication');
+		$stmt = phoromatic_server::$db->prepare('SELECT Title, SystemID, LocalIP, CurrentTask, LastCommunication FROM phoromatic_systems WHERE AccountID = :account_id AND State >= 0 ORDER BY LastCommunication DESC');
 		$stmt->bindValue(':account_id', $_SESSION['AccountID']);
 		$result = $stmt->execute();
 		$row = $result->fetchArray();
@@ -243,7 +243,7 @@ class phoromatic_main implements pts_webui_interface
 				if(strtotime($row['LastCommunication']) < (time() - 86400))
 					break;
 				if(stripos($row['CurrentTask'], 'shutdown') !== false || stripos($row['CurrentTask'], 'exit') !== false)
-					break;
+					continue;
 
 				$main .= '<a href="?systems/' . $row['SystemID'] . '"><li>' . $row['Title'] . '<br /><table><tr><td>' . $row['CurrentTask'] . '</td><td>' . $row['LocalIP'] . '</td><td>' . phoromatic_user_friendly_timedate($row['LastCommunication']) . '</td></tr></table></li></a>';
 				$sys_act = true;
