ba2a325762d41a137f77ac2ac2bd8c54b659ca58
Michael Larabel <Michael@phoronix.com>
2015-03-26 13:32:43 -0500

    phoromatic: Get rid of block-poweroff file check in client module now that it can be done per-system from server-side

--- pts-core/modules/phoromatic.php
+++ pts-core/modules/phoromatic.php
@@ -23,7 +23,7 @@
 class phoromatic extends pts_module_interface
 {
 	const module_name = 'Phoromatic Client';
-	const module_version = '0.5.0';
+	const module_version = '0.5.1';
 	const module_description = 'The Phoromatic client is used for connecting to a Phoromatic server (Phoromatic.com or a locally run server) to facilitate the automatic running of tests, generally across multiple test nodes in a routine manner. For more details visit http://www.phoromatic.com/. This module is intended to be used with Phoronix Test Suite 5.2+ clients and servers.';
 	const module_author = 'Phoronix Media';
 
@@ -552,10 +552,6 @@ class phoromatic extends pts_module_interface
 						if(!$supports_wol)
 							break;
 					case 'shutdown':
-						if(pts_module::read_file('block-poweroff'))
-						{
-							break;
-						}
 						echo PHP_EOL . 'Phoromatic received a remote command to shutdown.' . PHP_EOL;
 						phoromatic::update_system_status('Attempting System Shutdown');
 						if(pts_client::executable_in_path('poweroff'))
