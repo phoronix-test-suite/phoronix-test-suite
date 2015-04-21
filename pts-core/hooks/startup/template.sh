fe613f1f5436f7ead49769aca0e2fee4c37c9241
Michael Larabel <Michael@phoronix.com>
2015-04-21 11:00:10 -0500

    pts-core: Add pts-core/hooks/startup

---
+++ pts-core/hooks/startup/template.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# Scripts placed within this directory are called immediately prior to launching the Phoronix Test Suite for the first time.
+
+# hooks/startup are useful if wanting to pre-seed certain environment variables prior to executing the Phoronix Test Suite, etc.
+
+# No special environment variables are passed/pre-seeded prior to running the scripts in this folder.
+
