bfdb7c58e41f63937e495f4c8f44c5d5423b5a1b
Michael Larabel <Michael@phoronix.com>
2015-05-11 11:12:22 -0500

    phoromatic: Add $dc to build_suite page

--- pts-core/phoromatic/pages/phoromatic_build_suite.php
+++ pts-core/phoromatic/pages/phoromatic_build_suite.php
@@ -140,6 +140,7 @@ class phoromatic_build_suite implements pts_webui_interface
 			<h3>Add Another Test</h3>';
 			$main .= '<select name="add_to_suite_select_test" id="add_to_suite_select_test" onchange="phoromatic_build_suite_test_details();">';
 
+			$dc = pts_strings::add_trailing_slash(pts_client::parse_home_directory(pts_config::read_user_config('PhoronixTestSuite/Options/Installation/CacheDirectory', PTS_DOWNLOAD_CACHE_PATH)));
 			$dc_exists = is_file($dc . 'pts-download-cache.json');
 			foreach(pts_openbenchmarking::available_tests() as $test)
 			{
