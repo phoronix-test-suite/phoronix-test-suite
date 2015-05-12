e6c8d9f546f7412bff8c493eeadaedddb20db86a
Michael Larabel <Michael@phoronix.com>
2015-05-12 18:39:40 -0500

    pts-core: Support for using DNF directly when available on Fedora

--- pts-core/external-test-dependencies/scripts/install-fedora-packages.sh
+++ pts-core/external-test-dependencies/scripts/install-fedora-packages.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 # Fedora / Red Hat package installation
-
-if [ `whoami` = "ec2-user" ]; then
+if [ -x /usr/bin/dnf ]; then
+	sudo dnf -y install $*
+elif [ `whoami` = "ec2-user" ]; then
 	sudo yum -y --skip-broken install $*
 else
 	echo "Please enter your root password below:" 1>&2
