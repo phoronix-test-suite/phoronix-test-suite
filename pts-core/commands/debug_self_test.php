4cf4978f6f6b3012666455e871e67c4b6eea62a5
Michael Larabel <Michael@phoronix.com>
2015-09-04 12:56:55 -0500

    pts-core: Rename emalloc line

--- pts-core/commands/debug_self_test.php
+++ pts-core/commands/debug_self_test.php
@@ -72,7 +72,7 @@ class debug_self_test implements pts_option_interface
 
 		echo PHP_EOL . 'TOTAL ELAPSED TIME: ' . (str_repeat(' ', $longest_c - strlen('ELAPSED TIME'))) . round($finish - $start, 3) . ' seconds';
 		echo PHP_EOL . 'PEAK MEMORY USAGE: ' . (str_repeat(' ', $longest_c - strlen('PEAK MEMORY USAGE'))) . round(memory_get_peak_usage(true) / 1048576, 3) . ' MB';
-		echo PHP_EOL . 'PEAK MEMORY USAGE (emalloc): ' . (str_repeat(' ', $longest_c - strlen('PEAK MEMORY USAGE (emalloc)'))) . round(memory_get_peak_usage() / 1048576, 3) . ' MB';
+		echo PHP_EOL . 'PEAK MEMORY USAGE EMALLOC: ' . (str_repeat(' ', $longest_c - strlen('PEAK MEMORY USAGE (emalloc)'))) . round(memory_get_peak_usage() / 1048576, 3) . ' MB';
 		echo PHP_EOL;
 	}
 }
