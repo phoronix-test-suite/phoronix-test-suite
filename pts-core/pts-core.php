b7a5644216c4692fdf203f579705248ecb3e57a2
Michael Larabel <Michael@phoronix.com>
2015-09-24 11:20:33 -0500

    Phoronix Test Suite 6.0 Milestone 2

--- pts-core/pts-core.php
+++ pts-core/pts-core.php
@@ -164,8 +164,8 @@ function pts_version_codenames()
 		);
 }
 
-pts_define('PTS_VERSION', '6.0.0m1');
-pts_define('PTS_CORE_VERSION', 5910);
+pts_define('PTS_VERSION', '6.0.0m2');
+pts_define('PTS_CORE_VERSION', 5920);
 pts_define('PTS_CODENAME', 'HAMMERFEST');
 pts_define('PTS_IS_CLIENT', (defined('PTS_MODE') && strstr(PTS_MODE, 'CLIENT') !== false));
 pts_define('PTS_IS_WEB_CLIENT', (defined('PTS_MODE') && PTS_MODE == 'WEB_CLIENT'));
