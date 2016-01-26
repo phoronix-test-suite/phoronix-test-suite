phoronix-test-suite/phoronix-test-suite

commit 120638b8ff6b51b2a011c0990489fc2fbfa01482

pts_Graph: rendering fix on collision of result identifiers

pts-core/objects/pts_Graph/pts_graph_horizontal_bars.php
@@ -109,9 +109,9 @@ class pts_graph_horizontal_bars extends pts_graph_core
 					}
 				}
 
-				$i_o = $this->calc_offset($group_offsets, $identifier);
-				$i = $this->calc_offset($id_offsets, $buffer_item->get_result_identifier());
 				$value = $buffer_item->get_result_value();
+				$i_o = $this->calc_offset($group_offsets, $identifier);
+				$i = $this->calc_offset($id_offsets, $buffer_item->get_result_identifier() . ' ' . $value);
 				$graph_size = max(0, round(($value / $this->i['graph_max_value']) * $work_area_width));
 				$value_end_right = max($this->i['left_start'] + $graph_size, 1);
 				$px_bound_top = $this->i['top_start'] + ($this->is_multi_way_comparison ? 5 : 0) + ($this->i['identifier_height'] * $i) + ($bar_height * $i_o) + ($separator_height * ($i_o + 1));

commit 6c2f939266ac5370ecb5a4f9a584b3048da4d706

pts-core: Fix regression of multi-way comparison graphs caused by 120638b8ff6b51b2a011c0990489fc2fbfa01482

pts-core/objects/pts_Graph/pts_graph_horizontal_bars.php
@@ -119,7 +119,12 @@ class pts_graph_horizontal_bars extends pts_graph_core
 
 				$value = $buffer_item->get_result_value();
 				$i_o = $this->calc_offset($group_offsets, $identifier);
-				$i = $this->calc_offset($id_offsets, $buffer_item->get_result_identifier() . ' ' . $value);
+
+				if($this->is_multi_way_comparison)
+					$i = $this->calc_offset($id_offsets, $buffer_item->get_result_identifier());
+				else
+					$i = $this->calc_offset($id_offsets, $buffer_item->get_result_identifier() . ' ' . $value);
+
 				$graph_size = max(0, round(($value / $this->i['graph_max_value']) * $work_area_width));
 				$value_end_right = max($this->i['left_start'] + $graph_size, 1);
 				$px_bound_top = $this->i['top_start'] + ($this->is_multi_way_comparison ? 5 : 0) + ($this->i['identifier_height'] * $i) + ($bar_height * $i_o) + ($separator_height * ($i_o + 1));