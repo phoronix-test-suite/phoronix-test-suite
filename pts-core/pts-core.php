2755eff0aceb10aac100b1c0859daf5de41d201f
Michael Larabel <Michael@phoronix.com>
2016-02-05 09:26:44 -0600

    Phoronix Test Suite 6.2 Milestone 5

--- pts-core/pts-core.php
+++ pts-core/pts-core.php
@@ -232,8 +232,8 @@ function pts_version_codenames()
 		);
 }
 
-pts_define('PTS_VERSION', '6.2.0m4');
-pts_define('PTS_CORE_VERSION', 6140);
+pts_define('PTS_VERSION', '6.2.0m5');
+pts_define('PTS_CORE_VERSION', 6150);
 pts_define('PTS_CODENAME', 'GAMVIK');
 pts_define('PTS_IS_CLIENT', (defined('PTS_MODE') && strstr(PTS_MODE, 'CLIENT') !== false));
 pts_define('PTS_IS_WEB_CLIENT', (defined('PTS_MODE') && PTS_MODE == 'WEB_CLIENT'));
