111bab9c279c82f4c0d7a1d73c7eec37dddaa17a
Michael Larabel <Michael@phoronix.com>
2016-04-10 09:32:32 -0500

    pts-core: Use pts_math::set_precision() rather than round(x, 2);

--- pts-core/objects/client/pts_stress_run_manager.php
+++ pts-core/objects/client/pts_stress_run_manager.php
@@ -232,7 +232,7 @@ class pts_stress_run_manager extends pts_test_run_manager
 				$table = array(array('TEST', 'MIN', 'AVG', 'MAX'));
 				foreach($sensor_data_archived as $sensor_name => &$sensor_data)
 				{
-					$table[] = array($sensor_name . ': ', min($sensor_data), round(array_sum($sensor_data) / count($sensor_data), 2), max($sensor_data));
+					$table[] = array($sensor_name . ': ', min($sensor_data), pts_math::set_precision((array_sum($sensor_data) / count($sensor_data), 2), max($sensor_data));
 				}
 				$report_buffer .= pts_user_io::display_text_table($table, '   - ', 2) . PHP_EOL;
 				$report_buffer .= '######' . PHP_EOL;
