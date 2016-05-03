a5bce454bccee79fb5813c96e909cbb2dce39655
Michael Larabel <Michael@phoronix.com>
2016-05-03 11:52:07 -0500

    phoromatic: Don't show title and description on result page when merging multiple results

--- pts-core/phoromatic/pages/phoromatic_result.php
+++ pts-core/phoromatic/pages/phoromatic_result.php
@@ -309,8 +309,15 @@ class phoromatic_result implements pts_webui_interface
 				return;
 			}
 
-			$main .= '<h1>' . ($result_file->get_title() != null ? $result_file->get_title() : 'Phoromatic Results') . '</h1>';
-			$main .= '<p>' . $result_file->get_description() . '</p>';
+			if(count($result_files) > 1)
+			{
+				$main .= '<h1>Phoromatic Comparison</h1>';
+			}
+			else
+			{
+				$main .= '<h1>' . ($result_file->get_title() != null ? $result_file->get_title() : 'Phoromatic Results') . '</h1>';
+				$main .= '<p>' . $result_file->get_description() . '</p>';
+			}
 			$main .= phoromatic_annotate_entry('RESULT', implode(',', $upload_ids), 'TOP');
 
 			if($result_file->get_system_count() == 1 || ($intent = pts_result_file_analyzer::analyze_result_file_intent($result_file, $intent, true)))
