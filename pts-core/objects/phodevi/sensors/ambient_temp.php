6c594d4266151fb2c7b4d8532377e92389657179
Michael Larabel <Michael@phoronix.com>
2016-05-05 09:04:02 -0500

    phodevi: Add ambient.temp sensor for reporting ambient temperature, currently supports temperv14 USB sensors

---
+++ pts-core/objects/phodevi/sensors/ambient_temp.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+	Phoronix Test Suite
+	URLs: http://www.phoronix.com, http://www.phoronix-test-suite.com/
+	Copyright (C) 2016, Phoronix Media
+	Copyright (C) 2016, Michael Larabel
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+class ambient_temp extends phodevi_sensor
+{
+	const SENSOR_TYPE = 'ambient';
+	const SENSOR_SENSES = 'temp';
+	const SENSOR_UNIT = 'Celsius';
+
+	public function read_sensor()
+	{
+		$sys_temp = -1;
+		if(pts_client::executable_in_path('temperv14'))
+		{
+			$temperv14 = trim(shell_exec('temperv14 -c 2>&1'));
+
+			if(!empty($temperv14) && is_numeric($temperv14))
+			{
+				return $temperv14;
+			}
+		}
+	}
+}
+
+?>
