eb241892bb26c52c6a9c5f3cf22220a029eb875e
Michael Larabel <Michael@phoronix.com>
2016-08-06 13:02:29 -0500

    pts-core: Restructure bash on windows check

--- pts-core/objects/client/pts_test_execution.php
+++ pts-core/objects/client/pts_test_execution.php
@@ -416,7 +416,7 @@ class pts_test_execution
 			}
 		}
 
-		if($abort_testing && getenv('BASH_FOR_WINDOWS') == false) // BASH_FOR_WINDOWS is temporary workaround
+		if($abort_testing && !is_dir('/mnt/c/Windows')) // bash on Windows has issues where this is always called, looks like bad exit status on Windows
 		{
 			self::test_run_error($test_run_manager, $test_run_request, 'This test execution has been abandoned.');
 			return false;
