283ea3f704aba49b3265a4cbbeaf81c1fab67b90
Peter O'Connor <sunnyflunk@gmail.com>
2016-09-30 11:27:32 +1000

    pts-core: Update Solus external dependency handling

--- pts-core/external-test-dependencies/scripts/install-solus-packages.sh
+++ pts-core/external-test-dependencies/scripts/install-solus-packages.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-# Pardus package installation
+# Solus package installation
 
 echo "Please enter your root password below:" 1>&2
-su root -c "pisi install --ignore-safety --yes-all $*"
+sudo eopkg install --yes-all $*
 exit
