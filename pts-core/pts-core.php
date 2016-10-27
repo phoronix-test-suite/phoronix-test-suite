bdc952bd0c53568d5a4f945da11a217d926f45b6
Michael Larabel <Michael@phoronix.com>
2016-10-27 07:15:40 -0500

    Phoronix Test Suite 6.8.0 Milestone 1 (Tana)

--- pts-core/pts-core.php
+++ pts-core/pts-core.php
@@ -240,9 +240,9 @@ function pts_version_codenames()
 		);
 }
 
-pts_define('PTS_VERSION', '6.6.1');
-pts_define('PTS_CORE_VERSION', 6610);
-pts_define('PTS_CODENAME', 'LOPPA');
+pts_define('PTS_VERSION', '6.8.0m1');
+pts_define('PTS_CORE_VERSION', 6710);
+pts_define('PTS_CODENAME', 'TANA');
 pts_define('PTS_IS_CLIENT', (defined('PTS_MODE') && strstr(PTS_MODE, 'CLIENT') !== false));
 pts_define('PTS_IS_WEB_CLIENT', (defined('PTS_MODE') && PTS_MODE == 'WEB_CLIENT'));
 pts_define('PTS_IS_DEV_BUILD', (substr(PTS_VERSION, -2, 1) == 'm'));
