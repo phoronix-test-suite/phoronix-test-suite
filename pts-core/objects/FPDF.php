9cf7ecd68cbdd394871bd6b3846d717c9e3f062d
Michael Larabel <Michael@phoronix.com>
2016-12-01 17:32:20 -0600

    FPDF: update

--- pts-core/objects/FPDF.php
+++ pts-core/objects/FPDF.php
@@ -86,8 +86,7 @@ var $PDFVersion;         // PDF version number
 *                               Public methods                                 *
 *                                                                              *
 *******************************************************************************/
-function FPDF($orientation='P', $unit='mm', $size='A4')
-{
+public function __construct($orientation='P',$unit='mm',$size='A4') {
 	// Some checks
 	$this->_dochecks();
 	// Initialization of properties
