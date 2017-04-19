7f5028f3cef2cc016325709a31038412e7deada8
Michael Larabel <Michael@phoronix.com>
2017-04-19 10:04:27 -0500

    perf_per_dollar: Allow custom units, support LIB results

--- pts-core/modules/perf_per_dollar.php
+++ pts-core/modules/perf_per_dollar.php
@@ -3,8 +3,8 @@
 /*
 	Phoronix Test Suite
 	URLs: http://www.phoronix.com, http://www.phoronix-test-suite.com/
-	Copyright (C) 2015 - 2016, Phoronix Media
-	Copyright (C) 2015 - 2016, Michael Larabel
+	Copyright (C) 2015 - 2017, Phoronix Media
+	Copyright (C) 2015 - 2017, Michael Larabel
 	perf_per_dollar.php: This module is derived from the system_monitor module
 
 	This program is free software; you can redistribute it and/or modify
@@ -24,18 +24,19 @@
 class perf_per_dollar extends pts_module_interface
 {
 	const module_name = 'Performance Per Dollar/Cost Calculator';
-	const module_version = '0.1.1';
-	const module_description = 'Setting the COST_PERF_PER_DOLLAR= environment variable to whatever value of the system cost/component you are running a comparison on will yield extra graphs that calculate the performance-per-dollar based on the test being run. The COST_PERF_PER_DOLLAR environment variable is applied just to the current test run identifier.';
+	const module_version = '0.2.0';
+	const module_description = 'Setting the COST_PERF_PER_DOLLAR= environment variable to whatever value of the system cost/component you are running a comparison on will yield extra graphs that calculate the performance-per-dollar based on the test being run. The COST_PERF_PER_DOLLAR environment variable is applied just to the current test run identifier. Set the COST_PERF_PER_UNIT= environment variable if wishing to use a metric besides dollar/cost.';
 	const module_author = 'Michael Larabel';
 
 	private static $COST_PERF_PER_DOLLAR = 0;
+	private static $COST_PERF_PER_UNIT = 'Dollar';
 	private static $successful_test_run_request = null;
 	private static $perf_per_dollar_collection;
 	private static $result_identifier;
 
 	public static function module_environmental_variables()
 	{
-		return array('COST_PERF_PER_DOLLAR');
+		return array('COST_PERF_PER_DOLLAR', 'COST_PERF_PER_UNIT');
 	}
 	public static function module_info()
 	{
@@ -48,6 +49,11 @@ class perf_per_dollar extends pts_module_interface
 			self::$COST_PERF_PER_DOLLAR = $d;
 			echo PHP_EOL . 'The Phoronix Test Suite will generate performance-per-dollar graphs with an assumed value of $' . $d . '.' . PHP_EOL;
 			self::$perf_per_dollar_collection = array();
+
+			if(($d = getenv('COST_PERF_PER_UNIT')) != false)
+			{
+				self::$COST_PERF_PER_UNIT = $d;
+			}
 		}
 		else
 		{
@@ -73,6 +79,12 @@ class perf_per_dollar extends pts_module_interface
 			if(self::$successful_test_run_request->test_profile->get_result_proportion() == 'HIB')
 			{
 				$result = pts_math::set_precision(self::$successful_test_run_request->active->get_result() / self::$COST_PERF_PER_DOLLAR);
+				$scale = $test_result->test_profile->get_result_scale() . ' Per ' . self::$COST_PERF_PER_UNIT;
+			}
+			else if(self::$successful_test_run_request->test_profile->get_result_proportion() == 'LIB')
+			{
+				$result = pts_math::set_precision(self::$successful_test_run_request->active->get_result() * self::$COST_PERF_PER_DOLLAR);
+				$scale = $test_result->test_profile->get_result_scale() . ' x ' . self::$COST_PERF_PER_UNIT;
 			}
 
 			if($result != 0)
@@ -82,7 +94,7 @@ class perf_per_dollar extends pts_module_interface
 				$test_result->test_profile->set_identifier(null);
 				$test_result->set_used_arguments_description('Performance / Cost - ' . $test_result->get_arguments_description());
 				$test_result->set_used_arguments('dollar comparison ' . $test_result->get_arguments());
-				$test_result->test_profile->set_result_scale($test_result->test_profile->get_result_scale() . ' Per Dollar');
+				$test_result->test_profile->set_result_scale($scale);
 				$test_result->test_result_buffer = new pts_test_result_buffer();
 				$test_result->test_result_buffer->add_test_result(self::$result_identifier, $result, null, array('install-footnote' => '$' . self::$COST_PERF_PER_DOLLAR . ' reported cost.'));
 				$result_file->add_result($test_result);
