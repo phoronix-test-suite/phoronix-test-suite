4d6b97d39af99df4a4721c8f8d52970f98ee29e6
Michael Larabel <Michael@phoronix.com>
2017-07-19 09:20:48 -0500

    perf_tip_prompter: Add a powersave tip, make unified handling of performance tips, style the tips nicely for CLI

--- pts-core/modules/perf_tip_prompter.php
+++ pts-core/modules/perf_tip_prompter.php
@@ -19,6 +19,31 @@
 	along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
+class pts_perf_tip_msg
+{
+	public $message;
+	public $action;
+	public $reference_url;
+	public function __construct($msg, $act = null, $url = null)
+	{
+		$this->message = $msg;
+		$this->action = $act;
+		$this->reference_url = $url;
+	}
+	public function get_message()
+	{
+		return $this->message;
+	}
+	public function get_action()
+	{
+		return $this->action;
+	}
+	public function get_reference_url()
+	{
+		return $this->reference_url;
+	}
+}
+
 class perf_tip_prompter extends pts_module_interface
 {
 	const module_name = 'Performance Tip Prompter';
@@ -45,6 +70,7 @@ class perf_tip_prompter extends pts_module_interface
 	public static function __pre_run_process(&$test_run_manager)
 	{
 		$test_hardware_types = array();
+		$perf_tips = array();
 		foreach($test_run_manager->get_tests_to_run() as $test_run_request)
 		{
 			pts_arrays::unique_push($test_hardware_types, $test_run_request->test_profile->get_test_hardware_type());
@@ -55,7 +81,7 @@ class perf_tip_prompter extends pts_module_interface
 			// BELOW ARE CHECKS TO MAKE IF WANTING TO SHOW FOR 'DISK' TESTS
 			$disk_scheduler = phodevi::read_property('disk', 'scheduler');
 
-			if($disk_scheduler == 'BFQ' || $disk_scheduler == 'BFQ-MQ' || $disk_scheduler == 'BFQ-SQ')
+			if(true || $disk_scheduler == 'BFQ' || $disk_scheduler == 'BFQ-MQ' || $disk_scheduler == 'BFQ-SQ')
 			{
 				$mount_options = phodevi::read_property('disk', 'mount-options');
 				$partition = basename($mount_options['device']);
@@ -63,21 +89,44 @@ class perf_tip_prompter extends pts_module_interface
 				$low_latency_file = '/sys/block/' . $device . '/queue/iosched/low_latency';
 				$low_latency = pts_file_io::file_get_contents($low_latency_file);
 
-				if($low_latency == 0)
-					return;
+				//if($low_latency == 0)
+				//	return;
 
-				echo PHP_EOL . "\t\t\t\tWARNING" . PHP_EOL;
-				echo PHP_EOL . 'This is not a disk benchmark to measure responsiveness or latency for' . PHP_EOL;
-				echo 'soft real-time applications, but BFQ is being used in low-latency mode!' . PHP_EOL;
-				echo PHP_EOL . 'In low-latency mode, BFQ sacrifices throughput when needed to guarantee' . PHP_EOL;
-				echo 'either maximum responsiveness or low latency to isochronous I/O (the I/O' . PHP_EOL;
-				echo 'of, e.g., video and audio players).' . PHP_EOL;
-				echo PHP_EOL . 'For this benchmark, please execute' . PHP_EOL;
-				echo 'echo 0 > ' . $low_latency_file . PHP_EOL;
-				echo '(after every switch to BFQ), or set SUPPRESS_PERF_TIPS to suppress this' . PHP_EOL;
-				echo 'WARNING.' . PHP_EOL;
-				echo PHP_EOL . 'Press any key to continue or CTRL-C to stop the test.';
-				pts_user_io::read_user_input();
+				$perf_tips[] = new pts_perf_tip_msg('The BFQ I/O scheduler was detected and is being used in low-latency mode. In low-latency mode, BFQ sacrifices throughput when needed to guarantee either maximum responsiveness or low latency to isochronous I/O (the I/O of, e.g., video and audio players).', 'echo 0 > ' . $low_latency_file);
+			}
+		}
+		if(in_array('System', $test_hardware_types) || in_array('Processor', $test_hardware_types))
+		{
+			// BELOW ARE CHECKS TO MAKE IF WANTING TO SHOW FOR 'Processor' OR 'System' TESTS
+			$cpu_scaling_governor = phodevi::read_property('cpu', 'scaling-governor');
+
+			if(stripos($cpu_scaling_governor, 'powersave') !== false)
+			{
+				$perf_tips[] = new pts_perf_tip_msg('The powersave CPU scaling governor is currently in use. It\'s possible to obtain greater performance if using the performance governor.', 'echo performance | tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor', 'http://openbenchmarking.org/result/1706268-TR-CPUGOVERN32');
+			}
+		}
+
+		if(!empty($perf_tips))
+		{
+			foreach($perf_tips as &$tip)
+			{
+				pts_client::$display->display_interrupt_message($tip->get_message(), 'Performance Tip', 'green');
+				if($tip->get_action() != null)
+				{
+					pts_client::$display->display_interrupt_message('To change behavior, run: ', null, 'gray');
+					pts_client::$display->display_interrupt_message($tip->get_action(), null, 'gray');
+				}
+				if($tip->get_reference_url() != null)
+				{
+					pts_client::$display->display_interrupt_message('Reference: ' . $tip->get_reference_url(), null, 'red');
+				}
+				echo PHP_EOL;
+			}
+
+			if($test_run_manager->is_interactive_mode())
+			{
+				pts_client::$display->display_interrupt_message('Continuing in 5 seconds or press CTRL-C to stop the testing process.', null, 'green');
+				sleep(5);
 			}
 		}
 	}
