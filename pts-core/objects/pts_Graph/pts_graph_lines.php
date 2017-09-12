f6bf4378ac5dab3215770e68690b15b80dc1290c
Michael Larabel <Michael@phoronix.com>
2017-09-12 16:45:27 -0500

    pts_Graph: Increase max size

--- pts-core/objects/pts_Graph/pts_graph_lines.php
+++ pts-core/objects/pts_Graph/pts_graph_lines.php
@@ -139,7 +139,7 @@ class pts_graph_lines extends pts_graph_core
 		}
 		else
 		{
-			$max_value *= 1.05; // leave room at top of graph
+			$max_value *= 1.15; // leave room at top of graph
 			$this->i['graph_max_value'] = round($max_value, $max_value < 10 ? 1 : 0);
 			$this->i['graph_max_value'] = round(ceil($this->i['graph_max_value'] / $this->i['mark_count']), (0 - strlen($this->i['graph_max_value']) + 2)) * $this->i['mark_count'];
 
