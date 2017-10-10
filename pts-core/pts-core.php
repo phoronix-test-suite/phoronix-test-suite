913ffd4450dd50ac4d80e96044fdb629a247d067
Michael Larabel <Michael@phoronix.com>
2017-10-10 05:14:50 -0500

    Phoronix Test Suite 7.6 Milestone 1

--- pts-core/pts-core.php
+++ pts-core/pts-core.php
@@ -248,9 +248,9 @@ function pts_version_codenames()
 		);
 }
 
-pts_define('PTS_VERSION', '7.4.0');
-pts_define('PTS_CORE_VERSION', 7400);
-pts_define('PTS_CODENAME', 'TYNSET');
+pts_define('PTS_VERSION', '7.6.0m1');
+pts_define('PTS_CORE_VERSION', 7510);
+pts_define('PTS_CODENAME', 'ALVDAL');
 pts_define('PTS_IS_CLIENT', (defined('PTS_MODE') && strstr(PTS_MODE, 'CLIENT') !== false));
 pts_define('PTS_IS_WEB_CLIENT', (defined('PTS_MODE') && PTS_MODE == 'WEB_CLIENT'));
 pts_define('PTS_IS_DEV_BUILD', (substr(PTS_VERSION, -2, 1) == 'm'));
