ae3d38ddec65253db8d6b505056d5c8e3e0141c7
Michael Larabel <Michael@phoronix.com>
2017-10-27 16:03:36 -0500

    phodevi: Fix for DragonFlyBSD sometimes reporting string as "       "

--- pts-core/objects/phodevi/components/phodevi_motherboard.php
+++ pts-core/objects/phodevi/components/phodevi_motherboard.php
@@ -365,7 +365,7 @@ class phodevi_motherboard extends phodevi_device_interface
 			$product = phodevi_bsd_parser::read_kenv('smbios.system.product');
 			$version = phodevi_bsd_parser::read_kenv('smbios.system.version'); // for at least Lenovo ThinkPads this is where it displays ThinkPad model
 
-			if($vendor != null && ($product != null || $version != null))
+			if($vendor != null && ($product != null || $version != null) && str_replace(array('"', ' '), null, $vendor) != null)
 			{
 				$info = $vendor . ' ' . $product . ' ' . $version;
 			}
