f2ad85e7357e7a56913d5e8bdca7573ec72d4653
Michael Larabel <Michael@phoronix.com>
2017-11-05 09:23:29 -0600

    phodevi: physical CPU core detection count on macOS

--- pts-core/objects/phodevi/components/phodevi_cpu.php
+++ pts-core/objects/phodevi/components/phodevi_cpu.php
@@ -184,6 +184,10 @@ class phodevi_cpu extends phodevi_device_interface
 			// hw.cpu_topology_core_ids works at least on DragonFly BSD
 			$physical_cores = intval(phodevi_bsd_parser::read_sysctl(array('hw.cpu_topology_core_ids')));
 		}
+		else if(phodevi::is_macosx())
+		{
+			$info = intval(phodevi_bsd_parser::read_sysctl(array('hw.physicalcpu')));
+		}
 
 		if(empty($physical_cores) || !is_numeric($physical_cores))
 		{
