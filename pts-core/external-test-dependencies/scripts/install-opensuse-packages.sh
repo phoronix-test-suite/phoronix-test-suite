1a799cce6acad860feb80409a55c5d95384d3253
Michael Larabel <Michael@phoronix.com>
2017-11-27 17:26:25 -0600

    pts-core: Better external dependency handling when running on EC2 instances (SUSE and Gentoo)

--- pts-core/external-test-dependencies/scripts/install-opensuse-packages.sh
+++ pts-core/external-test-dependencies/scripts/install-opensuse-packages.sh
@@ -3,8 +3,9 @@
 # OpenSuSE package installation
 
 echo "Please enter your root password below:" 1>&2
-
-if [ -x /usr/bin/zypper ]; then
+if [ `whoami` = "ec2-user" ] && [ -x /usr/bin/zypper ]; then
+	sudo zypper install -l -y -n --force-resolution $*
+elif [ -x /usr/bin/zypper ]; then
 	su root -c "zypper install -l -y -n --force-resolution $*"
 else
 	su root -c "yast -i $*"
