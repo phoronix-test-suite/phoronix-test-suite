85df8554f0ffccfa729412596c83b09692445b58
Michael Larabel <Michael@phoronix.com>
2018-01-31 08:48:27 -0600

    pts-core: Add a bounds check on CSV output in pts_result_file_output
    https://www.phoronix.com/forums/forum/phoronix/phoronix-test-suite/1004804-export-results-to-csv-or-xml-crashes-sometimes-in-phoromatic

--- pts-core/objects/pts_result_file_output.php
+++ pts-core/objects/pts_result_file_output.php
@@ -3,8 +3,8 @@
 /*
 	Phoronix Test Suite
 	URLs: http://www.phoronix.com, http://www.phoronix-test-suite.com/
-	Copyright (C) 2010 - 2017, Phoronix Media
-	Copyright (C) 2010 - 2017, Michael Larabel
+	Copyright (C) 2010 - 2018, Phoronix Media
+	Copyright (C) 2010 - 2018, Michael Larabel
 
 	This program is free software; you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
@@ -84,7 +84,7 @@ class pts_result_file_output
 
 			foreach($columns as $column)
 			{
-				$csv_output .= $delimiter . $table_data[$column][$i];
+				$csv_output .= $delimiter . (isset($table_data[$column][$i]) ? $table_data[$column][$i] : null);
 			}
 
 			$csv_output .= PHP_EOL;
