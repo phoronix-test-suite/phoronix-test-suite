dc4cd0739f6690361988fd9413c329b7be8f5ede
Michael Larabel <Michael@phoronix.com>
2018-05-31 17:11:12 -0500

    pts-core: Add PTS_RELEASE_DATE

--- pts-core/modules/update_checker.php
+++ pts-core/modules/update_checker.php
@@ -3,8 +3,8 @@
 /*
 	Phoronix Test Suite
 	URLs: http://www.phoronix.com, http://www.phoronix-test-suite.com/
-	Copyright (C) 2008 - 2016, Phoronix Media
-	Copyright (C) 2008 - 2016, Michael Larabel
+	Copyright (C) 2008 - 2018, Phoronix Media
+	Copyright (C) 2008 - 2018, Michael Larabel
 	update_checker.php: This module checks to see if the Phoronix Test Suite -- and its tests and suites -- are up to date.
 
 	This program is free software; you can redistribute it and/or modify
@@ -30,15 +30,18 @@ class update_checker extends pts_module_interface
 
 	public static function __pre_option_process()
 	{
+		// if first run of the day or if the PTS release build is more than 1 year old, start pestering user to upgrade...
+		$do_check = IS_FIRST_RUN_TODAY || (time() > strtotime(PTS_RELEASE_DATE) + (86400 * 30 * 12));
+
 		// Once a day check for new version
-		if(IS_FIRST_RUN_TODAY && pts_network::internet_support_available())
+		if($do_check && pts_network::internet_support_available())
 		{
 			// Check For pts-core updates
 			$latest_reported_version = pts_network::http_get_contents('http://www.phoronix-test-suite.com/LATEST_CORE');
 			if(is_numeric($latest_reported_version) && $latest_reported_version > PTS_CORE_VERSION)
 			{
 				// New version of PTS is available
-				pts_client::$display->generic_heading(strtoupper('An outdated version of the Phoronix Test Suite is installed.' . PHP_EOL . 'The version in use is ' . PTS_VERSION . ' (' . PTS_CORE_VERSION . '), but the latest is pts-core ' . $latest_reported_version . '.' . PHP_EOL . 'Visit http://www.phoronix-test-suite.com/ to update this software.'));
+				pts_client::$display->generic_heading(strtoupper('An outdated version of the Phoronix Test Suite is installed.' . PHP_EOL . 'The version in use is ' . PTS_VERSION . ' (' . PTS_CORE_VERSION . '), but the latest is pts-core ' . $latest_reported_version . '.' . PHP_EOL . 'Visit https://www.phoronix-test-suite.com/ to update this software.'));
 			}
 		}
 
--- pts-core/pts-core.php
+++ pts-core/pts-core.php
@@ -255,6 +255,8 @@ function pts_version_codenames()
 pts_define('PTS_VERSION', '8.0.0m5');
 pts_define('PTS_CORE_VERSION', 7950);
 pts_define('PTS_CODENAME', 'AREMARK');
+pts_define('PTS_RELEASE_DATE', '20180531');
+
 pts_define('PTS_IS_CLIENT', (defined('PTS_MODE') && strstr(PTS_MODE, 'CLIENT') !== false));
 pts_define('PTS_IS_WEB_CLIENT', (defined('PTS_MODE') && PTS_MODE == 'WEB_CLIENT'));
 pts_define('PTS_IS_DEV_BUILD', (substr(PTS_VERSION, -2, 1) == 'm'));
