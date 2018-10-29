1f6c7b7fa4f3734f0e612a9a08d880579e9554cd
Michael Larabel <michael@phoronix.com>
2018-10-29 07:01:46 -0500

    phodevi: Another BSD core count fix

--- pts-core/objects/phodevi/components/phodevi_cpu.php
+++ pts-core/objects/phodevi/components/phodevi_cpu.php
@@ -160,7 +160,7 @@ class phodevi_cpu extends phodevi_device_interface
 			// hw.cpu_topology_core_ids works at least on DragonFly BSD
 			$phys_ids = intval(phodevi_bsd_parser::read_sysctl(array('hw.cpu_topology_phys_ids')));
 			$physical_cores = intval(phodevi_bsd_parser::read_sysctl(array('hw.cpu_topology_core_ids')));
-			if($phys_ids > 0 && ($phys_ids * $physical_cores) <= phodevi::read_property('cpu', 'thread-count'))
+			if($phys_ids > 0 && ($phys_ids * $physical_cores) <= phodevi::read_property('cpu', 'thread-count') && $physical_cores % 2 == 0)
 			{
 				$physical_cores = $phys_ids * $physical_cores;
 			}
