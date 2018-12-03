d4152606c9b980283f0acfd354e365e2de563175
Michael Larabel <michael@phoronix.com>
2018-12-03 09:24:50 -0600

    pts-core: Make Debian EC2 happy for ex dep package installs

--- pts-core/external-test-dependencies/scripts/install-debian-packages.sh
+++ pts-core/external-test-dependencies/scripts/install-debian-packages.sh
@@ -4,6 +4,9 @@ if [ -x /usr/bin/aptitude ]; then
 	# aptitude is nice since it doesn't fail if a non-existant package is hit
 	# See: http://bugs.debian.org/cgi-bin/bugreport.cgi?bug=503215
 	su -c "aptitude -y install $*"
+elif [ `whoami` = "admin" ] && [ -x /usr/bin/sudo ]; then
+	# Amazon EC2 is admin user, sudo works
+	sudo apt-get -y --ignore-missing install $*
 else
 	su -c "apt-get -y --ignore-missing install $*"
 fi
