0003dbf4e3a292bfdcbf4e5cbebe67d6a484cf1e
Michael Larabel <michael@phoronix.com>
2019-01-04 16:35:49 -0600

    pts-core: Allow outputting line graphs to terminal interface as basic box plot graphs

--- pts-core/modules/system_monitor.php
+++ pts-core/modules/system_monitor.php
@@ -3,8 +3,8 @@
 /*
 	Phoronix Test Suite
 	URLs: http://www.phoronix.com, http://www.phoronix-test-suite.com/
-	Copyright (C) 2008 - 2017, Phoronix Media
-	Copyright (C) 2008 - 2017, Michael Larabel
+	Copyright (C) 2008 - 2019, Phoronix Media
+	Copyright (C) 2008 - 2019, Michael Larabel
 	system_monitor.php: System sensor monitoring module for PTS
 
 	This program is free software; you can redistribute it and/or modify
