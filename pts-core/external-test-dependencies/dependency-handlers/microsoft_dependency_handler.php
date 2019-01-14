8444b1abee1455d68aa05d9aac44d8676710c396
Michael Larabel <michael@phoronix.com>
2019-01-14 07:17:07 -0600

    pts-core: ex dep updates

--- pts-core/external-test-dependencies/dependency-handlers/microsoft_dependency_handler.php
+++ pts-core/external-test-dependencies/dependency-handlers/microsoft_dependency_handler.php
@@ -131,6 +131,8 @@ class microsoft_dependency_handler implements pts_dependency_handler
 				else
 				{
 					echo 'Downloading...' . PHP_EOL;
+					$download_package = new pts_test_file_download($url);
+					pts_client::$display->test_install_download_file('DOWNLOAD', $download_package);
 					pts_network::download_file($url, $download_destination);
 				}
 				echo 'Executing...' . PHP_EOL;
