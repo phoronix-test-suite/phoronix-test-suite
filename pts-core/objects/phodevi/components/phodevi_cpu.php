ac40f89a43dd9d4f73da74313b7d80857341eb3c
Michael Larabel <michael@phoronix.com>
2019-03-14 14:02:22 -0500

    phodevi: Cavium addition

--- pts-core/objects/phodevi/components/phodevi_cpu.php
+++ pts-core/objects/phodevi/components/phodevi_cpu.php
@@ -614,7 +614,7 @@ class phodevi_cpu extends phodevi_device_interface
 					// Haven't found a better way to detect Ampere eMAG as not exposed via cpuinfo, etc
 					$info = 'Ampere eMAG ' . $info;
 				}
-				else if(strpos(phodevi::$vfs->dmesg, 'thunderx') !== false)
+				else if(strpos(phodevi::$vfs->dmesg, 'thunderx') !== false || strpos(phodevi::$vfs->dmesg, 'Cavium erratum') !== false)
 				{
 					// Haven't found a better way to detect ThunderX as not exposed via cpuinfo, etc
 					$info = 'Cavium ThunderX ' . $info;
