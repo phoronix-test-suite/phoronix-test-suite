609725f3223a3e6271b865fc96d9a92737541640
Michael Larabel <michael@phoronix.com>
2019-03-28 20:14:37 -0500

    ob_auto_compare: Tweak median reporting

--- pts-core/modules/ob_auto_compare.php
+++ pts-core/modules/ob_auto_compare.php
@@ -335,7 +335,7 @@ class ob_auto_compare extends pts_module_interface
 					}
 
 					echo PHP_EOL;
-					echo '    ' . pts_client::cli_just_italic('Result compared to ' . pts_client::cli_just_bold(number_format($sample_count)) . ' OpenBenchmarking.org samples; median: ' . pts_client::cli_just_bold(round($percentiles[50], 2)) . '. Box plot of sampling:') . PHP_EOL;
+					echo '    ' . pts_client::cli_just_italic('Result compared to ' . pts_client::cli_just_bold(number_format($sample_count)) . ' OpenBenchmarking.org samples; median: ' . pts_client::cli_just_bold(round($percentiles[50], ($percentiles[50] < 100 ? 2 : 0))) . '. Box plot of sampling:') . PHP_EOL;
 					echo '    ' . implode('', $box_plot) . PHP_EOL;
 					foreach($box_plot_complement as $line_r)
 					{
