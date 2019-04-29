fa9e306cca794087a5717f3e8cfc3d4605352b95
Michael Larabel <michael@phoronix.com>
2019-04-29 18:23:04 -0500

    pts-core: Set MESA_VK_WSI_PRESENT_MODE=immediate by default to work around possible 60 FPS RADV bug:

--- pts-core/objects/client/pts_tests.php
+++ pts-core/objects/client/pts_tests.php
@@ -181,6 +181,7 @@ class pts_tests
 
 		// Safe-guards to try to ensure more accurate testing
 		$extra_vars['vblank_mode'] = '0'; // Avoid sync to vblank with the open-source drivers
+		$extra_vars['MESA_VK_WSI_PRESENT_MODE'] = 'immediate'; // https://cgit.freedesktop.org/mesa/mesa/commit/?id=a182adfd83ad00e326153b00a725a014e0359bf0
 		$extra_vars['__GL_SYNC_TO_VBLANK'] = '0'; // Avoid sync to vblank with the NVIDIA binary drivers
 		$extra_vars['CCACHE_DISABLE'] = '1'; // Should avoid ccache being used in compiler tests
 
