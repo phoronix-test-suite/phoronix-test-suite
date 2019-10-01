fd7242cf1d77c2a34c82430198161a13b09083c9
phoronix <phoronix@localhost.localdomain>
2019-10-01 14:03:05 -0400

    pts-core: Add CentOS packages for ones not found in Fedora

---
+++ pts-core/external-test-dependencies/scripts/install-centos-packages.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# Fedora / Red Hat package installation
+if [ `whoami` = "root" ] && [ ! -w /usr/bin/sudo ]; then
+	yum -y install $*
+elif [ -x /usr/bin/dnf ]; then
+	sudo dnf -y --skip-broken install $*
+elif [ `whoami` = "ec2-user" ]; then
+	sudo yum -y --skip-broken install $*
+else
+	echo "Please enter your SUDO password below:" 1>&2 
+	read -s -p "Password: " passwd
+	if ! echo $passwd | sudo -S -p '' yum -y --skip-broken install $*; then
+        	echo "Please enter your ROOT password below:" 1>&2
+		su root -c "yum -y --skip-broken install $*"
+	fi
+fi
+
+exit
