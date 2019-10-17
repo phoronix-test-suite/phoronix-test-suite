1adcaa0e1eeae1e2ed0490275b5c19ef40153e3e
Michael Larabel <michael@phoronix.com>
2019-10-17 05:36:57 -0500

    Update for PTS 9.2 M1

--- pts-core/pts-core.php
+++ pts-core/pts-core.php
@@ -278,9 +278,9 @@ function pts_version_codenames()
 		);
 }
 
-pts_define('PTS_VERSION', '9.2.0m0');
-pts_define('PTS_CORE_VERSION', 9100);
-pts_define('PTS_RELEASE_DATE', '20191001');
+pts_define('PTS_VERSION', '9.2.0m1');
+pts_define('PTS_CORE_VERSION', 9110);
+pts_define('PTS_RELEASE_DATE', '20191017');
 pts_define('PTS_CODENAME', 'Hurdal');
 
 pts_define('PTS_IS_CLIENT', (defined('PTS_MODE') && strstr(PTS_MODE, 'CLIENT') !== false));
