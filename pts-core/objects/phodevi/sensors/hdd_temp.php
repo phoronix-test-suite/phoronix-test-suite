2ae9865540a335be4368a004846aec20b7316a1b
Michael Larabel <michael@phoronix.com>
2020-01-12 14:20:35 -0600

    phodevi: Add support for Linux 5.6+ drivetemp kernel module for ATA drive temperatures

--- pts-core/objects/phodevi/sensors/hdd_temp.php
+++ pts-core/objects/phodevi/sensors/hdd_temp.php
@@ -3,8 +3,8 @@
 /*
 	Phoronix Test Suite
 	URLs: http://www.phoronix.com, http://www.phoronix-test-suite.com/
-	Copyright (C) 2009 - 2019, Phoronix Media
-	Copyright (C) 2009 - 2019, Michael Larabel
+	Copyright (C) 2009 - 2020, Phoronix Media
+	Copyright (C) 2009 - 2020, Michael Larabel
 
 	This program is free software; you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
@@ -115,6 +115,12 @@ class hdd_temp extends phodevi_sensor
 		if($temp == -1)
 		{
 			$temp = phodevi_linux_parser::read_sysfs_node('/sys/class/hwmon/hwmon*/temp*_input', 'POSITIVE_NUMERIC', array('name' => 'nvme'));
+
+			if($temp == null)
+			{
+				$temp = phodevi_linux_parser::read_sysfs_node('/sys/class/hwmon/hwmon*/temp*_input', 'POSITIVE_NUMERIC', array('name' => 'drivetemp'));
+			}
+
 			if($temp > 1000)
 			{
 				$temp = round($temp / 1000, 2);
