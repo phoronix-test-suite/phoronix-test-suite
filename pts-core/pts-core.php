fdea1e740e2753a0fb1b29f2a335ff934bfc21c4
Michael Larabel <michael@phoronix.com>
2020-01-13 07:00:25 -0600

    pts-core: Ready for 9.4 M1

--- pts-core/pts-core.php
+++ pts-core/pts-core.php
@@ -278,9 +278,9 @@ function pts_version_codenames()
 		);
 }
 
-pts_define('PTS_VERSION', '9.4.0m0');
-pts_define('PTS_CORE_VERSION', 9309);
-pts_define('PTS_RELEASE_DATE', '20200112');
+pts_define('PTS_VERSION', '9.4.0m1');
+pts_define('PTS_CORE_VERSION', 9310);
+pts_define('PTS_RELEASE_DATE', '20200113');
 pts_define('PTS_CODENAME', 'Vestby');
 
 pts_define('PTS_IS_CLIENT', (defined('PTS_MODE') && strstr(PTS_MODE, 'CLIENT') !== false));
