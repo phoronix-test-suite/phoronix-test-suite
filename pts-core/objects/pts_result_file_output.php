a689b6e1417836462c461b13ccffea3df20ba2e6
Michael Larabel <michael@phoronix.com>
2020-01-30 06:53:09 -0600

    pts-core: Add description to CSV export per https://www.phoronix.com/forums/forum/phoronix/phoronix-test-suite/1156296-export-to-csv-doesnt-include-description

--- pts-core/objects/pts_result_file_output.php
+++ pts-core/objects/pts_result_file_output.php
@@ -56,7 +56,7 @@ class pts_result_file_output
 	{
 		$csv_output = null;
 
-		$csv_output .= $result_file->get_title() . PHP_EOL . PHP_EOL;
+		$csv_output .= $result_file->get_title() . PHP_EOL . $result_file->get_description() . PHP_EOL . PHP_EOL;
 
 		$columns = array();
 		$hw = array();
