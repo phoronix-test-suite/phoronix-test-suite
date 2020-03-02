a82946ca7f1dac55a9b3f7e729e2be73bc340ebb
Michael Larabel <michael@phoronix.com>
2020-03-02 10:43:40 -0600

    PTS 9.4.1 will be out this week as small update

--- pts-core/pts-core.php
+++ pts-core/pts-core.php
@@ -288,9 +288,9 @@ function pts_version_codenames()
 		);
 }
 
-pts_define('PTS_VERSION', '9.4.0');
-pts_define('PTS_CORE_VERSION', 9400);
-pts_define('PTS_RELEASE_DATE', '20200225');
+pts_define('PTS_VERSION', '9.4.1');
+pts_define('PTS_CORE_VERSION', 9410);
+pts_define('PTS_RELEASE_DATE', '20200302');
 pts_define('PTS_CODENAME', 'Vestby');
 
 pts_define('PTS_IS_CLIENT', (defined('PTS_MODE') && strstr(PTS_MODE, 'CLIENT') !== false));
