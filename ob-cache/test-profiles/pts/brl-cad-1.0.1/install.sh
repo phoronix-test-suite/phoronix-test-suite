b3bb07418d3e89f5d46c9f713b123dbd5a05dd87
Arsalan Afzal <afzal.arsalan@gmail.com>
2020-05-16 21:03:33 -0500

    Updated BRL-CAD from 7.28.0 to 7.38.8 in order to fix build
    BRL-CAD no longer builds on modern CMake due to a change in build dependencies. The latest upstream BRL-CAD fixes this issue

--- ob-cache/test-profiles/pts/brl-cad-1.0.1/install.sh
+++ ob-cache/test-profiles/pts/brl-cad-1.0.1/install.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-tar -xf brlcad-7.28.0.tar.bz2
+tar -xf brlcad-7.30.8.tar.bz2
 
-mkdir brlcad-7.28.0/build
-cd brlcad-7.28.0/build
+mkdir brlcad-7.30.8/build
+cd brlcad-7.30.8/build
 cmake .. -DBRLCAD_ENABLE_STRICT=NO -DBRLCAD_BUNDLED_LIBS=ON -DBRLCAD_OPTIMIZED_BUILD=ON -DCMAKE_BUILD_TYPE=Release
 make -j $NUM_CPU_CORES
 echo $? > ~/install-exit-status
 cd ~
 
 echo "#!/bin/sh
-cd brlcad-7.28.0/build
+cd brlcad-7.30.8/build
 ./bench/benchmark run -P\$NUM_CPU_CORES > \$LOG_FILE 2>&1
 echo \$? > ~/test-exit-status" > brl-cad
 chmod +x brl-cad
