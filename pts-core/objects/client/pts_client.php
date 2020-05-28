5424df27904b9b1e35a7e1ee30491dbc06809964
Michael Larabel <michael@phoronix.com>
2020-05-28 06:07:45 -0500

    pts-core: Revert Python update for Windows

--- pts-core/objects/client/pts_client.php
+++ pts-core/objects/client/pts_client.php
@@ -1823,6 +1823,7 @@ class pts_client
 			$possible_paths_to_add = array('C:\Users\\' . getenv('USERNAME') . '\AppData\Local\Programs\Python\Python36-32',
 				'C:\Users\\' . getenv('USERNAME') . '\AppData\Local\Programs\Python\Python37',
 				'C:\Users\\' . getenv('USERNAME') . '\AppData\Local\Programs\Python\Python38',
+				'C:\Users\\' . getenv('USERNAME') . '\AppData\Local\Programs\Python\Python39',
 				'C:\Python27',
 				'C:\Go\bin',
 				'C:\Strawberry\perl\bin',
