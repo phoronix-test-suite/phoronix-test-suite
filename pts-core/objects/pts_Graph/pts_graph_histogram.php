2ea4e6ca0385d1ef63a173737eeaca33bd789198
Michael Larabel <michael@phoronix.com>
2020-07-14 16:39:32 -0500

    pts_Graph: Improve histo

--- pts-core/objects/pts_Graph/pts_graph_histogram.php
+++ pts-core/objects/pts_Graph/pts_graph_histogram.php
@@ -36,7 +36,7 @@ class pts_graph_histogram extends pts_graph_core
 			
 		$this->val_min = floor(min($all_values));
 		$this->val_max = ceil(max($all_values));
-		$result->set_used_arguments_description('Range of results of ' . $this->val_min . ' to ' . $this->val_max);
+		$result->set_used_arguments_description('Range Of Results ' . $this->val_min . ' To ' . $this->val_max . ' ');
 		$range = abs($this->val_min - $this->val_max);
 		$this->number_of_bins = min(30, ceil(sqrt($range)));
 		$this->bin_increment = ($range / $this->number_of_bins);
