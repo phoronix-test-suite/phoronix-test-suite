5c9d4c016664d11847870289adcb4ba118f3ca3b
Michael Larabel <michael@phoronix.com>
2020-08-13 19:29:30 -0500

    pts-core: AE improvement to clear old data in component-data

--- pts-core/objects/pts_ae_data.php
+++ pts-core/objects/pts_ae_data.php
@@ -373,7 +373,7 @@ class pts_ae_data
 			// Update/Create Component JSON
 			//
 			
-			if(count($comparison_components) > 20 && $last_appeared > strtotime('-3 months'))
+			if(count($comparison_components) > 20 && $last_appeared > strtotime('-5 months'))
 			{
 				foreach($comparison_components as $component => $value)
 				{
