943f82cb35d1888ede6f4e04f5e436600e5540fc
Michael Larabel <michael@phoronix.com>
2020-09-12 20:00:35 -0500

    Prep for 10.0 Milestone 2

--- pts-core/pts-core.php
+++ pts-core/pts-core.php
@@ -297,9 +297,9 @@ function pts_version_codenames()
 		);
 }
 
-pts_define('PTS_VERSION', '10.0.0m1');
-pts_define('PTS_CORE_VERSION', 9911);
-pts_define('PTS_RELEASE_DATE', '20200819');
+pts_define('PTS_VERSION', '10.0.0m2');
+pts_define('PTS_CORE_VERSION', 9920);
+pts_define('PTS_RELEASE_DATE', '20200912');
 pts_define('PTS_CODENAME', 'Finnsnes');
 
 pts_define('PTS_IS_CLIENT', (defined('PTS_MODE') && strstr(PTS_MODE, 'CLIENT') !== false));
