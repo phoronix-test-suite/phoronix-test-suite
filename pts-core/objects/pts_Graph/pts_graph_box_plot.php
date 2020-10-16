e07fb1cf03073727dc8081480e9eee58e7d9ab28
Michael Larabel <michael@phoronix.com>
2020-10-16 08:24:13 -0500

    test

--- pts-core/objects/pts_Graph/pts_graph_box_plot.php
+++ pts-core/objects/pts_Graph/pts_graph_box_plot.php
@@ -165,14 +165,8 @@ class pts_graph_box_plot extends pts_graph_horizontal_bars
 			$raw = is_numeric($raw) ? max($raw) : 0;
 			$max = max($max, $val, $raw);
 		}
-var_dump($max);
-		$maximum = (ceil(round($max * 1.04) 
-		
-		
-		/ $this->i['mark_count']) + 1) *
-		
-		
-		 $this->i['mark_count'];
+
+		$maximum = (ceil(round($max * 1.04) / $this->i['mark_count']) + 1) * $this->i['mark_count'];
 		$maximum = round(ceil($maximum / $this->i['mark_count']), (0 - strlen($maximum) + 2)) * $this->i['mark_count'];
 		return $maximum;
 	}
