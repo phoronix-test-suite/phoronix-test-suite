c48d2c8ed3b98c5de4a77c9746a59b4ecca64e94
M Hickford <mirth.hickford@gmail.com>
2021-01-10 20:10:23 +0100

    Prefer sudo to su when installing packages on crostini (Chrome OS)
    Fixes #494

--- pts-core/external-test-dependencies/scripts/install-debian-packages.sh
+++ pts-core/external-test-dependencies/scripts/install-debian-packages.sh
@@ -9,8 +9,9 @@ if [ -x /usr/bin/aptitude ]; then
 	# aptitude is nice since it doesn't fail if a non-existant package is hit
 	# See: http://bugs.debian.org/cgi-bin/bugreport.cgi?bug=503215
 	su -c "aptitude -y install $*"
-elif [ `whoami` = "admin" ] && [ -x /usr/bin/sudo ]; then
+elif [ `whoami` = "admin" ] || [ -f /dev/.cros_milestone ]] && [ -x /usr/bin/sudo ]; then
 	# Amazon EC2 is admin user, sudo works
+	# crostini (Chrome OS) also defaults to sudo https://chromeos.dev/en/linux/setup#installing-linux-apps-and-packages
 	sudo apt-get -y --ignore-missing install $*
 else
 	su -c "apt-get -y --ignore-missing install $*"
