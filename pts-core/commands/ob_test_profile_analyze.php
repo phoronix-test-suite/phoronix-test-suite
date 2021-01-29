cc019ab6652716f5d2e371f0d32f247691cc0e77
Michael Larabel <michael@phoronix.com>
2021-01-29 05:35:11 -0500

    pts-core: Analyze improvements

--- pts-core/commands/ob_test_profile_analyze.php
+++ pts-core/commands/ob_test_profile_analyze.php
@@ -29,16 +29,11 @@ class ob_test_profile_analyze implements pts_option_interface
 	const doc_description = 'This option is intended for test profile creators and generates a range of meta-data and other useful information that can be submitted to OpenBenchmarking.org to provide more verbose information for users of your test profiles.';
 
 	public static function run($r)
-	{/*
-		if(pts_openbenchmarking_client::user_name() == false)
-		{
-			echo PHP_EOL . 'You must first be logged into an OpenBenchmarking.org account.' . PHP_EOL;
-			echo PHP_EOL . 'Create An Account: http://openbenchmarking.org/';
-			echo PHP_EOL . 'Log-In Command: phoronix-test-suite openbenchmarking-setup' . PHP_EOL . PHP_EOL;
-			return false;
-		} */
-
-		ini_set('memory_limit', '2048M');
+	{
+		//ini_set('memory_limit', '2048M');
+		// https://software.intel.com/sites/landingpage/IntrinsicsGuide/#expand=641
+		$intrinsics_search = array(
+			);
 		foreach(pts_types::identifiers_to_test_profile_objects($r, false, true) as $test_profile)
 		{
 			$qualified_identifier = $test_profile->get_identifier();
@@ -52,8 +47,18 @@ class ob_test_profile_analyze implements pts_option_interface
 			pts_openbenchmarking_client::override_client_setting('AutoUploadResults', false);
 			pts_openbenchmarking_client::override_client_setting('UploadSystemLogsByDefault', true);
 			pts_test_installer::standard_install($qualified_identifier, true);
+			if(!$test_profile->is_test_installed())
+			{
+				// Test has issues even installing, so skip it...
+				continue;
+			}
 			$test_binary = self::locate_test_profile_lead_binary($test_profile);
 
+			// search for intrinsics
+			$intrinsics = array();
+
+			//self::recursively_search_text_files_in_dir($test_profile->get_test_executable_dir(), $intrics_search, $intrinsics);
+			$honors_cflags = false;
 			$shared_library_dependencies = array();
 			$instruction_usage = array();
 			$gl_calls = null;
@@ -75,27 +80,60 @@ class ob_test_profile_analyze implements pts_option_interface
 				echo PHP_EOL . 'SHARED LIBRARY DEPENDENCIES: ' . PHP_EOL;
 				print_r($shared_library_dependencies);
 
-				foreach(array('core-avx-i', 'bdver2') as $march)
+				$external_dependencies = $test_profile->get_external_dependencies();
+				foreach(array('default', 'sandybridge', 'skylake', 'cascadelake -mprefer-vector-width=512', 'sapphirerapids -mprefer-vector-width=512', 'alderlake', 'znver2', 'znver3') as $march)
 				{
 					// So for any compiling tasks they will try to use the most aggressive instructions possible
-					putenv('CFLAGS=-march=' . $march . ' -O3');
-					putenv('CXXFLAGS=-march=' . $march . ' -O3');
-					pts_test_installer::standard_install($qualified_identifier, true);
-					$instruction_usage[$march] = self::analyze_binary_instruction_usage($test_binary);
-
-					if($instruction_usage[$march] == null)
+					if($march != 'default')
 					{
-						unset($instruction_usage[$march]);
+						if(!in_array('build-utilities', $external_dependencies))
+						{
+							continue;
+						}
+						putenv('CFLAGS= -march=' . $march . ' -O3 ');
+						putenv('CXXFLAGS= -march=' . $march . ' -O3 ');
+					}
+					else
+					{
+						putenv('CFLAGS');
+						putenv('CXXFLAGS');
+					}
+					if(($x = strpos($march, ' ')) !== false)
+					{
+						$march = substr($march, 0, $x);
+					}
+					pts_test_installer::standard_install($qualified_identifier, true);
+					if($test_profile->is_test_installed())
+					{
+						$iu = self::analyze_binary_instruction_usage($test_binary);
+
+						if($iu != null)
+						{
+							if($march != 'default' && $honors_cflags == false && !empty($iu) && isset($instruction_usage['default']) && $iu != $instruction_usage['default'])
+							{
+								$honors_cflags = true;
+							}
+							$instruction_usage[$march] = $iu;
+						}
 					}
 				}
 
-				if(!empty($instruction_usage) && count(array_unique($instruction_usage)) == 1)
-				{
-					$generic = array_pop($instruction_usage);
-
-					$instruction_usage = array('generic' => $generic);
-				}
+				var_dump($shared_library_dependencies);
 				var_dump($instruction_usage);
+				echo "HONORS: ";
+				var_dump($honors_cflags);
+
+				if(pts_openbenchmarking_client::user_name() != false)
+				{
+					$server_response = pts_openbenchmarking::make_openbenchmarking_request('upload_test_meta', array(
+						'i' => $test_profile->get_identifier(),
+						'ldd_libraries' => implode(',', $shared_library_dependencies),
+						'instruction_set_usage' => base64_encode(json_encode($instruction_usage)),
+						'honors_cflags' => ($honors_cflags ? 1 : 0)
+						));
+						var_dump($server_response);
+						$json = json_decode($server_response, true);
+				}
 			}
 			else
 			{
@@ -104,23 +142,7 @@ class ob_test_profile_analyze implements pts_option_interface
 		//		return false;
 			}
 		}
-			sleep(10);
 
-		var_dump($shared_library_dependencies);
-		var_dump($instruction_usage);
-		var_dump($gl_calls);
-/*
-		$server_response = pts_openbenchmarking::make_openbenchmarking_request('upload_test_meta', array(
-			'i' => $test_profile->get_identifier(),
-			'ldd_libraries' => implode(',', $shared_library_dependencies),
-			'opengl_calls' => $gl_calls,
-			'instruction_set_usage' => base64_encode(json_encode($instruction_usage))
-			));
-var_dump($server_response);
-			$json = json_decode($server_response, true);
-			*/
-
-		pts_file_io::unlink($ss_zip_file);
 	}
 	public static function locate_test_profile_lead_binary(&$test_profile)
 	{
@@ -148,9 +170,9 @@ var_dump($server_response);
 
 		if($test_binary)
 		{
-			if(is_executable($test_profile->get_test_executable_dir() . '/' . $test_binary))
+			if(is_executable($test_profile->get_test_executable_dir() . $test_binary) && $test_profile->get_test_executable_dir() . $test_binary != $test_profile_launcher)
 			{
-				$test_binary = $test_profile->get_test_executable_dir() . '/' . $test_binary;
+				$test_binary = $test_profile->get_test_executable_dir() . $test_binary;
 			}
 			else if(($s = strpos($launcher_contents, PHP_EOL . 'cd ')))
 			{
@@ -166,6 +188,36 @@ var_dump($server_response);
 
 		return $test_binary;
 	}
+	public static function recursively_search_text_files_in_dir($object, &$search_for, &$hits)
+	{
+		if(is_dir($object))
+		{
+			$object = pts_strings::add_trailing_slash($object);
+		}
+
+		foreach(pts_file_io::glob($object . '*') as $to_read)
+		{
+			if(is_file($to_read) && mime_content_type($to_remove) == 'text/plain')
+			{
+				$to_read_contents = file_get_contents($to_read);
+				foreach($search_for as $search => $report)
+				{
+					if(strpos($to_read_contents, $search) !== false)
+					{
+						if(!isset($hits[$to_read]))
+						{
+							$hits[$to_read][] = $report;
+						}
+					}
+				}
+				
+			}
+			else if(is_dir($to_read))
+			{
+				self::recursively_search_text_files_in_dir($to_read, $search_for, $hits);
+			}
+		}
+	}
 	public static function analyze_binary_instruction_usage(&$binary)
 	{
 		// Based on data from https://github.com/dirtyepic/scripts/blob/master/analyze-x86
@@ -178,25 +230,49 @@ var_dump($server_response);
 		'SSE4_1' => array('blendpd', 'blendps', 'blendvpd', 'blendvps', 'dppd', 'dpps', 'extractps', 'insertps', 'movntdqa', 'mpsadbw', 'packusdw', 'pblendvb', 'pblendw', 'pcmpeqq', 'pextrb', 'pextrd', 'pextrq', 'phminposuw', 'pinsrb', 'pinsrd', 'pinsrq', 'pmaxsb', 'pmaxsd', 'pmaxud', 'pmaxuw', 'pminsb', 'pminsd', 'pminud', 'pminuw', 'pmovsxbd', 'pmovsxbq', 'pmovsxbw', 'pmovsxdq', 'pmovsxwd', 'pmovsxwq', 'pmovzxbd', 'pmovzxbq', 'pmovzxbw', 'pmovzxdq', 'pmovzxwd', 'pmovzxwq', 'pmuldq', 'pmulld', 'ptest', 'roundpd', 'roundps', 'roundsd', 'roundss'),
 		'SSE4_2' => array('crc32', 'pcmpestri', 'pcmpestrm', 'pcmpgtq', 'pcmpistri', 'pcmpistrm', 'popcnt'),
 		'SSE4A' => array('extrq', 'insertq', 'movntsd', 'movntss'),
-		'AVX' => array('pclmulhqhqdq', 'pclmulhqlqdq', 'pclmullqhqdq', 'pclmullqlqdq', 'pclmulqdq', 'vaddpd', 'vaddps', 'vaddsd', 'vaddss', 'vaddsubpd', 'vaddsubps', 'vaesdec', 'vaesdeclast', 'vaesenc', 'vaesenclast', 'vaesimc', 'vaeskeygenassist', 'vandnpd', 'vandnps', 'vandpd', 'vandps', 'vblendpd', 'vblendps', 'vblendvpd', 'vblendvps', 'vbroadcastf128', 'vbroadcastsd', 'vbroadcastss', 'vcmpeq_ospd', 'vcmpeq_osps', 'vcmpeq_ossd', 'vcmpeq_osss', 'vcmpeqpd', 'vcmpeqps', 'vcmpeqsd', 'vcmpeqss', 'vcmpeq_uqpd', 'vcmpeq_uqps', 'vcmpeq_uqsd', 'vcmpeq_uqss', 'vcmpeq_uspd', 'vcmpeq_usps', 'vcmpeq_ussd', 'vcmpeq_usss', 'vcmpfalse_oqpd', 'vcmpfalse_oqps', 'vcmpfalse_oqsd', 'vcmpfalse_oqss', 'vcmpfalse_ospd', 'vcmpfalse_osps', 'vcmpfalse_ossd', 'vcmpfalse_osss', 'vcmpfalsepd', 'vcmpfalseps', 'vcmpfalsesd', 'vcmpfalsess', 'vcmpge_oqpd', 'vcmpge_oqps', 'vcmpge_oqsd', 'vcmpge_oqss', 'vcmpge_ospd', 'vcmpge_osps', 'vcmpge_ossd', 'vcmpge_osss', 'vcmpgepd', 'vcmpgeps', 'vcmpgesd', 'vcmpgess', 'vcmpgt_oqpd', 'vcmpgt_oqps', 'vcmpgt_oqsd', 'vcmpgt_oqss', 'vcmpgt_ospd', 'vcmpgt_osps', 'vcmpgt_ossd', 'vcmpgt_osss', 'vcmpgtpd', 'vcmpgtps', 'vcmpgtsd', 'vcmpgtss', 'vcmple_oqpd', 'vcmple_oqps', 'vcmple_oqsd', 'vcmple_oqss', 'vcmple_ospd', 'vcmple_osps', 'vcmple_ossd', 'vcmple_osss', 'vcmplepd', 'vcmpleps', 'vcmplesd', 'vcmpless', 'vcmplt_oqpd', 'vcmplt_oqps', 'vcmplt_oqsd', 'vcmplt_oqss', 'vcmplt_ospd', 'vcmplt_osps', 'vcmplt_ossd', 'vcmplt_osss', 'vcmpltpd', 'vcmpltps', 'vcmpltsd', 'vcmpltss', 'vcmpneq_oqpd', 'vcmpneq_oqps', 'vcmpneq_oqsd', 'vcmpneq_oqss', 'vcmpneq_ospd', 'vcmpneq_osps', 'vcmpneq_ossd', 'vcmpneq_osss', 'vcmpneqpd', 'vcmpneqps', 'vcmpneqsd', 'vcmpneqss', 'vcmpneq_uqpd', 'vcmpneq_uqps', 'vcmpneq_uqsd', 'vcmpneq_uqss', 'vcmpneq_uspd', 'vcmpneq_usps', 'vcmpneq_ussd', 'vcmpneq_usss', 'vcmpngepd', 'vcmpngeps', 'vcmpngesd', 'vcmpngess', 'vcmpnge_uqpd', 'vcmpnge_uqps', 'vcmpnge_uqsd', 'vcmpnge_uqss', 'vcmpnge_uspd', 'vcmpnge_usps', 'vcmpnge_ussd', 'vcmpnge_usss', 'vcmpngtpd', 'vcmpngtps', 'vcmpngtsd', 'vcmpngtss', 'vcmpngt_uqpd', 'vcmpngt_uqps', 'vcmpngt_uqsd', 'vcmpngt_uqss', 'vcmpngt_uspd', 'vcmpngt_usps', 'vcmpngt_ussd', 'vcmpngt_usss', 'vcmpnlepd', 'vcmpnleps', 'vcmpnlesd', 'vcmpnless', 'vcmpnle_uqpd', 'vcmpnle_uqps', 'vcmpnle_uqsd', 'vcmpnle_uqss', 'vcmpnle_uspd', 'vcmpnle_usps', 'vcmpnle_ussd', 'vcmpnle_usss', 'vcmpnltpd', 'vcmpnltps', 'vcmpnltsd', 'vcmpnltss', 'vcmpnlt_uqpd', 'vcmpnlt_uqps', 'vcmpnlt_uqsd', 'vcmpnlt_uqss', 'vcmpnlt_uspd', 'vcmpnlt_usps', 'vcmpnlt_ussd', 'vcmpnlt_usss', 'vcmpordpd', 'vcmpordps', 'vcmpord_qpd', 'vcmpord_qps', 'vcmpord_qsd', 'vcmpord_qss', 'vcmpordsd', 'vcmpord_spd', 'vcmpord_sps', 'vcmpordss', 'vcmpord_ssd', 'vcmpord_sss', 'vcmppd', 'vcmpps', 'vcmpsd', 'vcmpss', 'vcmptruepd', 'vcmptrueps', 'vcmptruesd', 'vcmptruess', 'vcmptrue_uqpd', 'vcmptrue_uqps', 'vcmptrue_uqsd', 'vcmptrue_uqss', 'vcmptrue_uspd', 'vcmptrue_usps', 'vcmptrue_ussd', 'vcmptrue_usss', 'vcmpunordpd', 'vcmpunordps', 'vcmpunord_qpd', 'vcmpunord_qps', 'vcmpunord_qsd', 'vcmpunord_qss', 'vcmpunordsd', 'vcmpunord_spd', 'vcmpunord_sps', 'vcmpunordss', 'vcmpunord_ssd', 'vcmpunord_sss', 'vcomisd', 'vcomiss', 'vcvtdq2pd', 'vcvtdq2ps', 'vcvtpd2dq', 'vcvtpd2ps', 'vcvtps2dq', 'vcvtps2pd', 'vcvtsd2si', 'vcvtsd2ss', 'vcvtsi2sd', 'vcvtsi2ss', 'vcvtss2sd', 'vcvtss2si', 'vcvttpd2dq', 'vcvttps2dq', 'vcvttsd2si', 'vcvttss2si', 'vdivpd', 'vdivps', 'vdivsd', 'vdivss', 'vdppd', 'vdpps', 'vextractf128', 'vextractps', 'vhaddpd', 'vhaddps', 'vhsubpd', 'vhsubps', 'vinsertf128', 'vinsertps', 'vlddqu', 'vldmxcsr', 'vldqqu', 'vmaskmovdqu', 'vmaskmovpd', 'vmaskmovps', 'vmaxpd', 'vmaxps', 'vmaxsd', 'vmaxss', 'vminpd', 'vminps', 'vminsd', 'vminss', 'vmovapd', 'vmovaps', 'vmovd', 'vmovddup', 'vmovdqa', 'vmovdqu', 'vmovhlps', 'vmovhpd', 'vmovhps', 'vmovlhps', 'vmovlpd', 'vmovlps', 'vmovmskpd', 'vmovmskps', 'vmovntdq', 'vmovntdqa', 'vmovntpd', 'vmovntps', 'vmovntqq', 'vmovq', 'vmovqqa', 'vmovqqu', 'vmovsd', 'vmovshdup', 'vmovsldup', 'vmovss', 'vmovupd', 'vmovups', 'vmpsadbw', 'vmulpd', 'vmulps', 'vmulsd', 'vmulss', 'vorpd', 'vorps', 'vpabsb', 'vpabsd', 'vpabsw', 'vpackssdw', 'vpacksswb', 'vpackusdw', 'vpackuswb', 'vpaddb', 'vpaddd', 'vpaddq', 'vpaddsb', 'vpaddsw', 'vpaddusb', 'vpaddusw', 'vpaddw', 'vpalignr', 'vpand', 'vpandn', 'vpavgb', 'vpavgw', 'vpblendvb', 'vpblendw', 'vpclmulhqhqdq', 'vpclmulhqlqdq', 'vpclmullqhqdq', 'vpclmullqlqdq', 'vpclmulqdq', 'vpcmpeqb', 'vpcmpeqd', 'vpcmpeqq', 'vpcmpeqw', 'vpcmpestri', 'vpcmpestrm', 'vpcmpgtb', 'vpcmpgtd', 'vpcmpgtq', 'vpcmpgtw', 'vpcmpistri', 'vpcmpistrm', 'vperm2f128', 'vpermilpd', 'vpermilps', 'vpextrb', 'vpextrd', 'vpextrq', 'vpextrw', 'vphaddd', 'vphaddsw', 'vphaddw', 'vphminposuw', 'vphsubd', 'vphsubsw', 'vphsubw', 'vpinsrb', 'vpinsrd', 'vpinsrq', 'vpinsrw', 'vpmaddubsw', 'vpmaddwd', 'vpmaxsb', 'vpmaxsd', 'vpmaxsw', 'vpmaxub', 'vpmaxud', 'vpmaxuw', 'vpminsb', 'vpminsd', 'vpminsw', 'vpminub', 'vpminud', 'vpminuw', 'vpmovmskb', 'vpmovsxbd', 'vpmovsxbq', 'vpmovsxbw', 'vpmovsxdq', 'vpmovsxwd', 'vpmovsxwq', 'vpmovzxbd', 'vpmovzxbq', 'vpmovzxbw', 'vpmovzxdq', 'vpmovzxwd', 'vpmovzxwq', 'vpmuldq', 'vpmulhrsw', 'vpmulhuw', 'vpmulhw', 'vpmulld', 'vpmullw', 'vpmuludq', 'vpor', 'vpsadbw', 'vpshufb', 'vpshufd', 'vpshufhw', 'vpshuflw', 'vpsignb', 'vpsignd', 'vpsignw', 'vpslld', 'vpslldq', 'vpsllq', 'vpsllw', 'vpsrad', 'vpsraw', 'vpsrld', 'vpsrldq', 'vpsrlq', 'vpsrlw', 'vpsubb', 'vpsubd', 'vpsubq', 'vpsubsb', 'vpsubsw', 'vpsubusb', 'vpsubusw', 'vpsubw', 'vptest', 'vpunpckhbw', 'vpunpckhdq', 'vpunpckhqdq', 'vpunpckhwd', 'vpunpcklbw', 'vpunpckldq', 'vpunpcklqdq', 'vpunpcklwd', 'vpxor', 'vrcpps', 'vrcpss', 'vroundpd', 'vroundps', 'vroundsd', 'vroundss', 'vrsqrtps', 'vrsqrtss', 'vshufpd', 'vshufps', 'vsqrtpd', 'vsqrtps', 'vsqrtsd', 'vsqrtss', 'vstmxcsr', 'vsubpd', 'vsubps', 'vsubsd', 'vsubss', 'vtestpd', 'vtestps', 'vucomisd', 'vucomiss', 'vunpckhpd', 'vunpckhps', 'vunpcklpd', 'vunpcklps', 'vxorpd', 'vxorps', 'vzeroall', 'vzeroupper'),
+		'AVX' => 'VBROADCASTSS VBROADCASTSD VBROADCASTF128 VINSERTF128 VEXTRACTF128 VMASKMOVPS VPERMILPS VPERMILPD VPERM2F128 VZEROALL VZEROUPPER',
+		'AVX2' => 'VPBROADCASTB VPBROADCASTW VPBROADCASTD VPBROADCASTQ VINSERTI128 VEXTRACTI128 VGATHERDPD VGATHERQPD VGATHERDPS VGATHERQPS VPGATHERDD VPGATHERDQ VPGATHERQD VPGATHERQQ VPMASKMOVD VPMASKMOVQ VPERMPS VPERMD VPERMPD VPERMQ VPERM2I128 VPBLENDD VPSLLVD VPSLLVQ  VPSRLVD VPSRLVQ  VPSRAVD',
+		'AES' => 'AESENC AESENCLAST AESDEC AESDECLAST AESKEYGENASSIST AESIMC',
+		'AVX512' => 'AVX512F AVX512CD AVX512DQ AVX512PF AVX512ER AVX512VL AVX512BW AVX512IFMA AVX512VBMI AVX512VBMI2 AVX512VAES AVX512BITALG AVX5124FMAPS AVX512VPCLMULQDQ AVX512GFNI AVX512_VNNI AVX5124VNNIW AVX512VPOPCNTDQ AVX512_BF16',
+		'VAES' => 'VAESDEC VAESDECLAST VAESENC VAESENCLAST',
+		'AVX-VNNI' => 'avxvnni',
+		'AMX' => 'LDTILECFG STTILECFG TILELOADD TILELOADDT1 TILESTORED TILERELEASE TILEZERO TDPBF16PS',
 		'FMA' => array('vfmadd123pd', 'vfmadd123ps', 'vfmadd123sd', 'vfmadd123ss', 'vfmadd132pd', 'vfmadd132ps', 'vfmadd132sd', 'vfmadd132ss', 'vfmadd213pd', 'vfmadd213ps', 'vfmadd213sd', 'vfmadd213ss', 'vfmadd231pd', 'vfmadd231ps', 'vfmadd231sd', 'vfmadd231ss', 'vfmadd312pd', 'vfmadd312ps', 'vfmadd312sd', 'vfmadd312ss', 'vfmadd321pd', 'vfmadd321ps', 'vfmadd321sd', 'vfmadd321ss', 'vfmaddsub123pd', 'vfmaddsub123ps', 'vfmaddsub132pd', 'vfmaddsub132ps', 'vfmaddsub213pd', 'vfmaddsub213ps', 'vfmaddsub231pd', 'vfmaddsub231ps', 'vfmaddsub312pd', 'vfmaddsub312ps', 'vfmaddsub321pd', 'vfmaddsub321ps', 'vfmsub123pd', 'vfmsub123ps', 'vfmsub123sd', 'vfmsub123ss', 'vfmsub132pd', 'vfmsub132ps', 'vfmsub132sd', 'vfmsub132ss', 'vfmsub213pd', 'vfmsub213ps', 'vfmsub213sd', 'vfmsub213ss', 'vfmsub231pd', 'vfmsub231ps', 'vfmsub231sd', 'vfmsub231ss', 'vfmsub312pd', 'vfmsub312ps', 'vfmsub312sd', 'vfmsub312ss', 'vfmsub321pd', 'vfmsub321ps', 'vfmsub321sd', 'vfmsub321ss', 'vfmsubadd123pd', 'vfmsubadd123ps', 'vfmsubadd132pd', 'vfmsubadd132ps', 'vfmsubadd213pd', 'vfmsubadd213ps', 'vfmsubadd231pd', 'vfmsubadd231ps', 'vfmsubadd312pd', 'vfmsubadd312ps', 'vfmsubadd321pd', 'vfmsubadd321ps', 'vfnmadd123pd', 'vfnmadd123ps', 'vfnmadd123sd', 'vfnmadd123ss', 'vfnmadd132pd', 'vfnmadd132ps', 'vfnmadd132sd', 'vfnmadd132ss', 'vfnmadd213pd', 'vfnmadd213ps', 'vfnmadd213sd', 'vfnmadd213ss', 'vfnmadd231pd', 'vfnmadd231ps', 'vfnmadd231sd', 'vfnmadd231ss', 'vfnmadd312pd', 'vfnmadd312ps', 'vfnmadd312sd', 'vfnmadd312ss', 'vfnmadd321pd', 'vfnmadd321ps', 'vfnmadd321sd', 'vfnmadd321ss', 'vfnmsub123pd', 'vfnmsub123ps', 'vfnmsub123sd', 'vfnmsub123ss', 'vfnmsub132pd', 'vfnmsub132ps', 'vfnmsub132sd', 'vfnmsub132ss', 'vfnmsub213pd', 'vfnmsub213ps', 'vfnmsub213sd', 'vfnmsub213ss', 'vfnmsub231pd', 'vfnmsub231ps', 'vfnmsub231sd', 'vfnmsub231ss', 'vfnmsub312pd', 'vfnmsub312ps', 'vfnmsub312sd', 'vfnmsub312ss', 'vfnmsub321pd', 'vfnmsub321ps', 'vfnmsub321sd', 'vfnmsub321ss'),
-		'FMA4' => array('vfmaddpd', 'vfmaddps', 'vfmaddsd', 'vfmaddss', 'vfmaddsubpd', 'vfmaddsubps', 'vfmsubaddpd', 'vfmsubaddps', 'vfmsubpd', 'vfmsubps', 'vfmsubsd', 'vfmsubss', 'vfnmaddpd', 'vfnmaddps', 'vfnmaddsd', 'vfnmaddss', 'vfnmsubpd', 'vfnmsubps', 'vfnmsubsd', 'vfnmsubss', 'vfrczpd', 'vfrczps', 'vfrczsd', 'vfrczss', 'vpcmov', 'vpcomb', 'vpcomd', 'vpcomq', 'vpcomub', 'vpcomud', 'vpcomuq', 'vpcomuw', 'vpcomw', 'vphaddbd', 'vphaddbq', 'vphaddbw', 'vphadddq', 'vphaddubd', 'vphaddubq', 'vphaddubw', 'vphaddudq', 'vphadduwd', 'vphadduwq', 'vphaddwd', 'vphaddwq', 'vphsubbw', 'vphsubdq', 'vphsubwd', 'vpmacsdd', 'vpmacsdqh', 'vpmacsdql', 'vpmacssdd', 'vpmacssdqh', 'vpmacssdql', 'vpmacsswd', 'vpmacssww', 'vpmacswd', 'vpmacsww', 'vpmadcsswd', 'vpmadcswd', 'vpperm', 'vprotb', 'vprotd', 'vprotq', 'vprotw', 'vpshab', 'vpshad', 'vpshaq', 'vpshaw', 'vpshlb', 'vpshld', 'vpshlq', 'vpshlw'),
+		'BMI2' => 'BZHI MULX PDEP PEXT RORX SARX SHRX SHLX',
 		);
+		
+		foreach($instruction_checks as $set => &$instructions)
+		{
+			if(!is_array($instructions))
+			{
+				$instructions = explode(' ', trim($instructions));
+			}
+			$instructions = array_map('trim', $instructions);
+			$instructions = array_map('strtolower', $instructions);
+		}
 
 		foreach(array_keys($instruction_checks) as $set)
 		{
-			$instruction_usage[$set] = 0;
+			$instruction_usage[$set] = array();
 		}
 		$instruction_usage['OTHER'] = 0;
+		
+		$objdump_file =  tempnam('/tmp', 'objdump');
+		shell_exec('objdump -d ' . $binary . ' | cut -f3 | cut -d\' \' -f1 > ' . $objdump_file);
 
-		foreach(explode(PHP_EOL , shell_exec('objdump -d ' . $binary . ' | cut -f3 | cut -d\' \' -f1')) as $instruction)
+		$handle = fopen($objdump_file, 'r');
+		while(($instruction = fgets($handle)) !== false)
 		{
 			$matched_instruction = false;
 			foreach($instruction_checks as $set => $instructions)
 			{
-				if(in_array(trim($instruction), $instructions))
+				$instr = trim(strtolower($instruction));
+				if($instr != null && in_array($instr, $instructions))
 				{
-					$instruction_usage[$set] += 1;
+					if(!isset($instruction_usage[$set][$instr]))
+					{
+						$instruction_usage[$set][$instr] = $instr;
+					}
 					$matched_instruction = true;
 					break;
 				}
@@ -204,15 +280,21 @@ var_dump($server_response);
 
 			if($matched_instruction == false)
 			{
-				$instruction_usage['OTHER'] += 1;
+				//$instruction_usage['OTHER'] += 1;
 			}
 		}
+		fclose($handle);
+		unlink($objdump_file);
 
 		foreach($instruction_usage as $instruction => $usage)
 		{
-			if($usage < 2)
+			if(!is_array($instruction_usage[$instruction]) || empty($instruction_usage[$instruction]))
 			{
 				unset($instruction_usage[$instruction]);
+			}
+			else
+			{
+				$instruction_usage[$instruction] = array_keys($usage);
 			}
 		}
 
