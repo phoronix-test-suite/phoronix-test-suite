f1f0629613b5402723af9298e3feb5ecfc4acc7c
Michael Larabel <michael@phoronix.com>
2021-07-29 05:01:17 -0500

    pts-core: Fix possible issue with Windows batch file execution
    Normally Windows path ends up handling "./" fine but it seems in at least some cases when passed as an argument to cmd.exe it will otherwise cause script execution to fail, so just remove it here since not necessary.

--- pts-core/objects/client/pts_test_execution.php
+++ pts-core/objects/client/pts_test_execution.php
@@ -124,6 +124,7 @@ class pts_test_execution
 		pts_client::$display->display_interrupt_message($test_run_request->test_profile->get_pre_run_message());
 		$runtime_identifier = time();
 		$execute_binary_prepend = '';
+		$execute_binary_prepend_final = './';
 		if($test_run_request->exec_binary_prepend != null)
 		{
 			$execute_binary_prepend = $test_run_request->exec_binary_prepend;
@@ -237,8 +238,6 @@ class pts_test_execution
 				$test_prepend = getenv('TEST_EXEC_PREPEND') != null ? getenv('TEST_EXEC_PREPEND') . ' ': null;
 				pts_client::$display->test_run_instance_header($test_run_request);
 				sleep(2);
-				pts_test_result_parser::debug_message('Test Run Directory: ' . $to_execute);
-				pts_test_result_parser::debug_message('Test Run Command: ' . $test_prepend . $execute_binary_prepend . ' ./' . $execute_binary . ' ' . $pts_test_arguments);
 
 				$host_env = $_SERVER;
 				unset($host_env['argv']);
@@ -252,6 +251,7 @@ class pts_test_execution
 						pts_client::$display->test_run_message('Using cmd.exe batch...');
 						$to_exec = 'C:\Windows\System32\cmd.exe';
 						$execute_binary_prepend = ' /c ';
+						$execute_binary_prepend_final = '';
 						$use_phoroscript = false;
 						$post_test_args = '';
 					}
@@ -267,6 +267,8 @@ class pts_test_execution
 					}
 				}
 
+				pts_test_result_parser::debug_message('Test Run Directory: ' . $to_execute);
+				pts_test_result_parser::debug_message('Test Run Command: ' . $test_prepend . $execute_binary_prepend . $execute_binary_prepend_final . $execute_binary . ' ' . $pts_test_arguments);
 				$is_monitoring = pts_test_result_parser::system_monitor_task_check($test_run_request);
 				$test_run_time_start = microtime(true);
 
@@ -298,7 +300,7 @@ class pts_test_execution
 						}
 					}
 
-					$test_process = proc_open($test_prepend . $to_exec . ' ' . $execute_binary_prepend . './' . $execute_binary . ' ' . $pts_test_arguments . $post_test_args, $descriptorspec, $pipes, $to_execute, array_merge($host_env, pts_client::environmental_variables(), $terv));
+					$test_process = proc_open($test_prepend . $to_exec . ' ' . $execute_binary_prepend . $execute_binary_prepend_final . $execute_binary . ' ' . $pts_test_arguments . $post_test_args, $descriptorspec, $pipes, $to_execute, array_merge($host_env, pts_client::environmental_variables(), $terv));
 
 					if(is_resource($test_process))
 					{
