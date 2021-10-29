32747158189dbd7e552dac4adc38e40faadca88f
Changming Sun <chasun@microsoft.com>
2021-10-29 13:24:19 -0700

    Update ONNX Runtime perf test

--- ob-cache/test-profiles/pts/onnx-1.1.0/install.sh
+++ ob-cache/test-profiles/pts/onnx-1.1.0/install.sh
@@ -3,8 +3,8 @@
 rm -rf onnxruntime
 git clone https://github.com/microsoft/onnxruntime
 cd onnxruntime
-git checkout v1.8.2
-./build.sh --config Release --build_shared_lib --parallel --use_openmp --skip_tests
+git checkout v1.9.1
+./build.sh --config Release --build_shared_lib --parallel --skip_tests --enable_lto --cmake_extra_defines onnxruntime_BUILD_FOR_NATIVE_MACHINE=ON
 echo $? > ~/install-exit-status
 retVal=$?
 if [ $retVal -ne 0 ]; then
@@ -17,10 +17,10 @@ tar -xf yolov4.tar.gz
 tar -xf fcn-resnet101-11.tar.gz
 tar -xf shufflenet-v2-10.tar.gz
 tar -xf super-resolution-10.tar.gz
-tar -xf bertsquad-10.tar.gz
+
 
 echo "#!/bin/bash
 
-OMP_NUM_THREADS=\$NUM_CPU_CORES ./onnxruntime/build/Linux/Release/onnxruntime_perf_test \$@ > \$LOG_FILE 2>&1
+./onnxruntime/build/Linux/Release/onnxruntime_perf_test \$@ > \$LOG_FILE 2>&1
 echo \$? > ~/test-exit-status" > onnx
 chmod +x onnx
